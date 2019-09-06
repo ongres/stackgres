a9617c81e0d7705738266ca9c56b421c5a0b15b0
Matteo Melli <matteom@ongres.com>
2019-09-06 12:07:54 +0200

    Aligned CRD with operator

--- operator/src/main/java/io/stackgres/operator/customresources/sgprofile/StackGresProfileSpec.java
+++ operator/src/main/java/io/stackgres/operator/customresources/sgprofile/StackGresProfileSpec.java
@@ -5,13 +5,13 @@
 
 package io.stackgres.operator.customresources.sgprofile;
 
-import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+
 import io.fabric8.kubernetes.api.model.KubernetesResource;
-import io.fabric8.kubernetes.api.model.ResourceRequirements;
 import io.quarkus.runtime.annotations.RegisterForReflection;
 
 @JsonDeserialize
@@ -21,28 +21,76 @@ public class StackGresProfileSpec implements KubernetesResource {
 
   private static final long serialVersionUID = -1037668102382589521L;
 
-  @JsonProperty("resources")
-  @Valid
-  private ResourceRequirements resources;
+  @JsonProperty("cpuRequests")
+  @NotBlank
+  private String cpuRequests;
 
-  @JsonProperty("volume")
-  @Valid
-  private VolumeConfig volume;
+  @JsonProperty("memoryRequests")
+  @NotBlank
+  private String memoryRequests;
 
-  public ResourceRequirements getResources() {
-    return resources;
+  @JsonProperty("cpuLimits")
+  @NotBlank
+  private String cpuLimits;
+
+  @JsonProperty("memoryLimits")
+  @NotBlank
+  private String memoryLimits;
+
+  @JsonProperty("volumeSize")
+  @NotBlank
+  private String volumeSize;
+
+  @JsonProperty("volumeStorageClass")
+  @NotBlank
+  private String volumeStorageClass;
+
+  public String getCpuRequests() {
+    return cpuRequests;
   }
 
-  public void setResources(ResourceRequirements resources) {
-    this.resources = resources;
+  public void setCpuRequests(String cpuRequests) {
+    this.cpuRequests = cpuRequests;
   }
 
-  public VolumeConfig getVolume() {
-    return volume;
+  public String getMemoryRequests() {
+    return memoryRequests;
   }
 
-  public void setVolume(VolumeConfig volume) {
-    this.volume = volume;
+  public void setMemoryRequests(String memoryRequests) {
+    this.memoryRequests = memoryRequests;
+  }
+
+  public String getCpuLimits() {
+    return cpuLimits;
+  }
+
+  public void setCpuLimits(String cpuLimits) {
+    this.cpuLimits = cpuLimits;
+  }
+
+  public String getMemoryLimits() {
+    return memoryLimits;
+  }
+
+  public void setMemoryLimits(String memoryLimits) {
+    this.memoryLimits = memoryLimits;
+  }
+
+  public String getVolumeSize() {
+    return volumeSize;
+  }
+
+  public void setVolumeSize(String volumeSize) {
+    this.volumeSize = volumeSize;
+  }
+
+  public String getVolumeStorageClass() {
+    return volumeStorageClass;
+  }
+
+  public void setVolumeStorageClass(String volumeStorageClass) {
+    this.volumeStorageClass = volumeStorageClass;
   }
 
 }
