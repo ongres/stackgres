1af7f83187cecee385102039348d382efe66ffc6
Matteo Melli <matteom@ongres.com>
2019-09-10 16:10:24 +0200

    Renamed StackGresPoolingConfig to StackGresConnectionPoolingConfig

--- sidecars/src/main/java/io/stackgres/sidecars/pgbouncer/customresources/StackGresPgbouncerConfigDefinition.java
+++ sidecars/src/main/java/io/stackgres/sidecars/pgbouncer/customresources/StackGresPgbouncerConfigDefinition.java
@@ -9,9 +9,9 @@ public class StackGresPgbouncerConfigDefinition {
 
   public static final String GROUP = "stackgres.io";
   public static final String VERSION = "v1alpha1";
-  public static final String KIND = "StackGresPoolingConfig";
-  public static final String SINGULAR = "sgpoolingconfig";
-  public static final String PLURAL = "sgpoolingconfigs";
+  public static final String KIND = "StackGresConnectionPoolingConfig";
+  public static final String SINGULAR = "sgconnectionpoolingconfig";
+  public static final String PLURAL = "sgconnectionpoolingconfigs";
   public static final String NAME = PLURAL + "." + GROUP;
   public static final String APIVERSION = GROUP + "/" + VERSION;
 
