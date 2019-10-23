8710f44b2a738cd4088d3add89e1ad3beb2f8af7
Matteo Melli <matteom@ongres.com>
2019-10-23 12:46:35 +0200

    it: run IT with operator deployment using builded image

--- operator/src/test/java/io/stackgres/operator/KubernetesOperatorRunner.java
+++ operator/src/test/java/io/stackgres/operator/KubernetesOperatorRunner.java
@@ -3,10 +3,10 @@ package io.stackgres.operator;
 import java.io.IOException;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
-import java.util.stream.Stream;
 
 import com.ongres.junit.docker.Container;
 
+import org.jooq.lambda.Unchecked;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -33,23 +33,35 @@ public class KubernetesOperatorRunner implements OperatorRunner {
   @Override
   public void run() throws Throwable {
     ItHelper.waitUntilOperatorIsReady(future, null, kind);
-    final Stream<String> runnerLogStream = kind.execute("sh", "-l", "-c",
-        "(sleep 120; while true; do sleep 5; seq -s ' ' 10000000 10000910; done) &"
-            + " kubectl get pod -n stackgres"
-            + " | grep 'stackgres-operator'"
-            + " | grep -v 'stackgres-operator-init'"
-            + " | cut -d ' ' -f 1"
-            + " | xargs kubectl logs -n stackgres -c stackgres-operator -f ")
-        .filter(ItHelper.EXCLUDE_TTY_WARNING)
-        .filter(line -> !line.startsWith("10000000 10000001 10000002 "));
-    CompletableFuture.runAsync(() -> {
+    CompletableFuture<String> runnerLogPid = new CompletableFuture<String>();
+    CompletableFuture<Void> runnerLogFuture = CompletableFuture.runAsync(() -> {
       try {
-        runnerLogStream.forEach(line -> LOGGER.info(line));
+        kind.execute("sh", "-l", "-c",
+            "echo $$;"
+                + " trap 'kill $(jobs -p)' EXIT;"
+                + " kubectl get pod -n stackgres"
+                + " | grep 'stackgres-operator'"
+                + " | grep -v 'stackgres-operator-init'"
+                + " | cut -d ' ' -f 1"
+                + " | xargs kubectl logs -n stackgres -c stackgres-operator -f ")
+            .filter(ItHelper.EXCLUDE_TTY_WARNING)
+            .peek(line -> {
+              if (!runnerLogPid.isDone()) {
+                runnerLogPid.complete(line);
+              }
+            })
+            .forEach(line -> LOGGER.info(line));
       } catch (Exception ex) {
-        LOGGER.warn("An error occurred while logging operator", ex);
+        return;
       }
     }, executor);
     future.join();
-    runnerLogStream.close();
+    CompletableFuture.runAsync(Unchecked.runnable(() -> {
+      kind.execute("sh", "-l", "-c",
+          "kill " + runnerLogPid.join())
+          .filter(ItHelper.EXCLUDE_TTY_WARNING)
+          .forEach(line -> LOGGER.info(line));
+    }), executor);
+     runnerLogFuture.join();
   }
 }
