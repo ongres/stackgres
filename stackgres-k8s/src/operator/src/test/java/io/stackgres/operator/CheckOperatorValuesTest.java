943f13d4ffacf9a2643d436f545071f4412954b1
Matteo Melli <matteom@ongres.com>
2020-01-22 14:08:47 +0100

    test: check component versions proerties file has correct values

--- stackgres-k8s/src/operator/src/test/java/io/stackgres/operator/CheckOperatorValuesTest.java
+++ stackgres-k8s/src/operator/src/test/java/io/stackgres/operator/CheckOperatorValuesTest.java
@@ -5,14 +5,20 @@
 
 package io.stackgres.operator;
 
+import java.net.URL;
 import java.nio.file.Paths;
+import java.util.Properties;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 
+import io.stackgres.operator.app.StackGresOperatorApp;
+import io.stackgres.operator.common.StackGresComponents;
 import io.stackgres.operator.common.StackGresUtil;
 
+import org.jooq.lambda.Seq;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -41,6 +47,33 @@ public class CheckOperatorValuesTest {
         operatorConfig.get("containerBuild").asText());
     Assert.assertEquals(StackGresUtil.PROMETHEUS_AUTOBIND,
         operatorConfig.get("prometheus").get("allowAutobind").asText());
+  }
+
+  @Test
+  public void checkComponentVersions() throws Exception {
+    ObjectMapper objectMapper = new YAMLMapper();
+    JsonNode versions = objectMapper.readTree(
+        new URL("https://stackgres.io/downloads/stackgres/components/"
+            + StackGresUtil.CONTAINER_BUILD + "/versions.yaml"));
+    Properties properties = new Properties();
+    properties.load(StackGresOperatorApp.class.getResourceAsStream("/versions.properties"));
+    Assert.assertArrayEquals(
+        Seq.seq((ArrayNode) versions.get("components").get("postgresql").get("versions"))
+        .map(JsonNode::asText)
+        .toArray(),
+        StackGresComponents.getAsArray("postgresql"));
+    Assert.assertEquals(
+        versions.get("components").get("patroni").get("versions").get(0).asText(),
+        StackGresComponents.get("patroni"));
+    Assert.assertEquals(
+        versions.get("components").get("wal_g").get("versions").asText(),
+        StackGresComponents.get("wal_g"));
+    Assert.assertEquals(
+        versions.get("components").get("pgbouncer").get("versions").get(0).asText(),
+        StackGresComponents.get("pgbouncer"));
+    Assert.assertEquals(
+        versions.get("components").get("postgres_exporter").get("versions").get(0).asText(),
+        StackGresComponents.get("postgres_exporter"));
   }
 
 }
