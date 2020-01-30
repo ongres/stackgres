2ef1871ee96c1cd3e3e7e490f5fda348ec3f9ce9
ldgarcia <lgarcia@ongres.com>
2020-01-30 05:08:08 +0100

    feat: hide lists on the main sidebar when there are no inner elements to show

--- stackgres-k8s/src/operator/src/main/resources/META-INF/resources/stackgres/js/components/Side.vue.js
+++ stackgres-k8s/src/operator/src/main/resources/META-INF/resources/stackgres/js/components/Side.vue.js
@@ -18,9 +18,9 @@ var Side = Vue.component("sg-side", {
 			</div>
 
 			<div id="sets">
-				<div class="set clu">
-					<h3>StackGres Clusters</h3>
-					<a href="#" class="addnew">+</a>
+				<div v-if="hasClusters" class="set clu">
+					<h3>Stackgres Clusters</h3>
+					<router-link :to="'/create/cluster/'+currentNamespace" class="addnew">+</router-link>
 
 					<template v-for="cluster in clusters">
 						<template v-if="cluster.data.metadata.namespace == currentNamespace">
@@ -36,39 +36,39 @@ var Side = Vue.component("sg-side", {
 						<router-link :to="'/backups/'+backup.data.metadata.namespace+'/'+backup.name" class="item" :class="backup.name" v-if="backup.data.metadata.namespace == currentNamespace">{{ backup.name }}</router-link>
 					</template>
 				</div>-->
-				<div class="set conf">
+				<div v-if="hasPGConfig || hasPoolConfig || hasBackupConfig" class="set conf">
 					<h3>Configurations</h3>
 
-					<div class="item pg">
+					<div v-if="hasPGConfig" class="item pg">
 						<router-link :to="'/configurations/postgresql/'+currentNamespace"><h4>PostgreSQL</h4></router-link>
-						<a href="#" class="addnew">+</a>
+						<!-- <a href="#" class="addnew">+</a> -->
 
 						<template v-for="config in pgConfig">
 							<router-link :to="'/configurations/postgresql/'+config.data.metadata.namespace+'/'+config.name" class="item" :class="config.name" v-if="config.data.metadata.namespace == currentNamespace">{{ config.name }}</router-link>
 						</template>
 					</div>
 
-					<div class="item pool">
+					<div v-if="hasPoolConfig" class="item pool">
 						<router-link :to="'/configurations/connectionpooling/'+currentNamespace"><h4>Connection Pooling</h4></router-link>
-						<a href="#" class="addnew">+</a>
+						<!-- <a href="#" class="addnew">+</a> -->
 
 						<template v-for="config in poolConfig">
 							<router-link :to="'/configurations/connectionpooling/'+config.data.metadata.namespace+'/'+config.name" class="item" :class="config.name" v-if="config.data.metadata.namespace == currentNamespace">{{ config.name }}</router-link>
 						</template>
 					</div>
 
-					<div class="item backup">
+					<div v-if="hasBackupConfig" class="item backup">
 						<router-link :to="'/configurations/backup/'+currentNamespace"><h4>Backup</h4></router-link>
-						<a href="#" class="addnew">+</a>
+						<!-- <a href="#" class="addnew">+</a> -->
 
 						<template v-for="config in bkConfig">
 							<router-link :to="'/configurations/backup/'+config.data.metadata.namespace+'/'+config.name" class="item" :class="config.name" v-if="config.data.metadata.namespace == currentNamespace">{{ config.name }}</router-link>
 						</template>
 					</div>
 				</div>
-				<div class="set prof">
+				<div v-if="hasProfiles" class="set prof">
 					<h3>Instance Profiles</h3>
-					<a href="#" class="addnew">+</a>
+					<!-- <a href="#" class="addnew">+</a> -->
 
 					<template v-for="profile in profiles">
 						<router-link :to="'/profiles/'+profile.data.metadata.namespace+'/'+profile.name" class="item" :class="profile.name" v-if="profile.data.metadata.namespace == currentNamespace">{{ profile.name }}</router-link>
@@ -119,6 +119,42 @@ var Side = Vue.component("sg-side", {
 
 		profiles () {
 			return store.state.profiles;
+		},
+
+		hasClusters () {
+			let index = store.state.clusters.find(c => (store.state.currentNamespace == c.data.metadata.namespace) );
+
+			if(typeof index !== "undefined")
+				return true
+			else
+				return false
+		},
+
+		hasPoolConfig () {
+			let index = store.state.poolConfig.find(p => (store.state.currentNamespace == p.data.metadata.namespace) );
+
+			if(typeof index !== "undefined")
+				return true
+			else
+				return false
+		},
+
+		hasBackupConfig () {
+			let index = store.state.backupConfig.find(c => (store.state.currentNamespace == c.data.metadata.namespace) );
+
+			if(typeof index !== "undefined")
+				return true
+			else
+				return false
+		},
+
+		hasProfiles () {
+			let index = store.state.profiles.find(p => (store.state.currentNamespace == p.data.metadata.namespace) );
+
+			if(typeof index !== "undefined")
+				return true
+			else
+				return false
 		}
 	}
 })
