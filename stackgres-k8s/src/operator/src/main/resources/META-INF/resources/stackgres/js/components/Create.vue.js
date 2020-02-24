1056fe4c82e9bb41733e8a47c00950b67582325a
ldgarcia <lgarcia@ongres.com>
2020-02-24 03:00:13 +0100

    feat: add "Create" component to enable CRD creation forms

--- stackgres-k8s/src/operator/src/main/resources/META-INF/resources/stackgres/js/components/Create.vue.js
+++ stackgres-k8s/src/operator/src/main/resources/META-INF/resources/stackgres/js/components/Create.vue.js
@@ -1,13 +1,12 @@
-var Create = Vue.component("sg-create", {
+var CRD = Vue.component("sg-crd", {
 	template: `
 		<div id="sg-create">
 			<header>
-				<h2 class="title">Create New {{ $route.params.name }}</h2>
-				<!-- <h3 class="subtitle">Namespace: {{ currentNamespace }} </h3> -->
+				<h2 class="title">{{ $route.params.action }} {{ $route.params.type }}</h2>
 			</header>
 
 			<div class="content">
-				<template v-if="$route.params.name == 'cluster'">
+				<template v-if="($route.params.type == 'cluster')">
 					<create-cluster></create-cluster>
 				</template>
 			</div>
