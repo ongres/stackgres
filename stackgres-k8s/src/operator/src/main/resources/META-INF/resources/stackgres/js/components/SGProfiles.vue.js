f33a87c6e8fe2e1672a801b18532a21ffdf0d601
ldgarcia <lgarcia@ongres.com>
2020-02-24 02:54:42 +0100

    feat: added delete function to Instance Profiles | Related to #242

--- stackgres-k8s/src/operator/src/main/resources/META-INF/resources/stackgres/js/components/SGProfiles.vue.js
+++ stackgres-k8s/src/operator/src/main/resources/META-INF/resources/stackgres/js/components/SGProfiles.vue.js
@@ -8,16 +8,28 @@ var SGProfiles = Vue.component("sg-profile", {
 
 			<div class="content">
 				<div class="profiles boxes">
-					<div v-for="prof in profiles" class="box" v-bind:class="{'show':($route.params.name == prof.name)}" v-if="prof.data.metadata.namespace == currentNamespace">
+					<div v-for="prof in profiles" v-bind:id="prof.name+'-'+prof.data.metadata.namespace" class="box" v-bind:class="[($route.params.name == prof.name) ? 'show' : '']" v-if="prof.data.metadata.namespace == currentNamespace">
 						<h4>{{ prof.name }}</h4>
-						<span>Namespace</span>
-						{{ prof.data.metadata.namespace }}
-						<hr>
-						<span>RAM</span>
-						{{ prof.data.spec.memory }}
-						<hr>
-						<span>CPU</span>
-						{{ prof.data.spec.cpu }}
+						<div class="table no-margin">
+							<div class="head row no-border">
+								<div class="col text">
+									<span>Namespace</span>
+									{{ prof.data.metadata.namespace }}
+								</div>
+								<div class="col">
+									<span>RAM</span>
+									{{ prof.data.spec.memory }}
+								</div>
+								<div class="col">
+									<span>CPU</span>
+									{{ prof.data.spec.cpu }}
+								</div>
+							</div>
+						</div>
+						<div class="form">
+							<router-link :to="'/crd/edit/profile/'+$route.params.namespace+'/'+prof.name" class="btn">Edit Profile</router-link> 
+							<button @click="deleteProfile(prof.name, prof.data.metadata.namespace)" class="border">Delete Profile</button>
+						</div>
 					</div>
 				</div>
 			</div>
@@ -38,5 +50,44 @@ var SGProfiles = Vue.component("sg-profile", {
 			return store.state.currentNamespace
 		},
 
+	},
+	methods: {
+		deleteProfile: function(profName, profNamespace) {
+			//e.preventDefault();
+
+			let confirmDelete = confirm("DELETE ITEM\nAre you sure you want to delete this item?")
+
+			if(confirmDelete) {
+				const profile = {
+					name: profName,
+					namespace: profNamespace
+				}
+	
+				const res = axios
+				.delete(
+					apiURL+'profile/', 
+					{
+						data: {
+							"metadata": {
+								"name": profile.name,
+								"namespace": profile.namespace
+							}
+						}
+					}
+				)
+				.then(function (response) {
+					console.log("DELETED");
+					//console.log(response);
+					notify('Profile <strong>"'+profName+'"</strong> deleted successfully', 'message');
+					$('#'+profName+'-'+profNamespace).addClass("deleted");
+					//router.push('/overview/'+store.state.currentNamespace);
+				})
+				.catch(function (error) {
+					console.log(error.response);
+					notify(error.response.data.message,'error');
+				});
+			}
+
+		}	
 	}
 })
