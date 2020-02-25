c23856651b0603e871790ba3c517a814ac076be9
ldgarcia <lgarcia@ongres.com>
2020-02-25 17:27:08 +0100

    feat: setup new breadcrumbs structure | edit: disable non-editable CRD parameters | fix: include "kind" parameter when creating or editing pooling configs

--- stackgres-k8s/src/operator/src/main/resources/META-INF/resources/stackgres/js/components/forms/CreatePGConfig.vue.js
+++ stackgres-k8s/src/operator/src/main/resources/META-INF/resources/stackgres/js/components/forms/CreatePGConfig.vue.js
@@ -1,38 +1,53 @@
 var CreatePGConfig = Vue.component("create-pgconfig", {
     template: `
-        <div id="create-pgconfig" class="form">
+        <form id="create-pgconfig">
             <header>
-                <h2 class="title">{{ $route.params.action }} PostgreSQL Configuration</h2>
+                <ul class="breadcrumbs">
+                    <li class="namespace">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20.026" height="27"><g fill="#00adb5"><path d="M1.513.9l-1.5 13a.972.972 0 001 1.1h18a.972.972 0 001-1.1l-1.5-13a1.063 1.063 0 00-1-.9h-15a1.063 1.063 0 00-1 .9zm.6 11.5l.9-8c0-.2.3-.4.5-.4h12.9a.458.458 0 01.5.4l.9 8a.56.56 0 01-.5.6h-14.7a.56.56 0 01-.5-.6zM1.113 17.9a1.063 1.063 0 011-.9h15.8a1.063 1.063 0 011 .9.972.972 0 01-1 1.1h-15.8a1.028 1.028 0 01-1-1.1zM3.113 23h13.8a.972.972 0 001-1.1 1.063 1.063 0 00-1-.9h-13.8a1.063 1.063 0 00-1 .9 1.028 1.028 0 001 1.1zM3.113 25.9a1.063 1.063 0 011-.9h11.8a1.063 1.063 0 011 .9.972.972 0 01-1 1.1h-11.8a1.028 1.028 0 01-1-1.1z"/></g></svg>
+                        {{ currentNamespace }}
+                    </li>
+                    <li>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 26.7 20"><path d="M10.946 18.7a.841.841 0 01-.622-.234.862.862 0 01-.234-.635v-7.817a.8.8 0 01.221-.6.834.834 0 01.608-.214h3.29a3.4 3.4 0 012.353.755 2.7 2.7 0 01.843 2.12 2.72 2.72 0 01-.843 2.126 3.379 3.379 0 01-2.353.764h-2.394v2.875a.8.8 0 01-.869.867zM14 13.637q1.778 0 1.778-1.551T14 10.535h-2.18v3.1zm11.968-.107a.683.683 0 01.494.181.625.625 0 01.191.477v2.875a1.717 1.717 0 01-.16.87 1.174 1.174 0 01-.655.414 6.882 6.882 0 01-1.242.294 9.023 9.023 0 01-1.364.107 5.252 5.252 0 01-2.527-.573 3.883 3.883 0 01-1.638-1.665 5.548 5.548 0 01-.569-2.6 5.5 5.5 0 01.569-2.575 3.964 3.964 0 011.611-1.671 4.965 4.965 0 012.455-.59 4.62 4.62 0 013.089 1.016 1.058 1.058 0 01.234.294.854.854 0 01-.087.843.479.479 0 01-.388.2.737.737 0 01-.267-.047 1.5 1.5 0 01-.281-.153 4.232 4.232 0 00-1.1-.582 3.648 3.648 0 00-1.146-.167 2.747 2.747 0 00-2.2.859 3.834 3.834 0 00-.742 2.561q0 3.477 3.049 3.477a6.752 6.752 0 001.815-.254v-2.36h-1.517a.737.737 0 01-.5-.161.664.664 0 010-.909.732.732 0 01.5-.161zM.955 4.762h10.5a.953.953 0 100-1.9H.955a.953.953 0 100 1.9zM14.8 7.619a.954.954 0 00.955-.952V4.762h4.3a.953.953 0 100-1.9h-4.3V.952a.955.955 0 00-1.909 0v5.715a.953.953 0 00.954.952zM.955 10.952h4.3v1.9a.955.955 0 001.909 0V7.143a.955.955 0 00-1.909 0v1.9h-4.3a.953.953 0 100 1.9zm6.681 4.286H.955a.953.953 0 100 1.905h6.681a.953.953 0 100-1.905z" class="a"></path></svg>
+                        {{ $route.params.action }} postgresql configuration
+                    </li>
+                    <li v-if="editMode">
+                        {{ $route.params.name }}
+                    </li>
+                </ul>
             </header>
 
-            <label for="pgConfigNamespace">Configuration Namespace</label>
-            <select v-model="pgConfigNamespace" :disabled="(editMode)">
-                <option disabled value="">Choose a Namespace</option>
-                <option v-for="namespace in allNamespaces">{{ namespace }}</option>
-            </select>
+            <div class="form">
 
-            <label for="pgConfigName">Configuration Name</label>
-            <input v-model="pgConfigName" :disabled="(editMode)">
+                <label for="pgConfigNamespace">Configuration Namespace</label>
+                <select v-model="pgConfigNamespace" :disabled="(editMode)" required>
+                    <option disabled value="">Choose a Namespace</option>
+                    <option v-for="namespace in allNamespaces">{{ namespace }}</option>
+                </select>
 
-            <label for="pgConfigVersion">PostgreSQL Version</label>
-            <select v-model="pgConfigVersion" :disabled="(editMode)">
-                <option disabled value="">Select PostgreSQL Version</option>
-                <option value="11">11</option>
-                <option value="12">12</option>
-            </select>
+                <label for="pgConfigName">Configuration Name</label>
+                <input v-model="pgConfigName" :disabled="(editMode)" required>
 
-            <label for="pgConfigParams">Parameters</label>
-            <textarea v-model="pgConfigParams" placeholder="parameter = value"></textarea>
+                <label for="pgConfigVersion">PostgreSQL Version</label>
+                <select v-model="pgConfigVersion" required>
+                    <option disabled value="">Select PostgreSQL Version</option>
+                    <option value="11">11</option>
+                    <option value="12">12</option>
+                </select>
 
-            <template v-if="editMode">
-                <button @click="updatePGConfig">Update Configuration</button>
-            </template>
-            <template v-else>
-                <button @click="createPGConfig">Create Configuration</button>
-            </template>
-            
-            <button @click="cancel" class="border">Cancel</button>
-		</div>`,
+                <label for="pgConfigParams">Parameters</label>
+                <textarea v-model="pgConfigParams" placeholder="parameter = value"></textarea>
+
+                <template v-if="editMode">
+                    <button @click="createPGConfig">Update Configuration</button>
+                </template>
+                <template v-else>
+                    <button @click="createPGConfig">Create Configuration</button>
+                </template>
+                
+                <button @click="cancel" class="border">Cancel</button>
+            </div>
+        </form>`,
 	data: function() {
 
         if (vm.$route.params.action == 'create') {
@@ -71,78 +86,88 @@ var CreatePGConfig = Vue.component("create-pgconfig", {
 	computed: {
         allNamespaces () {
             return store.state.allNamespaces
+        },
+
+        currentNamespace () {
+            return store.state.currentNamespace
         }
     },
     methods: {
 
         createPGConfig: function(e) {
-            e.preventDefault();
+            //e.preventDefault();
 
-            var config = { 
-                "metadata": {
-                    "name": this.pgConfigName,
-                    "namespace": this.pgConfigNamespace
-                },
-                "spec": {
-                    "pgVersion": this.pgConfigVersion,
-                    "postgresql.conf": getJSON(this.pgConfigParams)
+            let isValid = true;
+            
+            $('input:required, select:required').each(function() {
+                if ($(this).val() === '') {
+                    isValid = false;
+                    return false;
                 }
-            }
-
-            const res = axios
-            .post(
-                apiURL+'pgconfig/', 
-                config 
-            )
-            .then(function (response) {
-                console.log("GOOD");
-                notify('Postgres configuration <strong>"'+config.metadata.name+'"</strong> created successfully', 'message');
-
-                store.commit('updatePGConfig', { 
-                    name: config.metadata.name,
-                    data: config
-                });
-         
-            })
-            .catch(function (error) {
-                console.log(error.response);
-                notify(error.response.data.message,'error');
+                    
             });
 
-        },
+            if(isValid) {
+                var config = { 
+                    "metadata": {
+                        "name": this.pgConfigName,
+                        "namespace": this.pgConfigNamespace
+                    },
+                    "spec": {
+                        "pgVersion": this.pgConfigVersion,
+                        "postgresql.conf": getJSON(this.pgConfigParams)
+                    }
+                }
 
-        updatePGConfig: function(e) {
-            e.preventDefault();
+                console.log(config);
 
-            var config = { 
-                "metadata": {
-                    "name": this.pgConfigName,
-                    "namespace": this.pgConfigNamespace
-                },
-                "spec": {
-                    "pgVersion": this.pgConfigVersion,
-                    "postgresql.conf": getJSON(this.pgConfigParams)
+                if(this.editMode) {
+                    const res = axios
+                    .put(
+                        apiURL+'pgconfig/', 
+                        config 
+                    )
+                    .then(function (response) {
+                        console.log("GOOD");
+                        notify('Postgres configuration <strong>"'+config.metadata.name+'"</strong> updated successfully', 'message');
+
+                        vm.fetchAPI();
+                        router.push('/configurations/postgresql/'+config.metadata.namespace+'/'+config.metadata.name);
+                    })
+                    .catch(function (error) {
+                        console.log(error.response);
+                        notify(error.response.data.message,'error');
+                    });
+                } else {
+                    const res = axios
+                    .post(
+                        apiURL+'pgconfig/', 
+                        config 
+                    )
+                    .then(function (response) {
+                        console.log("GOOD");
+                        notify('Postgres configuration <strong>"'+config.metadata.name+'"</strong> created successfully', 'message');
+        
+                        vm.fetchAPI();
+                        router.push('/configurations/postgresql/'+config.metadata.namespace+'/'+config.metadata.name);
+                        
+                        /* store.commit('updatePGConfig', { 
+                            name: config.metadata.name,
+                            data: config
+                        }); */
+                
+                    })
+                    .catch(function (error) {
+                        console.log(error.response);
+                        notify(error.response.data.message,'error');
+                    });
                 }
             }
-
-            const res = axios
-            .put(
-                apiURL+'pgconfig/', 
-                config 
-            )
-            .then(function (response) {
-                console.log("GOOD");
-                notify('Postgres configuration <strong>"'+config.metadata.name+'"</strong> updated successfully', 'message');
-            })
-            .catch(function (error) {
-                console.log(error.response);
-                notify(error.response.data.message,'error');
-            });
 
         },
 
         cancel: function() {
-            router.push('/overview/'+store.state.currentNamespace);
+            router.push('/configurations/postgresql/'+store.state.currentNamespace);
         },
 
         showFields: function( fields ) {
--- stackgres-k8s/src/operator/src/main/resources/META-INF/resources/stackgres/js/components/forms/CreateProfile.vue.js
+++ stackgres-k8s/src/operator/src/main/resources/META-INF/resources/stackgres/js/components/forms/CreateProfile.vue.js
@@ -1,60 +1,69 @@
 var CreateProfile = Vue.component("create-profile", {
     template: `
-        <div id="create-profile" class="form">
+        <form id="create-profile">
             <header>
-                <h2 class="title">{{ $route.params.action }} Instance Profile</h2>
+                <ul class="breadcrumbs">
+                    <li class="namespace">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20.026" height="27"><g fill="#00adb5"><path d="M1.513.9l-1.5 13a.972.972 0 001 1.1h18a.972.972 0 001-1.1l-1.5-13a1.063 1.063 0 00-1-.9h-15a1.063 1.063 0 00-1 .9zm.6 11.5l.9-8c0-.2.3-.4.5-.4h12.9a.458.458 0 01.5.4l.9 8a.56.56 0 01-.5.6h-14.7a.56.56 0 01-.5-.6zM1.113 17.9a1.063 1.063 0 011-.9h15.8a1.063 1.063 0 011 .9.972.972 0 01-1 1.1h-15.8a1.028 1.028 0 01-1-1.1zM3.113 23h13.8a.972.972 0 001-1.1 1.063 1.063 0 00-1-.9h-13.8a1.063 1.063 0 00-1 .9 1.028 1.028 0 001 1.1zM3.113 25.9a1.063 1.063 0 011-.9h11.8a1.063 1.063 0 011 .9.972.972 0 01-1 1.1h-11.8a1.028 1.028 0 01-1-1.1z"/></g></svg>
+                        {{ currentNamespace }}
+                    </li>
+                    <li>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 26.5 20"><path d="M14.305 18.749a4.7 4.7 0 01-2.388-.589 3.91 3.91 0 01-1.571-1.685 5.668 5.668 0 01-.546-2.568 5.639 5.639 0 01.548-2.561 3.916 3.916 0 011.571-1.678 4.715 4.715 0 012.388-.593 5.189 5.189 0 011.658.261 4.324 4.324 0 011.378.756.758.758 0 01.24.281.859.859 0 01.067.361.768.768 0 01-.16.495.479.479 0 01-.388.2.984.984 0 01-.548-.191 4 4 0 00-1.07-.595 3.405 3.405 0 00-1.1-.167 2.571 2.571 0 00-2.106.869 3.943 3.943 0 00-.72 2.562 3.963 3.963 0 00.716 2.568 2.568 2.568 0 002.106.869 3.147 3.147 0 001.063-.173 5.112 5.112 0 001.1-.589 2.018 2.018 0 01.267-.134.751.751 0 01.29-.048.477.477 0 01.388.2.767.767 0 01.16.494.863.863 0 01-.067.355.739.739 0 01-.24.286 4.308 4.308 0 01-1.378.757 5.161 5.161 0 01-1.658.257zm5.71-.04a.841.841 0 01-.622-.234.856.856 0 01-.234-.636v-7.824a.8.8 0 01.22-.6.835.835 0 01.609-.214h3.29a3.4 3.4 0 012.354.755 2.7 2.7 0 01.842 2.12 2.725 2.725 0 01-.842 2.127 3.386 3.386 0 01-2.354.764h-2.393v2.875a.8.8 0 01-.87.868zm3.05-5.069q1.779 0 1.779-1.552t-1.779-1.551h-2.18v3.1zM.955 4.762h10.5a.953.953 0 100-1.9H.955a.953.953 0 100 1.9zM14.8 7.619a.954.954 0 00.955-.952V4.762h4.3a.953.953 0 100-1.9h-4.3V.952a.955.955 0 00-1.909 0v5.715a.953.953 0 00.954.952zM.955 10.952h4.3v1.9a.955.955 0 001.909 0V7.143a.955.955 0 00-1.909 0v1.9h-4.3a.953.953 0 100 1.9zm6.681 4.286H.955a.953.953 0 100 1.905h6.681a.953.953 0 100-1.905z"></path></svg>
+                        {{ $route.params.action }} instance profile
+                    </li>
+                    <li v-if="editMode">
+                        {{ $route.params.name }}
+                    </li>
+                </ul>
             </header>
-            
-            <label for="profileNamespace">K8S Namespace</label>
-            <select v-model="profileNamespace" :disabled="(editMode)">
-                <option disabled value="">Choose a Namespace</option>
-                <option v-for="namespace in allNamespaces">{{ namespace }}</option>
-            </select>
 
-            <label for="profileName">Profile Name</label>
-            <input v-model="profileName" :disabled="(editMode)">
-
-            
-            <div class="unit-select">
-                <label for="profileRAM">RAM</label>
-                <input v-model="profileRAM" class="size">
-
-                <select v-model="profileRAMUnit" class="unit">
-                    <option disabled value="">Select Unit</option>
-                    <option>Mi</option>
-                    <option>Gi</option>
-                    <option>Ti</option>
-                    <option>Pi</option>
-                    <option>Ei</option>
-                    <option>Zi</option>
-                    <option>Yi</option>        
+            <div class="form">
+                <label for="profileNamespace">K8S Namespace</label>
+                <select v-model="profileNamespace" :disabled="(editMode)" required>
+                    <option disabled value="">Choose a Namespace</option>
+                    <option v-for="namespace in allNamespaces">{{ namespace }}</option>
                 </select>
+
+                <label for="profileName">Profile Name</label>
+                <input v-model="profileName" :disabled="(editMode)" required>
+
+                
+                <div class="unit-select">
+                    <label for="profileRAM">RAM</label>
+                    <input v-model="profileRAM" class="size" required>
+
+                    <select v-model="profileRAMUnit" class="unit" required>
+                        <option disabled value="">Select Unit</option>
+                        <option>Mi</option>
+                        <option>Gi</option>
+                    </select>
+                </div>
+
+                <label for="profileCPU">CPU</label>
+                <select v-model="profileCPU" required>    
+                    <option disabled value="">CPU</option>
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                    <option>6</option>
+                    <option>7</option>
+                    <option>8</option>
+                    <option>9</option>
+                    <option>10</option>
+                </select>
+
+                <template v-if="editMode">
+                    <button @click="createProfile">Update Profile</button>
+                </template>
+                <template v-else>
+                    <button @click="createProfile">Create Profile</button>
+                </template>
+
+                <button @click="cancel" class="border">Cancel</button>
             </div>
-
-            <label for="profileCPU">CPU</label>
-            <select v-model="profileCPU">    
-                <option disabled value="">CPU</option>
-                <option>1</option>
-                <option>2</option>
-                <option>3</option>
-                <option>4</option>
-                <option>5</option>
-                <option>6</option>
-                <option>7</option>
-                <option>8</option>
-                <option>9</option>
-                <option>10</option>
-            </select>
-
-            <template v-if="editMode">
-                <button @click="updateProfile">Update Profile</button>
-            </template>
-            <template v-else>
-                <button @click="createProfile">Create Profile</button>
-            </template>
-
-            <button @click="cancel" class="border">Cancel</button>
-		</div>`,
+        </form>`,
 	data: function() {
 
         if (vm.$route.params.action == 'create') {
@@ -93,73 +102,87 @@ var CreateProfile = Vue.component("create-profile", {
 	computed: {
         allNamespaces () {
             return store.state.allNamespaces
+        },
+
+        currentNamespace () {
+            return store.state.currentNamespace
         }
     },
     methods: {
 
         createProfile: function(e) {
-            e.preventDefault();
+            //e.preventDefault();
 
-            var profile = { 
-                "metadata": {
-                    "name": this.profileName,
-                    "namespace": this.profileNamespace
-                },
-                "spec": {
-                    "cpu": this.profileCPU,
-                    "memory": this.profileRAM+this.profileRAMUnit,
+            let isValid = true;
+            
+            $('input:required, select:required').each(function() {
+                if ($(this).val() === '') {
+                    isValid = false;
+                    return false;
                 }
-            }
-
-            const res = axios
-            .post(
-                apiURL+'profile/', 
-                profile 
-            )
-            .then(function (response) {
-                console.log("GOOD");
-                notify('Profile <strong>"'+profile.metadata.name+'"</strong> created successfully', 'message');
-
-                store.commit('updateProfiles', { 
-                    name: profile.metadata.name,
-                    data: profile
-                });
-
-            })
-            .catch(function (error) {
-                console.log(error.response);
-                notify(error.response.data.message,'error');
+                    
             });
 
-        },
+            if(isValid) {
 
-        updateProfile: function(e) {
-            e.preventDefault();
-
-            var profile = { 
-                "metadata": {
-                    "name": this.profileName,
-                    "namespace": this.profileNamespace
-                },
-                "spec": {
-                    "cpu": this.profileCPU,
-                    "memory": this.profileRAM+this.profileRAMUnit,
+                var profile = { 
+                    "metadata": {
+                        "name": this.profileName,
+                        "namespace": this.profileNamespace
+                    },
+                    "spec": {
+                        "cpu": this.profileCPU,
+                        "memory": this.profileRAM+this.profileRAMUnit,
+                    }
                 }
-            }
 
-            const res = axios
-            .put(
-                apiURL+'profile/', 
-                profile 
-            )
-            .then(function (response) {
-                console.log("GOOD");
-                notify('Profile <strong>"'+profile.metadata.name+'"</strong> updated successfully', 'message');
-            })
-            .catch(function (error) {
-                console.log(error.response);
-                notify(error.response.data.message,'error');
-            });
+                if(this.editMode) {
+                    const res = axios
+                    .put(
+                        apiURL+'profile/', 
+                        profile 
+                    )
+                    .then(function (response) {
+                        console.log("GOOD");
+                        notify('Profile <strong>"'+profile.metadata.name+'"</strong> updated successfully', 'message');
+
+                        vm.fetchAPI();
+                        router.push('/profiles/'+profile.metadata.namespace+'/'+profile.metadata.name);
+
+                    })
+                    .catch(function (error) {
+                        console.log(error.response);
+                        notify(error.response.data.message,'error');
+                    });
+
+                } else {
+                    const res = axios
+                    .post(
+                        apiURL+'profile/', 
+                        profile 
+                    )
+                    .then(function (response) {
+                        console.log("GOOD");
+                        notify('Profile <strong>"'+profile.metadata.name+'"</strong> created successfully', 'message');
+        
+                        /* store.commit('updateProfiles', { 
+                            name: profile.metadata.name,
+                            data: profile
+                        }); */
+
+                        vm.fetchAPI();
+                        router.push('/profiles/'+profile.metadata.namespace+'/'+profile.metadata.name);
+        
+                    })
+                    .catch(function (error) {
+                        console.log(error.response);
+                        notify(error.response.data.message,'error');
+                    });
+
+                }
+    
+                
+            }
 
         },
 
