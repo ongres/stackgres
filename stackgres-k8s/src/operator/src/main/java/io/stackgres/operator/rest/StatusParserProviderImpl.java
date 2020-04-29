8b7aabd531404228a759162289429c988f3f43e3
Matteo Melli <matteom@ongres.com>
2020-04-29 12:40:58 +0200

    fix: allow suffix in minor version parsing

--- stackgres-k8s/src/operator/src/main/java/io/stackgres/operator/rest/StatusParserProviderImpl.java
+++ stackgres-k8s/src/operator/src/main/java/io/stackgres/operator/rest/StatusParserProviderImpl.java
@@ -5,37 +5,47 @@
 
 package io.stackgres.operator.rest;
 
+import java.util.Objects;
 import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
 import io.fabric8.kubernetes.client.KubernetesClient;
+import io.fabric8.kubernetes.client.VersionInfo;
 import io.stackgres.operator.app.KubernetesClientFactory;
 
 @ApplicationScoped
 public class StatusParserProviderImpl implements StatusParserProvider {
 
-  private Integer kubernetesVersion;
+  private static final Pattern VERSION_PATTERN = Pattern.compile("^([0-9]+)[^0-9]*$");
+
+  private StatusParser statusParser;
 
   @PostConstruct
   @Inject
   public void init(KubernetesClientFactory clientFactory) {
     try (KubernetesClient client = clientFactory.create()) {
-
-      kubernetesVersion = Optional.ofNullable(client.getVersion())
-          .map(v -> Integer.parseInt(v.getMinor()))
-          .orElse(16);
+      statusParser = Optional.ofNullable(client.getVersion())
+          .map(VersionInfo::getMinor)
+          .map(VERSION_PATTERN::matcher)
+          .filter(Matcher::find)
+          .map(matcher -> matcher.group(1))
+          .filter(Objects::nonNull)
+          .map(Integer::parseInt)
+          .filter(version -> version < 16)
+          .map(version -> new Kubernetes12StatusParser())
+          .map(StatusParser.class::cast)
+          .orElseGet(() -> new Kubernetes16StatusParser());
     }
   }
 
   @Override
   public StatusParser getStatusParser() {
-    if (kubernetesVersion >= 16) {
-      return new Kubernetes16StatusParser();
-    } else {
-      return new Kubernetes12StatusParser();
-    }
+    return statusParser;
   }
 
 }
