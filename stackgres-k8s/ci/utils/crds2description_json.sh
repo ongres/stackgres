d1b7d19022c874d040716333d11611b8860ebd8d
Álvaro Hernández <aht@ongres.com>
2020-05-08 00:26:08 +0200

    feat: Expose full details of the CRD to the UI
    Previously, we were generating extracts of the `description` fields from
    the CRDs to feed the web UI tooltips. The extracts required a complex
    JSON processing (via `jq`), yet limited.
    
    This change simplifies the process. Generates slightly more complex
    JSONs, but that in turn contain more information from the CRDs that
    might be useful for the UI, like the types of fields and their
    constraints.
    
    Tooltips might be generated from the UI by executing json path style
    extraction or simply object de-reference from these new JSON files.
    
    Once this is integrated into the UI, the older
    *-description-EN.json
    files must be deleted. These files have been updated, in any case, to
    reflect the improved descriptions of some fields.

--- stackgres-k8s/ci/utils/crds2description_json.sh
+++ stackgres-k8s/ci/utils/crds2description_json.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-SCRIPTPATH=$(dirname "$(readlink -f "$0")")
+
+SCRIPT_PATH=$(dirname "$(readlink -f "$0")")
+
 
 message_and_exit() {
 	echo "\n\t$1\n\n"
@@ -12,7 +14,7 @@ usage() {
 }
 
 linter() {
-    yamllint -c ${SCRIPTPATH}/yamllint-config.yaml ${1}
+    yamllint -c ${SCRIPT_PATH}/yamllint-config.yaml ${1}
 }
 
 [ $# -eq 2 ] || usage
@@ -22,40 +24,11 @@ linter() {
 command -v jq > /dev/null || message_and_exit "The program `jq` is required to be in PATH" 8
 command -v yq > /dev/null || message_and_exit "The program `yq` is required to be in PATH" 16
 
-jq_script="/tmp/.tempfile-$RANDOM-$RANDOM"
-
-cat << 'EOF' > $jq_script
-def extract_properties:
-    to_entries
-    | map({
-        key: .key,
-        value: (
-            if .value.type == "object" and .value.properties != null
-            then .value.properties | extract_properties
-            else .value.description
-            end
-        )
-    })
-    | from_entries
-    ;
-
-def flatten_to_dot_notation:
-    [
-        leaf_paths as $path
-        | { "key": $path | join("."), "value": getpath($path) }
-    ]
-    | from_entries
-    ;
-
-.spec .validation .openAPIV3Schema .properties | extract_properties | flatten_to_dot_notation
-EOF
-
 for crd in $1/*.yaml
 do
-    linter ${crd}
+	linter ${crd} || message_and_exit "YAML lint failed for ${crd}" 32
 	crd_name=`yq -r '.spec.names.kind' $crd`
-	output_filename="crd-${crd_name}-description-EN.json"
-	yq '.' $crd | jq -f $jq_script > $2/$output_filename
+	output_filename="crd-${crd_name}-EN.json"
+	yq '.' $crd \
+		| jq '.spec .validation .openAPIV3Schema .properties' > $2/$output_filename
 done
-
-rm -f $jq_script
