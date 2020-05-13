19e43c6f6094a1737eb463348843ddc7b4782dbd
Matteo Melli <matteom@ongres.com>
2020-05-13 10:20:58 +0200

    e2e: allow to execute a subset of all tests

--- stackgres-k8s/e2e/run-all-tests.sh
+++ stackgres-k8s/e2e/run-all-tests.sh
@@ -4,6 +4,7 @@
 
 E2E_PARALLELISM="${E2E_PARALLELISM:-8}"
 E2E_RETRY="${E2E_RETRY:-2}"
+E2E_ONLY_INCLUDES="${E2E_ONLY_INCLUDES}"
 
 echo "Preparing environment"
 
@@ -11,12 +12,17 @@ setup_k8s
 
 echo "Functional tests results" > "$TARGET_PATH/logs/results.log"
 
-SPECS="$(find "$SPEC_PATH" -maxdepth 1 -type f | grep '^.*/[^\.]\+$')"
-
-if [ -d "$SPEC_PATH/$E2E_ENV" ]
+if [ -z "$E2E_ONLY_INCLUDES" ]
 then
-  ENV_SPECS="$(find "$SPEC_PATH/$E2E_ENV" -maxdepth 1 -type f | grep '^.*/[^\.]\+$')"
-  SPECS=$(echo "$SPECS\n$ENV_SPECS")
+  SPECS="$(find "$SPEC_PATH" -maxdepth 1 -type f | grep '^.*/[^\.]\+$')"
+  
+  if [ -d "$SPEC_PATH/$E2E_ENV" ]
+  then
+    ENV_SPECS="$(find "$SPEC_PATH/$E2E_ENV" -maxdepth 1 -type f | grep '^.*/[^\.]\+$')"
+    SPECS=$(echo "$SPECS\n$ENV_SPECS")
+  fi
+else
+  SPECS="$(echo_raw "$E2E_ONLY_INCLUDES" | tr ' ' '\n' | xargs -r -n 1 -I % echo "$SPEC_PATH/%")"
 fi
 
 export K8S_REUSE=true
