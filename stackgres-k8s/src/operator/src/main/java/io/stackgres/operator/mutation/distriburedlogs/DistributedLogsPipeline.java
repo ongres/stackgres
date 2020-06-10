41b9c5abe01e7e255e36d3d11566aabcace6aa3e
Xavier Sierra <xaviersb@ongres.com>
2020-06-10 23:32:45 +0200

    feat: Add stackgres.operatorVersion to sgdistributedlogs CRs

---
+++ stackgres-k8s/src/operator/src/main/java/io/stackgres/operator/mutation/distriburedlogs/DistributedLogsPipeline.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2019 OnGres, Inc.
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+package io.stackgres.operator.mutation.distriburedlogs;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Instance;
+import javax.inject.Inject;
+
+import com.github.fge.jsonpatch.JsonPatchOperation;
+import io.stackgres.operator.common.StackGresDistributedLogsReview;
+import io.stackgres.operatorframework.admissionwebhook.mutating.JsonPatchMutationPipeline;
+
+@ApplicationScoped
+public class DistributedLogsPipeline
+    implements JsonPatchMutationPipeline<StackGresDistributedLogsReview> {
+
+  private final Instance<DistributedLogsMutator> mutators;
+
+  @Inject
+  public DistributedLogsPipeline(Instance<DistributedLogsMutator> mutators) {
+    this.mutators = mutators;
+  }
+
+  @Override
+  public Optional<String> mutate(StackGresDistributedLogsReview review) {
+    List<JsonPatchOperation> operations = new ArrayList<>();
+
+    mutators.forEach(mutator -> operations.addAll(mutator.mutate(review)));
+
+    if (operations.isEmpty()) {
+      return Optional.empty();
+    } else {
+      return Optional.of(join(operations));
+    }
+  }
+}
