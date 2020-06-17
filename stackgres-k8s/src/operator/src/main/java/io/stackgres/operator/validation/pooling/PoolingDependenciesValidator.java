18b87f86b77b59e89934552466efcc4e83ffd7dd
Matteo Melli <matteom@ongres.com>
2020-06-17 13:08:54 +0200

    fix: fixed dependencies validations

--- stackgres-k8s/src/operator/src/main/java/io/stackgres/operator/validation/pooling/PoolingDependenciesValidator.java
+++ stackgres-k8s/src/operator/src/main/java/io/stackgres/operator/validation/pooling/PoolingDependenciesValidator.java
@@ -5,6 +5,8 @@
 
 package io.stackgres.operator.validation.pooling;
 
+import java.util.Objects;
+
 import javax.enterprise.context.ApplicationScoped;
 
 import io.stackgres.common.ErrorType;
@@ -13,22 +15,16 @@ import io.stackgres.operator.common.PoolingReview;
 import io.stackgres.operator.validation.DependenciesValidator;
 import io.stackgres.operator.validation.ValidationType;
 import io.stackgres.operatorframework.admissionwebhook.validating.ValidationFailed;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @ApplicationScoped
 @ValidationType(ErrorType.FORBIDDEN_CR_DELETION)
 public class PoolingDependenciesValidator extends DependenciesValidator<PoolingReview>
     implements PoolingValidator {
 
-  private static final Logger LOGGER = LoggerFactory
-      .getLogger(PoolingDependenciesValidator.class);
-
   @Override
   public void validate(PoolingReview review, StackGresCluster i) throws ValidationFailed {
-    final String name = review.getRequest().getName();
-    LOGGER.info("validating deletion of " + name);
-    if (name.equals(i.getSpec().getConfiguration().getConnectionPoolingConfig())) {
+    if (Objects.equals(i.getSpec().getConfiguration().getConnectionPoolingConfig(),
+        review.getRequest().getName())) {
       fail(review, i);
     }
   }
