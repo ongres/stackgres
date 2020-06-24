cd8534838932ab488226ebbc5b685e7e5d21dd85
Jorge Solórzano <jorsol@ongres.com>
2020-06-24 18:32:44 +0200

    fix: Filter operator namespace in the REST API

--- stackgres-k8s/src/api-web/src/test/java/io/stackgres/apiweb/rest/NamespaceResourceTest.java
+++ stackgres-k8s/src/api-web/src/test/java/io/stackgres/apiweb/rest/NamespaceResourceTest.java
@@ -5,13 +5,21 @@
 
 package io.stackgres.apiweb.rest;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.hasItems;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.not;
 import static org.mockito.Mockito.when;
 
 import java.util.List;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.fabric8.kubernetes.api.model.Namespace;
 import io.fabric8.kubernetes.api.model.NamespaceList;
+import io.stackgres.apiweb.config.WebApiContext;
 import io.stackgres.common.resource.ResourceScanner;
 import io.stackgres.testutil.JsonUtil;
 import org.junit.jupiter.api.BeforeEach;
@@ -35,19 +43,38 @@ class NamespaceResourceTest {
     namespaces = JsonUtil
         .readFromJson("namespace/list.json", NamespaceList.class);
 
-    resource = new NamespaceResource();
-    resource.setNamespaceScanner(scanner);
+    resource = new NamespaceResource(scanner, new WebApiContext());
   }
 
   @Test
   void getShouldReturnAllNamespacesNames() {
+    final List<Namespace> listNamespaces = namespaces.getItems();
+    when(scanner.findResources()).thenReturn(listNamespaces);
+
+    final List<String> namespaces = resource.get();
+
+    assertThat(listNamespaces, hasSize(7));
+    assertThat(namespaces, hasSize(3));
+
+    assertThat(namespaces, contains("default", "odoo", "pgconf-staging"));
+  }
+
+  @Test
+  void getShouldNotReturnStackGresNamespace() {
     when(scanner.findResources()).thenReturn(namespaces.getItems());
 
-    List<String> namespaces = resource.get();
+    final List<String> namespaces = resource.get();
 
-    assertEquals(1, namespaces.size());
-
-    assertEquals("default", namespaces.get(0));
-
+    assertThat(namespaces, not(hasItem("stackgres")));
   }
+
+  @Test
+  void getShouldNotReturnKubeNamespacesNames() {
+    when(scanner.findResources()).thenReturn(namespaces.getItems());
+
+    final List<String> namespaces = resource.get();
+
+    assertThat(namespaces, not(hasItems("kube-public", "kube-node-lease", "kube-system")));
+  }
+
 }
