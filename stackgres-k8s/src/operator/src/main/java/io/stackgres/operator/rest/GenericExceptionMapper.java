0d28c4772215e2e58d611891f0fcebf1ce453adc
Matteo Melli <matteom@ongres.com>
2020-07-02 18:52:35 +0200

    feat: re-added generic, reader and writer exception mapper

--- stackgres-k8s/src/operator/src/main/java/io/stackgres/operator/rest/GenericExceptionMapper.java
+++ stackgres-k8s/src/operator/src/main/java/io/stackgres/operator/rest/GenericExceptionMapper.java
@@ -5,33 +5,9 @@
 
 package io.stackgres.operator.rest;
 
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 
-import com.google.common.base.Throwables;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 @Provider
-public class GenericExceptionMapper implements ExceptionMapper<Throwable> {
+public class GenericExceptionMapper extends AbstractGenericExceptionMapper<Throwable> {
 
-  private static final Logger LOGGER = LoggerFactory.getLogger(
-      GenericExceptionMapper.class);
-
-  @Override
-  public Response toResponse(Throwable throwable) {
-    Throwable cause = Throwables.getRootCause(throwable);
-
-    String message = cause.getMessage();
-
-    int statusCode = Response.Status.INTERNAL_SERVER_ERROR.getStatusCode();
-
-    LOGGER.error("An error occurred in the operator web api", throwable);
-
-    return Response.status(statusCode).type(MediaType.APPLICATION_JSON)
-        .entity(message)
-        .build();
-  }
 }
