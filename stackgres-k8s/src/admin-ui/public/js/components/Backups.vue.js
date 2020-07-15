32fa1e537cb3fac10d3e91226a8e0aec0e0e6e92
ldgarcia <lgarcia@ongres.com>
2020-07-15 12:22:59 +0000

    fix: backups on cluster views point to general backup section rather than cluster backups view

--- stackgres-k8s/src/admin-ui/public/js/components/Backups.vue.js
+++ stackgres-k8s/src/admin-ui/public/js/components/Backups.vue.js
@@ -786,9 +786,6 @@ var Backups = Vue.component("Backups", {
 					else
 						router.push('/admin/backups/'+store.state.currentNamespace)
 				}
-
-				/* $(this).parent().next().toggle().addClass("open");
-				$(this).parent().toggleClass("open"); */
 			});
 
 		});
