858226149e78d38c98a83bc140de067a37de6131
Jorge Solórzano <jorsol@ongres.com>
2020-07-16 18:21:46 +0200

    feat: Add new endpoint to list permissions for the UI

--- stackgres-k8s/src/api-web/src/main/java/io/stackgres/apiweb/rest/RbacResource.java
+++ stackgres-k8s/src/api-web/src/main/java/io/stackgres/apiweb/rest/RbacResource.java
@@ -5,6 +5,10 @@
 
 package io.stackgres.apiweb.rest;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import javax.enterprise.context.RequestScoped;
@@ -19,10 +23,12 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
+import com.google.common.collect.ImmutableList;
 import io.fabric8.kubernetes.api.model.authorization.SubjectAccessReview;
 import io.fabric8.kubernetes.api.model.authorization.SubjectAccessReviewBuilder;
 import io.fabric8.kubernetes.client.DefaultKubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClient;
+import io.stackgres.apiweb.dto.PermissionsListDto;
 import io.stackgres.common.StackGresProperty;
 import org.eclipse.microprofile.jwt.Claim;
 import org.eclipse.microprofile.jwt.Claims;
@@ -41,11 +47,20 @@ public class RbacResource {
   @Claim(standard = Claims.sub)
   String user;
 
+  NamespaceResource namespaces;
+
+  @Inject
+  public RbacResource(NamespaceResource namespaces) {
+    super();
+    this.namespaces = namespaces;
+  }
+
   @GET
   @Path("/can-i/{verb}/{resource}")
   public Response verb(@PathParam("verb") String verb, @PathParam("resource") String resource,
       @QueryParam("namespace") String namespace, @QueryParam("group") Optional<String> group) {
     LOGGER.debug("User to review access {}", user);
+    // Connect with the serviceaccount permissions
     try (KubernetesClient client = new DefaultKubernetesClient()) {
 
       SubjectAccessReview review = new SubjectAccessReviewBuilder()
@@ -72,6 +87,77 @@ public class RbacResource {
       }
 
     }
+  }
+
+  @GET
+  @Produces(MediaType.APPLICATION_JSON)
+  @Path("/can-i")
+  public Response caniList() {
+    // Connect with the serviceaccount permissions
+    try (KubernetesClient client = new DefaultKubernetesClient()) {
+
+      List<String> verbs = ImmutableList.of("get", "list", "create", "patch", "delete");
+      List<String> resourcesNamespaced = ImmutableList.of("pods", "secrets",
+          "sgbackupconfigs.stackgres.io", "sgbackups.stackgres.io", "sgclusters.stackgres.io",
+          "sgdistributedlogs.stackgres.io", "sginstanceprofiles.stackgres.io",
+          "sgpgconfigs.stackgres.io", "sgpoolconfigs.stackgres.io");
+      List<String> resourcesUnnamespaced =
+          ImmutableList.of("namespaces", "storageclasses.storage.k8s.io");
+
+      PermissionsListDto permissionsList = new PermissionsListDto();
+      List<PermissionsListDto.Namespaced> listNamespaced = new ArrayList<>();
+      for (String ns : namespaces.get()) {
+        PermissionsListDto.Namespaced permisionsNamespaced = new PermissionsListDto.Namespaced();
+        Map<String, List<String>> resourceNamespace = new HashMap<>();
+        for (String rsNamespaced : resourcesNamespaced) {
+          String[] resource = rsNamespaced.split("\\.", 2);
+          List<String> allowed = accessReview(client, user, ns, resource[0],
+              resource.length == 2 ? resource[1] : "", verbs);
+          resourceNamespace.put(resource[0], allowed);
+        }
+        permisionsNamespaced.setNamespace(ns);
+        permisionsNamespaced.setResources(resourceNamespace);
+        listNamespaced.add(permisionsNamespaced);
+      }
+
+      Map<String, List<String>> resourceUnamespace = new HashMap<>();
+      for (String rsUnnamespaced : resourcesUnnamespaced) {
+        String[] resource = rsUnnamespaced.split("\\.", 2);
+        List<String> allowed = accessReview(client, user, null, resource[0],
+            resource.length == 2 ? resource[1] : "", verbs);
+        resourceUnamespace.put(resource[0], allowed);
+      }
+
+      permissionsList.setNamespaced(listNamespaced);
+      permissionsList.setUnnamespaced(resourceUnamespace);
+
+      return Response.ok(permissionsList).build();
+    }
+  }
+
+  private List<String> accessReview(KubernetesClient client, String user, String namespace,
+      String resource, String group, List<String> verbs) {
+    List<String> allowed = new ArrayList<>();
+    for (String verb : verbs) {
+      SubjectAccessReview review = new SubjectAccessReviewBuilder()
+          .withNewSpec()
+          .withUser(user)
+          .withNewResourceAttributes()
+          .withNamespace(namespace)
+          .withResource(resource)
+          .withGroup(group)
+          .withVerb(verb)
+          .endResourceAttributes()
+          .endSpec()
+          .build();
+      review = client.subjectAccessReviewAuth()
+          .create(review);
+
+      if (Boolean.TRUE.equals(review.getStatus().getAllowed())) {
+        allowed.add(verb);
+      }
+    }
+    return allowed;
   }
 
 }
