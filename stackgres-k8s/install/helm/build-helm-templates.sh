e46818ff35349cc99866e51d12db6d53e051e6f3
emanuel.calvo <emanuel@ongres.com>
2020-07-29 14:04:51 -0300

    Added demo yamls tests in operator e2e specs
    Fixes around demo genaration (loadbalancer)
    
      - Test operator from demo yamls
      - Added LoadBalancer type to template build
      - Testing from generated yamls

--- stackgres-k8s/install/helm/build-helm-templates.sh
+++ stackgres-k8s/install/helm/build-helm-templates.sh
@@ -7,8 +7,8 @@ cd "$(dirname "$0")"
 STACKGRES_VERSION=$(grep '<artifactId>stackgres-parent</artifactId>' "../../src/pom.xml" -A 2 -B 2 \
  | grep -o '<version>\([^<]\+\)</version>' | tr '<>' '  ' | cut -d ' ' -f 3)
 
-mkdir -p "target/public/downloads/stackgres-k8s/stackgres/$STACKGRES_VERSION"
-cat << EOF > "target/public/downloads/stackgres-k8s/stackgres/$STACKGRES_VERSION/demo-operator.yml"
+mkdir -p "target/templates"
+cat << EOF > "target/templates/demo-operator.yml"
 apiVersion: v1
 kind: Namespace
 metadata:
@@ -17,8 +17,8 @@ metadata:
 EOF
 for CRD in stackgres-operator/crds/*.yaml
 do
-  cat "$CRD" >> "target/public/downloads/stackgres-k8s/stackgres/$STACKGRES_VERSION/demo-operator.yml"
-  echo --- >> "target/public/downloads/stackgres-k8s/stackgres/$STACKGRES_VERSION/demo-operator.yml"
+  cat "$CRD" >> "target/templates/demo-operator.yml"
+  echo --- >> "target/templates/demo-operator.yml"
 done
 helm repo add stable https://kubernetes-charts.storage.googleapis.com
 helm repo update
@@ -28,20 +28,26 @@ helm dependency update stackgres-cluster
 helm template --namespace stackgres stackgres-operator \
   stackgres-operator \
   --set-string adminui.service.type=LoadBalancer \
-  >> "target/public/downloads/stackgres-k8s/stackgres/$STACKGRES_VERSION/demo-operator.yml"
+  >> "target/templates/demo-operator.yml"
 
 helm template --namespace default simple \
   stackgres-cluster \
   --set nonProductionOptions.createMinio=false \
-  > "target/public/downloads/stackgres-k8s/stackgres/$STACKGRES_VERSION/demo-simple-config.yml"
+  --set configurations.create=true \
+  --set cluster.create=false \
+  > "target/templates/demo-simple-config.yml"
 
 helm template --namespace default simple \
   stackgres-cluster \
-  --set config.create=false \
+  --set configurations.create=false \
+  --set cluster.create=true \
   --set profiles=null \
+  --set cluster.sgInstanceProfile=size-xs \
   --set cluster.instances=2 \
+  --set instanceProfiles=null \
   --set nonProductionOptions.createMinio=false \
-  > "target/public/downloads/stackgres-k8s/stackgres/$STACKGRES_VERSION/demo-simple-cluster.yml"
+  --set nonProductionOptions.disableClusterPodAntiAffinity=true \
+  > "target/templates/demo-simple-cluster.yml"
 
 rm -rf target/minio
 helm fetch stable/minio \
@@ -50,5 +56,8 @@ helm fetch stable/minio \
 helm template minio \
   target/minio \
   --set buckets[0].name=stackgres,buckets[0].policy=none,buckets[0].purge=true \
-  > "target/public/downloads/stackgres-k8s/stackgres/$STACKGRES_VERSION/demo-minio.yml"
+  > "target/templates/demo-minio.yml"
 
+mkdir -p "target/public/downloads/stackgres-k8s/stackgres"
+rm -rf "target/public/downloads/stackgres-k8s/stackgres/$STACKGRES_VERSION"
+cp -a target/templates "target/public/downloads/stackgres-k8s/stackgres/$STACKGRES_VERSION"
