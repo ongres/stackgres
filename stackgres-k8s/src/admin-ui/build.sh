b978787d40d946249bedb7fa0429718e2ee43bb2
Matteo Melli <matteom@ongres.com>
2020-08-05 11:25:42 +0200

    ci: added adminui build job and scripts

---
+++ stackgres-k8s/src/admin-ui/build.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -e
+
+SHELL="$(readlink /proc/$$/exe)"
+if [ "$(basename "$SHELL")" = busybox ]
+then
+  SHELL=sh
+fi
+SHELL_XTRACE="$(! echo $- | grep -q x || echo "-x")"
+
+cd "$(dirname "$0")"
+mkdir -p target
+rm -rf target/public
+cp -a public target/public
+mkdir -p target/public/js/components/forms/help
+"$SHELL" $SHELL_XTRACE ../../ci/utils/crds2description_json.sh ../../install/helm/stackgres-operator/crds target/public/js/components/forms/help
