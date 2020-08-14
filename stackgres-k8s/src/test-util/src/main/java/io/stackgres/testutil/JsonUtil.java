89317ce2ce0886cd7cace7a5a5cc93fbcb8c789c
Jorge Solórzano <jorsol@ongres.com>
2020-08-14 21:07:30 +0200

    chore: Exclude false-positive spotbugs

--- stackgres-k8s/src/test-util/src/main/java/io/stackgres/testutil/JsonUtil.java
+++ stackgres-k8s/src/test-util/src/main/java/io/stackgres/testutil/JsonUtil.java
@@ -7,28 +7,36 @@ package io.stackgres.testutil;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.nio.charset.StandardCharsets;
+import java.util.Objects;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.io.CharStreams;
 import io.quarkus.runtime.annotations.RegisterForReflection;
-import org.apache.commons.io.IOUtils;
 
 public class JsonUtil {
 
   private static final ObjectMapper jsonMapper = new ObjectMapper();
 
+  private JsonUtil() {}
+
   public static <T> T readFromJson(String resource, Class<T> clazz) {
+    Objects.requireNonNull(resource, "resource");
+    Objects.requireNonNull(clazz, "clazz");
     if (clazz.getPackage().getName().startsWith("io.stackgres")
         && clazz.getAnnotation(RegisterForReflection.class) == null) {
-      throw new NullPointerException("class " + clazz.getName() + " must have the annotation: "
+      throw new IllegalStateException("class " + clazz.getName() + " must have the annotation: "
           + RegisterForReflection.class);
     }
     try (InputStream is = ClassLoader.getSystemResourceAsStream(resource)) {
       if (is == null) {
         throw new IllegalArgumentException("resource " + resource + " not found");
       }
-      String json = IOUtils.toString(is, StandardCharsets.UTF_8);
-      return jsonMapper.readValue(json, clazz);
+      try (Reader reader = new InputStreamReader(is, StandardCharsets.UTF_8)) {
+        return jsonMapper.readValue(CharStreams.toString(reader), clazz);
+      }
     } catch (IOException e) {
       throw new IllegalArgumentException("could not open resource " + resource, e);
     }
