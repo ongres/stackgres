5f016346030bc0746c3cbe7d7d1e1de078074759
ldgarcia <lgarcia@ongres.com>
2021-01-13 18:26:16 +0100

    fix: enable macOS compatibility for docs build script

--- doc/build.sh
+++ doc/build.sh
@@ -5,11 +5,14 @@ set -e
 rm -Rf "$(dirname "$0")/data/crds"
 mkdir -p "$(dirname "$0")/data/crds"
 find "$(dirname "$0")/../stackgres-k8s/src/jobs/src/main/resources/crds" -name '*.yaml' \
-  | tr '\n' '\0' | xargs -0 -r -n 1 -I % cp % "$(dirname "$0")/data/crds"
+  | while read -r FILE
+    do
+      cp "$FILE" "$(dirname "$0")/data/crds"
+    done
 
 STACKGRES_VERSION="${STACKGRES_VERSION:-$(
   grep '<artifactId>stackgres-parent</artifactId>' "$(dirname "$0")/../stackgres-k8s/src/pom.xml" -A 2 -B 2 \
-    | grep -oP '(?<=<version>).*?(?=</version>)')}"
+    | grep -o '<version>\([^<]\+\)</version>' | tr '<>' '  ' | cut -d ' ' -f 3)}"
 echo "current_version: \"$STACKGRES_VERSION\"" > "$(dirname "$0")/data/versions.yml"
 
 cp "$(dirname "$0")/../stackgres-k8s/src/api-web/target/swagger-merged.yaml" "$(dirname "$0")/themes/sg-doc/static/sg-swagger.yaml"
