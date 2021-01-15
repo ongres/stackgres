80fb53ad3920c07e32856afb5191c4ce1bcd5f6a
Matteo Melli <matteom@ongres.com>
2021-01-16 00:05:34 +0100

    e2e: fix operator demo

--- stackgres-k8s/ci/utils/update-version.sh
+++ stackgres-k8s/ci/utils/update-version.sh
@@ -12,14 +12,14 @@ usage() {
 }
 
 yq_update_file() {
-  [ $# -ge 2 ]
+  [ "$#" -ge 2 ]
   local INDEX=0
   local OFFSET
-  while [ $# -gt 2 ]
+  while [ "$#" -gt 2 ]
   do
-    OFFSET=$(tail -n +"$((INDEX+1))" "$(eval "echo \$$#")" | grep -n "$1")
-    OFFSET=${OFFSET%%:*}
-    INDEX=$((INDEX+OFFSET))
+    OFFSET="$(tail -n +"$((INDEX+1))" "$(eval "echo \$$#")" | grep -n "$1")"
+    OFFSET="${OFFSET%%:*}"
+    INDEX="$((INDEX+OFFSET))"
     shift
   done
   [ "$INDEX" -ge 0 ]
@@ -35,7 +35,7 @@ EOF
   rm "$2.bak"
 }
 
-[ $# -ge 1 ] || usage
+[ "$#" -ge 1 ] || usage
 
 command -v jq > /dev/null || message_and_exit 'The program `jq` is required to be in PATH' 8
 command -v yq > /dev/null || message_and_exit 'The program `yq` (https://kislyuk.github.io/yq/) is required to be in PATH' 16
@@ -60,6 +60,7 @@ cd stackgres-k8s/src
 yq_update_file "^operator:$" "^    tag:.*$" "    tag: \"$IMAGE_TAG\"" stackgres-k8s/install/helm/stackgres-operator/values.yaml
 yq_update_file "^restapi:$" "^    tag:.*$" "    tag: \"$IMAGE_TAG\"" stackgres-k8s/install/helm/stackgres-operator/values.yaml
 yq_update_file "^adminui:$" "^    tag:.*$" "    tag: \"$ADMINUI_IMAGE_TAG\"" stackgres-k8s/install/helm/stackgres-operator/values.yaml
+yq_update_file "^jobs:$" "^    tag:.*$" "    tag: \"$IMAGE_TAG\"" stackgres-k8s/install/helm/stackgres-operator/values.yaml
 
 yq_update_file "^version: .*$" "version: \"$VERSION\"" stackgres-k8s/install/helm/stackgres-operator/Chart.yaml
 yq_update_file "^appVersion: .*$" "appVersion: \"$VERSION\"" stackgres-k8s/install/helm/stackgres-operator/Chart.yaml
