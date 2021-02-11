20afd02baa9a1f86fcc7eb088a43fb676216525d
ldgarcia <lgarcia@ongres.com>
2021-02-11 16:52:54 +0100

    refactor: set unique names for every router path

--- stackgres-k8s/src/admin-ui/src/router/index.js
+++ stackgres-k8s/src/admin-ui/src/router/index.js
@@ -4,7 +4,7 @@ import VueRouter from 'vue-router';
 // Form Components
 import CreateCluster from '../components/forms/CreateCluster.vue'
 import CreateProfile from '../components/forms/CreateProfile.vue'
-import CreatePGConfig from '../components/forms/CreatePGConfig.vue'
+import CreatePgConfig from '../components/forms/CreatePgConfig.vue'
 import CreatePoolConfig from '../components/forms/CreatePoolConfig.vue'
 import CreateBackupConfig from '../components/forms/CreateBackupConfig.vue'
 import CreateBackup from '../components/forms/CreateBackup.vue'
@@ -19,7 +19,7 @@ import Backups from '../components/Backups.vue'
 import PgConfig from '../components/PgConfig.vue'
 import PoolConfig from '../components/PoolConfig.vue'
 import BackupConfig from '../components/BackupConfig.vue'
-import SGProfiles from '../components/SGProfile.vue'
+import InstanceProfile from '../components/InstanceProfile.vue'
 import LogsServer from '../components/LogsServer.vue'
 import Grafana from '../components/Grafana.vue'
 import NotFound from '../components/NotFound.vue'
@@ -29,112 +29,135 @@ Vue.use(VueRouter);
 
 const routes = [
   { 
-    path: '/admin/crd/:action/cluster/:namespace', 
+    path: '/crd/:action/cluster/:namespace', 
     component: CreateCluster,
+    name: 'CreateCluster',
     meta: {
       conditionalRoute: false
     },
   },
   { 
-    path: '/admin/crd/:action/cluster/:namespace/:name', 
+    path: '/crd/:action/cluster/:namespace/:name', 
     component: CreateCluster,
+    name: 'EditCluster',
     meta: {
       conditionalRoute: false
     },
   },
   { 
-    path: '/admin/crd/:action/profile/:namespace', 
+    path: '/crd/:action/profile/:namespace', 
     component: CreateProfile,
+    name: 'CreateProfile',
     meta: {
       conditionalRoute: false
     },
   },
   { 
-    path: '/admin/crd/:action/profile/:namespace/:name', 
+    path: '/crd/:action/profile/:namespace/:name', 
     component: CreateProfile,
+    name: 'EditProfile',
     meta: {
       conditionalRoute: false
     },
   },
   { 
-    path: '/admin/crd/:action/pgconfig/:namespace', 
-    component: CreatePGConfig,
+    path: '/crd/:action/pgconfig/:namespace', 
+    component: CreatePgConfig,
+    name: 'CreatePgConfig',
     meta: {
       conditionalRoute: false
     },
   },
   { 
-    path: '/admin/crd/:action/pgconfig/:namespace/:name', 
-    component: CreatePGConfig,
+    path: '/crd/:action/pgconfig/:namespace/:name', 
+    component: CreatePgConfig,
+    name: 'EditPgConfig',
     meta: {
       conditionalRoute: false
     },
   },
   { 
-    path: '/admin/crd/:action/connectionpooling/:namespace', 
+    path: '/crd/:action/connectionpooling/:namespace', 
     component: CreatePoolConfig,
+    name: 'CreatePoolConfig',
     meta: {
       conditionalRoute: false
     },
   },
   { 
-    path: '/admin/crd/:action/connectionpooling/:namespace/:name', 
+    path: '/crd/:action/connectionpooling/:namespace/:name', 
     component: CreatePoolConfig,
+    name: 'EditPoolConfig',
     meta: {
       conditionalRoute: false
     },
   },
   { 
-    path: '/admin/crd/:action/backupconfig/:namespace', 
+    path: '/crd/:action/backupconfig/:namespace', 
     component: CreateBackupConfig,
+    name: 'CreateBackupConfig',
     meta: {
       conditionalRoute: false
     },
   },
   { 
-    path: '/admin/crd/:action/backupconfig/:namespace/:name', 
+    path: '/crd/:action/backupconfig/:namespace/:name', 
     component: CreateBackupConfig,
+    name: 'EditBackupConfig',
     meta: {
       conditionalRoute: false
     },
   },
   { 
-    path: '/admin/crd/:action/backup/:namespace', 
+    path: '/crd/:action/backup/:namespace', 
     component: CreateBackup,
+    name: 'CreateBackups',
     meta: {
       conditionalRoute: false
     },
   },
   { 
-    path: '/admin/crd/create/backup/:namespace/:cluster', 
+    path: '/crd/create/backup/:namespace/:cluster', 
     component: CreateBackup,
+    name: 'CreateClusterBackup',
     meta: {
       conditionalRoute: false
     },
   },
   { 
-    path: '/admin/crd/:action/backup/:namespace/:uid', 
+    path: '/crd/:action/backup/:namespace/:uid', 
     component: CreateBackup,
+    name: 'EditBackup',
     meta: {
       conditionalRoute: false
     },
   },
   { 
-    path: '/admin/crd/:action/logs/:namespace', 
+    path: '/crd/:action/logs/:namespace', 
     component: CreateLogsServer,
+    name: 'CreateLogsServer',
     meta: {
       conditionalRoute: false
     },
   },
   { 
-    path: '/admin/crd/:action/logs/:namespace/:name', 
+    path: '/crd/:action/logs/:namespace/:name', 
     component: CreateLogsServer,
+    name: 'EditLogsServer',
     meta: {
       conditionalRoute: false
     },
   },
   { 
-    path: '', 
+    path: '/', 
+    component: ClusterOverview,
+    name: 'BaseUrl',
+    meta: {
+      conditionalRoute: false
+    },
+  },
+  { 
+    path: '/overview', 
     component: ClusterOverview,
     name: 'ClusterOverviewEmpty',
     meta: {
@@ -142,162 +165,169 @@ const routes = [
     },
   },
   { 
-    path: '/admin', 
+    path: '/overview/:namespace', 
     component: ClusterOverview,
+    name: 'ClusterOverview',
     meta: {
       conditionalRoute: false
     },
   },
   { 
-    path: '/admin/overview/:namespace', 
-    component: ClusterOverview,
-    meta: {
-      conditionalRoute: false
-    },
-  },
-  { 
-    path: '/admin/:cluster/configuration/:namespace/:name', 
+    path: '/:cluster/configuration/:namespace/:name', 
     component: ClusterInfo,
+    name: 'ClusterInfo',
     meta: {
       conditionalRoute: false
     },
   },
   { 
-    path: '/admin/:cluster/status/:namespace/:name', 
+    path: '/:cluster/status/:namespace/:name', 
     component: ClusterStatus,
+    name: 'ClusterStatus',
     meta: {
       conditionalRoute: false
     },
   },
   { 
-    path: '/admin/:cluster/logs/:namespace/:name', 
+    path: '/:cluster/logs/:namespace/:name', 
     component: Logs,
+    name: 'Logs',
     meta: {
       conditionalRoute: false
     },
   },
   { 
-    path: '/admin/:cluster/logs/:namespace/:name/:time/:index', 
+    path: '/:cluster/logs/:namespace/:name/:time/:index', 
     component: Logs,
+    name: 'SingleLogs',
     meta: {
       conditionalRoute: false
     },
   },
   {  
-    path: '/admin/backups/:namespace/', 
+    path: '/backups/:namespace/', 
     component: Backups,
+    name: 'NamespaceBackups',
     meta: {
       conditionalRoute: false
     },
   },
   { 
-    path: '/admin/backups/:namespace/:name/:uid', 
+    path: '/backups/:namespace/:name/:uid', 
     component: Backups,
+    name: 'SingleBackups',
     meta: {
       conditionalRoute: false
     },
   },
   { 
-    path: '/admin/:cluster/backups/:namespace/:name', 
+    path: '/:cluster/backups/:namespace/:name', 
     component: Backups,
+    name: 'ClusterBackups',
     meta: {
       conditionalRoute: false
     },
   },
   { 
-    path: '/admin/:cluster/backups/:namespace/:name/:uid', 
+    path: '/:cluster/backups/:namespace/:name/:uid', 
     component: Backups,
+    name: 'SingleClusterBackups',
     meta: {
       conditionalRoute: false
     },
   },
   { 
-    path: '/admin/configurations/postgres/:namespace', 
+    path: '/configurations/postgres/:namespace', 
     component: PgConfig,
+    name: 'PgConfig',
     meta: {
       conditionalRoute: false
     },
   },
   { 
-    path: '/admin/configurations/postgres/:namespace/:name', 
+    path: '/configurations/postgres/:namespace/:name', 
     component: PgConfig,
+    name: 'SinglePgConfig',
     meta: {
       conditionalRoute: false
     },
   },
   { 
-    path: '/admin/configurations/connectionpooling/:namespace', 
+    path: '/configurations/connectionpooling/:namespace', 
     component: PoolConfig,
+    name: 'PoolConfig',
     meta: {
       conditionalRoute: false
     },
   },
   { 
-    path: '/admin/configurations/connectionpooling/:namespace/:name', 
+    path: '/configurations/connectionpooling/:namespace/:name', 
     component: PoolConfig,
+    name: 'SinglePoolConfig',
     meta: {
       conditionalRoute: false
     },
   },
   { 
-    path: '/admin/configurations/backup/:namespace', 
+    path: '/configurations/backup/:namespace', 
     component: BackupConfig,
+    name: 'BackupConfig',
     meta: {
       conditionalRoute: false
     },
   },
   { 
-    path: '/admin/configurations/backup/:namespace/:name', 
+    path: '/configurations/backup/:namespace/:name', 
     component: BackupConfig,
+    name: 'SingleBackupConfig',
     meta: {
       conditionalRoute: false
     },
   },
   {  
-    path: '/admin/profiles/:namespace/', 
-    component: SGProfiles,
+    path: '/profiles/:namespace/', 
+    component: InstanceProfile,
+    name: 'InstanceProfile',
     meta: {
       conditionalRoute: false
     },
   },
   { 
-    path: '/admin/profiles/:namespace/:name', 
-    component: SGProfiles,
+    path: '/profiles/:namespace/:name', 
+    component: InstanceProfile,
+    name: 'SingleInstanceProfile',
     meta: {
       conditionalRoute: false
     },
   },
   { 
-    path: '/admin/logs/:namespace', 
+    path: '/logs/:namespace', 
     component: LogsServer,
+    name: 'LogsServer',
     meta: {
       conditionalRoute: false
     },
   },
   { 
-    path: '/admin/logs/:namespace/:name', 
+    path: '/logs/:namespace/:name', 
     component: LogsServer,
+    name: 'SingleLogsServer',
     meta: {
       conditionalRoute: false
     },
   },
   { 
-    path: '/admin/monitor/', 
+    path: '/:cluster/monitor/:namespace/:name', 
     component: Grafana,
+    name: 'ClusterMonitor',
     meta: {
       conditionalRoute: false
     },
   },
   { 
-    path: '/admin/:cluster/monitor/:namespace/:name', 
+    path: '/:cluster/monitor/:namespace/:name/:pod', 
     component: Grafana,
-    meta: {
-      conditionalRoute: false
-    },
-  },
-  { 
-    path: '/admin/:cluster/monitor/:namespace/:name/:pod', 
-    component: Grafana,
+    name: 'ClusterSingleMonitor',
     meta: {
       conditionalRoute: false
     },
