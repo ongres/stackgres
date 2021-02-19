aaa86e30badd2e2b7268dc40372ff99bab1b6c2e
ldgarcia <lgarcia@ongres.com>
2021-02-19 14:10:24 +0100

    refactor: make sure table records details point to the right location

--- stackgres-k8s/src/admin-ui/src/components/InstanceProfile.vue
+++ stackgres-k8s/src/admin-ui/src/components/InstanceProfile.vue
@@ -155,7 +155,7 @@
 			});
 
 			// Show configurations details when the row is clicked
-			$(document).on('click', 'table tr.base > td:not(.actions)', function(){    
+			$(document).on('click', 'table.profiles tr.base > td:not(.actions)', function(){    
 				const table = $(this).parents('table');
 				if(!$(this).parent().hasClass('open')) {
 					if(table.hasClass('configurations'))
