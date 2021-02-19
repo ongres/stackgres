c684bc29e03093e55a03fa82fa0581d77198243e
ldgarcia <lgarcia@ongres.com>
2021-02-19 18:33:28 +0100

    fix: show NotFound message only if logged in

--- stackgres-k8s/src/admin-ui/src/components/NotFound.vue
+++ stackgres-k8s/src/admin-ui/src/components/NotFound.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <div id="notFound">
+        <div id="notFound" v-if="loggedIn">
             <h1>Not Found</h1>
             <p>The resource you're looking for doesn't exist</p>
             <router-link to="/overview/default" class="btn">Go to Default Dashboard</router-link>
@@ -9,9 +9,18 @@
 </template>
 
 <script>
+    import store from '../store'
 
     export default {
-        name: 'NotFound'
+        name: 'NotFound',
+
+        computed: {
+
+            loggedIn () {
+                return store.state.loginToken.length
+            } 
+
+        }
     }
 </script>
 
