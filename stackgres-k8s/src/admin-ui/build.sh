f88bebda3335aaa79f808beca29df6d3d1563f53
Matteo Melli <matteom@ongres.com>
2021-02-19 16:21:14 +0100

    chore: new admin-ui build

--- stackgres-k8s/src/admin-ui/build.sh
+++ stackgres-k8s/src/admin-ui/build.sh
@@ -12,10 +12,9 @@ SHELL_XTRACE="$(! echo $- | grep -q x || echo "-x")"
 cd "$(dirname "$0")"
 mkdir -p target
 rm -rf target/public
-cp -a public target/public
+cp -a dist target/public
 
 mkdir -p target/public/info
-"$SHELL" $SHELL_XTRACE ../api-web/src/main/swagger/build.sh
 cp ../api-web/target/swagger-merged.json target/public/info/sg-tooltips.json
 
 
