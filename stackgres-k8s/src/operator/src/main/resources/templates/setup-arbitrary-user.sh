552a05fb7624502fe921bc1c07b0d504c146073b
Matteo Melli <matteom@ongres.com>
2021-03-26 17:21:36 +0100

    fix: set rw permissions for generated shadow and gshadow

--- stackgres-k8s/src/operator/src/main/resources/templates/setup-arbitrary-user.sh
+++ stackgres-k8s/src/operator/src/main/resources/templates/setup-arbitrary-user.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
-USER=postgres
-UID="$(id -u)"
-GID="$(id -g)"
-SHELL=/bin/sh
+USER_NAME=postgres
+USER_ID="$(id -u)"
+GROUP_ID="$(id -g)"
+USER_SHELL=/bin/sh
 cp "$TEMPLATES_PATH/passwd" /local/etc/.
 cp "$TEMPLATES_PATH/group" /local/etc/.
 cp "$TEMPLATES_PATH/shadow" /local/etc/.
 cp "$TEMPLATES_PATH/gshadow" /local/etc/.
-echo "$USER:x:$UID:$GID::$PG_BASE_PATH:$SHELL" >> /local/etc/passwd
+echo "$USER_NAME:x:$USER_ID:$GROUP_ID::$PG_BASE_PATH:$USER_SHELL" >> /local/etc/passwd
 chmod 644 /local/etc/passwd
-echo "$USER:x:$GID:" >> /local/etc/group
+echo "$USER_NAME:x:$GROUP_ID:" >> /local/etc/group
 chmod 644 /local/etc/group
-echo "$USER"':!!:18179:0:99999:7:::' >> /local/etc/shadow
-chmod 000 /local/etc/shadow
-echo "$USER"':!::' >> /local/etc/gshadow
-chmod 000 /local/etc/gshadow
+echo "$USER_NAME"':!!:18179:0:99999:7:::' >> /local/etc/shadow
+chmod 600 /local/etc/shadow
+echo "$USER_NAME"':!::' >> /local/etc/gshadow
+chmod 600 /local/etc/gshadow
