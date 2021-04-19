ea1a7268fc0c2b536315cf555054ab4c3f35ad12
Matteo Melli <matteom@ongres.com>
2021-04-19 13:11:56 +0200

    chore: use junit5 assertions

--- stackgres-k8s/src/operator/src/test/java/io/stackgres/operator/conversion/ConversionUtilTest.java
+++ stackgres-k8s/src/operator/src/test/java/io/stackgres/operator/conversion/ConversionUtilTest.java
@@ -5,8 +5,8 @@
 
 package io.stackgres.operator.conversion;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
