84d06c59be474e5733eb333f77d2d9879d83cf26
Matteo Melli <matteom@ongres.com>
2021-04-19 13:23:36 +0200

    doc: documented apiVersionAsNumber methods

--- stackgres-k8s/src/operator/src/main/java/io/stackgres/operator/conversion/ConversionUtil.java
+++ stackgres-k8s/src/operator/src/main/java/io/stackgres/operator/conversion/ConversionUtil.java
@@ -26,10 +26,52 @@ public interface ConversionUtil {
   String API_VERSION_1 = API_VERSION_PREFIX + "1";
   long VERSION_1 = apiVersionAsNumber(API_VERSION_1);
 
+  /**
+   * Extract `.apiVersion` field from a Kubernetes resource object and
+   *  extract the API version as a number (see {@link #apiVersionAsNumber(String)}).
+   */
   static long apiVersionAsNumberOf(ObjectNode node) {
     return apiVersionAsNumber(node.get("apiVersion").asText());
   }
 
+  /**
+   * Extract the StackGres API version and convert it to a number that allow
+   * to obtain an absolute ordering among all versions.
+   * 
+   * The conversion algorithm uses following format:
+   * 
+   * <pre>
+   *                       000 000 000
+   *   major version      ---|   |   |
+   *   suffix version     -------|   |
+   *   sub suffix version -----------|
+   * </pre>
+   * 
+   * For example `stackgres.io/v1beta1` will be converted to
+   * 
+   * <pre>
+   *                       001 001 001
+   *   major version      ---|   |   |
+   *   suffix version     -------|   |
+   *   sub suffix version -----------|
+   * </pre>
+   * 
+   * and `stackgres.io/v1` will be converted to
+   * 
+   * <pre>
+   *                       001 002 000
+   *   major version      ---|   |   |
+   *   suffix version     -------|   |
+   *   sub suffix version -----------|
+   * </pre>
+   * 
+   * so that, for example, following statements will result to `true`:
+   * 
+   * <pre>
+   *   apiVersionAsNumber("stackgres.io/v1") > apiVersionAsNumber("stackgres.io/v1beta1");
+   *   apiVersionAsNumber("stackgres.io/v1beta1") < apiVersionAsNumber("stackgres.io/v1");
+   * </pre>
+   */
   static long apiVersionAsNumber(String apiVersion) {
     if (!apiVersion.startsWith(API_VERSION_PREFIX)) {
       throw new IllegalArgumentException(
