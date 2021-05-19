3194c0480bc32a12cd36413fb95e94a25d5d9e14
ldgarcia <lgarcia@ongres.com>
2021-05-19 12:47:19 +0200

    fix(ui): show warning message on cluster connection info when no primary pod is available

--- stackgres-k8s/src/admin-ui/src/components/ClusterInfo.vue
+++ stackgres-k8s/src/admin-ui/src/components/ClusterInfo.vue
@@ -49,37 +49,49 @@
 
 			<div class="content">
 				<h2>Cluster Details</h2>
-				<div class="connectionInfo" v-if="hasProp(cluster, 'data.info')">
-					<a @click="setContentTooltip('#connectionInfo')"> 
+				<div class="connectionInfo" v-if="hasProp(cluster, 'data.info')" :set="hasPrimary = ( typeof ( cluster.data.pods.find(p => (p.role == 'primary')) ) == 'undefined' )">
+					<a @click="setContentTooltip('#connectionInfo', hasPrimary)"> 
 						<h2>View Connection Info</h2>
 						<svg xmlns="http://www.w3.org/2000/svg" width="18.556" height="14.004" viewBox="0 0 18.556 14.004"><g transform="translate(0 -126.766)"><path d="M18.459,133.353c-.134-.269-3.359-6.587-9.18-6.587S.232,133.084.1,133.353a.93.93,0,0,0,0,.831c.135.269,3.36,6.586,9.18,6.586s9.046-6.317,9.18-6.586A.93.93,0,0,0,18.459,133.353Zm-9.18,5.558c-3.9,0-6.516-3.851-7.284-5.142.767-1.293,3.382-5.143,7.284-5.143s6.516,3.85,7.284,5.143C15.795,135.06,13.18,138.911,9.278,138.911Z" transform="translate(0 0)"/><path d="M9.751,130.857a3.206,3.206,0,1,0,3.207,3.207A3.21,3.21,0,0,0,9.751,130.857Z" transform="translate(-0.472 -0.295)"/></g></svg>
 					</a>
 
 					<div id="connectionInfo" class="hidden">
 						<div class="connInfo">
-							<p>To access StackGres cluster <code>{{ $route.params.namespace !== 'default' ? $route.params.namespace+'.'+cluster.name : cluster.name }}</code> you can address one or both of the following DNS entries:
-								<ul>
-									<li>Read Write DNS: <code>{{ cluster.data.info.primaryDns }}</code> </li>
-									<li>Read Only DNS: <code>{{ cluster.data.info.replicasDns }}</code> </li>
-								</ul>
-							</p>	
+							<div class="textCenter" v-if="( typeof ( cluster.data.pods.find(p => (p.role == 'primary')) ) == 'undefined' )">
+								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 18"><g transform="translate(0 -183)"><path d="M18.994,201H1.006a1,1,0,0,1-.871-.516,1.052,1.052,0,0,1,0-1.031l8.993-15.974a1.033,1.033,0,0,1,1.744,0l8.993,15.974a1.052,1.052,0,0,1,0,1.031A1,1,0,0,1,18.994,201ZM2.75,198.937h14.5L10,186.059Z"/><rect width="2" height="5.378" rx="0.947" transform="translate(9 189.059)"/><rect width="2" height="2" rx="1" transform="translate(9 195.437)"/></g></svg>
+								<h4>Attention</h4>
+								<p>
+									No pods are available yet for this cluster.<br/>
+									You won't be able to connect to it until there's at least one active pod.
+								</p>
+							</div>
+							<template v-else>
+								<p>To access StackGres cluster <code>{{ $route.params.namespace !== 'default' ? $route.params.namespace+'.'+cluster.name : cluster.name }}</code> you can address one or both of the following DNS entries:
+									<ul>
+										<li>Read Write DNS: <code>{{ cluster.data.info.primaryDns }}</code> </li>
+										<li>Read Only DNS: <code>{{ cluster.data.info.replicasDns }}</code> </li>
+									</ul>
+								</p>	
 
-							<p>You may connect with Postgres client <code>psql</code> in two different ways:
-								<ul>
-									<li>
-										Local <code>psql</code> (runs within the same pod as Postgres):<br/>
-										<template v-for="pod in cluster.data.pods"><pre v-if="pod.role == 'primary'">kubectl {{ $route.params.namespace !== 'default' ? '-n '+$route.params.namespace : '' }}exec -ti {{ pod.name }} -c postgres-util -- psql {{ cluster.data.info.superuserUsername !== 'postgres' ? '-U '+cluster.data.info.superuserUsername : '' }}  <span class="copyClipboard" data-tooltip="Copied!" title="Copy to clipboard"></span></pre></template>
-									</li>
-									<li>
-										Externally to StackGres pods, from a container image that contains <code>psql</code> (this option is the only one available if you have disabled the <code>postgres-util</code> sidecar):<br/>
-										<pre>kubectl {{ $route.params.namespace !== 'default' ? '-n '+$route.params.namespace : '' }}run psql --rm -it --image ongres/postgres-util:v13.2-build-6.2 --restart=Never -- psql -h {{ cluster.name }}-primary {{ cluster.data.info.superuserUsername }} {{ cluster.data.info.superuserUsername }}  <span class="copyClipboard" data-tooltip="Copied!" title="Copy to clipboard"></span></pre>
-									</li>
-								</ul>
-							</p>
+								<p>You may connect with Postgres client <code>psql</code> in two different ways:
+									<ul>
+										<li>
+											Local <code>psql</code> (runs within the same pod as Postgres):<br/>
+											<template v-for="pod in cluster.data.pods">
+												<pre v-if="pod.role == 'primary'">kubectl {{ $route.params.namespace !== 'default' ? '-n '+$route.params.namespace : '' }} exec -ti {{ pod.name }} -c postgres-util -- psql{{ cluster.data.info.superuserUsername !== 'postgres' ? ' -U '+cluster.data.info.superuserUsername : '' }}<span class="copyClipboard" data-tooltip="Copied!" title="Copy to clipboard"></span></pre>
+											</template>
+										</li>
+										<li>
+											Externally to StackGres pods, from a container image that contains <code>psql</code> (this option is the only one available if you have disabled the <code>postgres-util</code> sidecar):<br/>
+											<pre>kubectl {{ $route.params.namespace !== 'default' ? '-n '+$route.params.namespace : '' }} run psql --rm -it --image ongres/postgres-util:v13.2-build-6.2 --restart=Never -- psql -h {{ cluster.name }}-primary {{ cluster.data.info.superuserUsername }} {{ cluster.data.info.superuserUsername }}  <span class="copyClipboard" data-tooltip="Copied!" title="Copy to clipboard"></span></pre>
+										</li>
+									</ul>
+								</p>
 
-							<p>The command will ask for the admin user password (prompt may not be shown, just type or paste the password). Use the following command to retrieve it:<br/>
-								<pre>kubectl {{ $route.params.namespace !== 'default' ? '-n '+$route.params.namespace : '' }}get secret {{ cluster.data.info.superuserSecretName }} --template <template v-pre>'{{</template> printf "%s" (index .data "{{ cluster.data.info.superuserPasswordKey }}" | base64decode) }}'  <span class="copyClipboard" data-tooltip="Copied!" title="Copy to clipboard"></span> </pre>
-							</p>
+								<p>The command will ask for the admin user password (prompt may not be shown, just type or paste the password). Use the following command to retrieve it:<br/>
+									<pre>kubectl {{ $route.params.namespace !== 'default' ? '-n '+$route.params.namespace : '' }}get secret {{ cluster.data.info.superuserSecretName }} --template <template v-pre>'{{</template> printf "%s" (index .data "{{ cluster.data.info.superuserPasswordKey }}" | base64decode) }}'<span class="copyClipboard" data-tooltip="Copied!" title="Copy to clipboard"></span></pre>
+								</p>
+							</template>
 						</div>
 					</div>
 				</div>
