37a831701e1ab8bebe0a833ee9d5ba2f609fb322
ldgarcia <lgarcia@ongres.com>
2021-05-26 12:13:09 +0200

    fix(ui): improve cluster status message when stats are being loaded

--- stackgres-k8s/src/admin-ui/public/js/components/ClusterStatus.vue.js
+++ stackgres-k8s/src/admin-ui/public/js/components/ClusterStatus.vue.js
@@ -48,97 +48,97 @@ var ClusterStatus = Vue.component("ClusterStatus", {
 				</ul>
 			</header>
 
-			<template v-if="cluster.status.hasOwnProperty('pods') && cluster.status.pods.length">
-				<div class="content">
-					<h2>Cluster</h2>
-					<table class="clusterInfo">
-						<thead>
-							<th>Total CPU</th>
-							<th>Total Memory</th>
-							<th>Primary Node Disk</th>
-							<th>Total Allocated Disk</th>
-							<th>Instances</th>
-						</thead>
-						<tbody>
-							<tr>
-								<td v-if="cluster.status.hasOwnProperty('cpuPsiAvg60')">
-									{{ cluster.status.cpuRequested }} (avg. load {{ cluster.status.cpuPsiAvg60 }})
-								</td>
-								<td v-else>
-									{{ cluster.status.cpuRequested }} <template v-if="cluster.status.hasOwnProperty('averageLoad1m')">(avg. load {{ cluster.status.averageLoad1m }})</template>
-								</td>
-								<td v-if="cluster.status.hasOwnProperty('memoryPsiAvg60')">
-									{{ cluster.status.memoryPsiAvg60 }}
-								</td>
-								<td>
-									{{ cluster.status.memoryRequested }}
-								</td>
-								<td class="flex-center">
-									<template v-if="cluster.status.hasOwnProperty('pods') && (typeof (cluster.status.pods.find(p => (p.role == 'primary'))) !== 'undefined')">
-										<template v-for="pod in cluster.status.pods" v-if="pod.role == 'primary'">
-												<div class="donut">
-												<svg class="loader" xmlns="http://www.w3.org/2000/svg" version="1.1">
-													<circle cx="12.5" cy="12.5" r="10" stroke-width="5" fill="none" :stroke-dasharray="diskUsed+',63'" />
-												</svg>
-												<svg class="background" xmlns="http://www.w3.org/2000/svg" version="1.1">
-													<circle cx="12.5" cy="12.5" r="10" stroke-width="5" fill="none" />
-												</svg>
-											</div>
-											{{ pod.diskUsed }} / {{ pod.diskRequested }}
-										</template>
+			
+			<div class="content" v-if="hasProp(cluster, 'status.pods') && cluster.status.pods.length">
+				<h2>Cluster</h2>
+				<table class="clusterInfo">
+					<thead>
+						<th>Total CPU</th>
+						<th>Total Memory</th>
+						<th>Primary Node Disk</th>
+						<th>Total Allocated Disk</th>
+						<th>Instances</th>
+					</thead>
+					<tbody>
+						<tr>
+							<td v-if="cluster.status.hasOwnProperty('cpuPsiAvg60')">
+								{{ cluster.status.cpuRequested }} (avg. load {{ cluster.status.cpuPsiAvg60 }})
+							</td>
+							<td v-else>
+								{{ cluster.status.cpuRequested }} <template v-if="cluster.status.hasOwnProperty('averageLoad1m')">(avg. load {{ cluster.status.averageLoad1m }})</template>
+							</td>
+							<td v-if="cluster.status.hasOwnProperty('memoryPsiAvg60')">
+								{{ cluster.status.memoryPsiAvg60 }}
+							</td>
+							<td>
+								{{ cluster.status.memoryRequested }}
+							</td>
+							<td class="flex-center">
+								<template v-if="cluster.status.hasOwnProperty('pods') && (typeof (cluster.status.pods.find(p => (p.role == 'primary'))) !== 'undefined')">
+									<template v-for="pod in cluster.status.pods" v-if="pod.role == 'primary'">
+											<div class="donut">
+											<svg class="loader" xmlns="http://www.w3.org/2000/svg" version="1.1">
+												<circle cx="12.5" cy="12.5" r="10" stroke-width="5" fill="none" :stroke-dasharray="diskUsed+',63'" />
+											</svg>
+											<svg class="background" xmlns="http://www.w3.org/2000/svg" version="1.1">
+												<circle cx="12.5" cy="12.5" r="10" stroke-width="5" fill="none" />
+											</svg>
+										</div>
+										{{ pod.diskUsed }} / {{ pod.diskRequested }}
 									</template>
-									<template v-else>
-										-
-									</template>
-								</td>
-								<td>{{ cluster.status.hasOwnProperty('diskRequested') ? cluster.status.diskRequested : '-' }}</td>
-								<td>{{ cluster.data.podsReady }} / {{ cluster.data.spec.instances }}</td>
-							</tr>
-						</tbody>
-					</table>
+								</template>
+								<template v-else>
+									-
+								</template>
+							</td>
+							<td>{{ cluster.status.hasOwnProperty('diskRequested') ? cluster.status.diskRequested : '-' }}</td>
+							<td>{{ cluster.data.podsReady }} / {{ cluster.data.spec.instances }}</td>
+						</tr>
+					</tbody>
+				</table>
 
-					<h2>Pods</h2>
-					<table class="podStatus">
-						<thead>
-							<th>Pod Name</th>
-							<th>Role</th>
-							<th>Status</th>
-							<th>CPU</th>
-							<th>Memory</th>
-							<th>Disk</th>
-							<th>Containers</th>
-						</thead>
-						<tbody>
-							<tr v-for="pod in cluster.status.pods">
-								<td>{{ pod.name }}</td>
-								<td class="label" :class="pod.role"><span>{{ pod.role }}</span></td>
-								<td class="label" :class="pod.status"><span>{{ pod.status }}</span></td>
-								<td v-if="pod.hasOwnProperty('cpuPsiAvg60')">
-									{{ pod.cpuRequested }} (avg. load {{ pod.cpuPsiAvg60 }})
-								</td>
-								<td v-else>
-									{{ pod.cpuRequested }} <template v-if="pod.hasOwnProperty('averageLoad1m')">(avg. load {{ pod.averageLoad1m }})</template>
-								</td>
-								<td v-if="pod.hasOwnProperty('memoryPsiAvg60')">
-									{{ pod.memoryPsiAvg60 }}
-								</td>
-								<td>
-									{{ pod.memoryRequested }}
-								</td>
-								<td>
-								<template v-if="pod.hasOwnProperty('diskUsed')">{{ pod.diskUsed }}</template><template v-else>-</template> / {{ pod.diskRequested }} <span v-if="pod.hasOwnProperty('diskPsiAvg60')">(psi avg. {{ pod.diskPsiAvg60 }})</span>
-								</td>
-								<td>{{ pod.containersReady }} / {{ pod.containers }}</td>
-							</tr>
-						</tbody>
-					</table>
-				</div>
-			</template>
-			<template v-else>
-				<div class="no-data">
-					No pods yet available
-				</div>
-			</template>
+				<h2>Pods</h2>
+				<table class="podStatus">
+					<thead>
+						<th>Pod Name</th>
+						<th>Role</th>
+						<th>Status</th>
+						<th>CPU</th>
+						<th>Memory</th>
+						<th>Disk</th>
+						<th>Containers</th>
+					</thead>
+					<tbody>
+						<tr v-for="pod in cluster.status.pods">
+							<td>{{ pod.name }}</td>
+							<td class="label" :class="pod.role"><span>{{ pod.role }}</span></td>
+							<td class="label" :class="pod.status"><span>{{ pod.status }}</span></td>
+							<td v-if="pod.hasOwnProperty('cpuPsiAvg60')">
+								{{ pod.cpuRequested }} (avg. load {{ pod.cpuPsiAvg60 }})
+							</td>
+							<td v-else>
+								{{ pod.cpuRequested }} <template v-if="pod.hasOwnProperty('averageLoad1m')">(avg. load {{ pod.averageLoad1m }})</template>
+							</td>
+							<td v-if="pod.hasOwnProperty('memoryPsiAvg60')">
+								{{ pod.memoryPsiAvg60 }}
+							</td>
+							<td>
+								{{ pod.memoryRequested }}
+							</td>
+							<td>
+							<template v-if="pod.hasOwnProperty('diskUsed')">{{ pod.diskUsed }}</template><template v-else>-</template> / {{ pod.diskRequested }} <span v-if="pod.hasOwnProperty('diskPsiAvg60')">(psi avg. {{ pod.diskPsiAvg60 }})</span>
+							</td>
+							<td>{{ pod.containersReady }} / {{ pod.containers }}</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+			<div class="no-data" v-else-if="hasProp(cluster, 'status.pods') && !cluster.status.pods.length">
+				No pods yet available
+			</div>
+			<div class="no-data" v-else>
+				Loading cluster status...
+			</div>
 		</template>
 		</div>`,
 	data: function() {
