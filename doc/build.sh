95b42bb153b27960a542bd8b6d24a8e33fdec5cb
Jorge Solórzano <jorsol@ongres.com>
2021-06-03 19:05:20 +0200

    ci: Documentation only pipeline
    Signed-off-by: Jorge Solórzano <jorsol@ongres.com>

--- doc/build.sh
+++ doc/build.sh
@@ -2,20 +2,10 @@
 
 set -e
 
-rm -Rf "$(dirname "$0")/data/crds"
-mkdir -p "$(dirname "$0")/data/crds"
-find "$(dirname "$0")/../stackgres-k8s/src/common/src/main/resources/crds" -name '*.yaml' \
-  | while read -r FILE
-    do
-      cp "$FILE" "$(dirname "$0")/data/crds"
-    done
+CURRENT_DIR="$(dirname "$(realpath "$0")")"
+PROJECT_DIR="$(dirname "$CURRENT_DIR")"
 
-STACKGRES_VERSION="${STACKGRES_VERSION:-$(
-  grep '<artifactId>stackgres-parent</artifactId>' "$(dirname "$0")/../stackgres-k8s/src/pom.xml" -A 2 -B 2 \
-    | grep -o '<version>\([^<]\+\)</version>' | tr '<>' '  ' | cut -d ' ' -f 3)}"
-echo "current_version: \"$STACKGRES_VERSION\"" > "$(dirname "$0")/data/versions.yml"
-
-if [ ! -f "$(dirname "$0")/../stackgres-k8s/src/api-web/target/swagger-merged.yaml" ]
+if [ ! -f "${PROJECT_DIR}/stackgres-k8s/src/api-web/target/swagger-merged.yaml" ]
 then
   echo "Please build Stackgres operator and swagger first:"
   echo
@@ -25,4 +15,17 @@ then
   echo
   exit 1
 fi
-cp "$(dirname "$0")/../stackgres-k8s/src/api-web/target/swagger-merged.yaml" "$(dirname "$0")/themes/sg-doc/static/sg-swagger.yaml"
+cp -v "${PROJECT_DIR}/stackgres-k8s/src/api-web/target/swagger-merged.yaml" "${CURRENT_DIR}/themes/sg-doc/static/sg-swagger.yaml"
+
+rm -Rf "${CURRENT_DIR}/data/crds" && mkdir -p "${CURRENT_DIR}/data/crds"
+find "${PROJECT_DIR}/stackgres-k8s/src/common/src/main/resources/crds" -name '*.yaml' \
+  | while read -r FILE
+    do
+      cp -v "$FILE" "${CURRENT_DIR}/data/crds"
+    done
+
+STACKGRES_VERSION="${STACKGRES_VERSION:-$(
+  grep '<artifactId>stackgres-parent</artifactId>' "${PROJECT_DIR}/stackgres-k8s/src/pom.xml" -C 2 --color=never \
+    | grep --color=never -o '<version>\([^<]\+\)</version>' | tr '<>' '  ' | cut -d ' ' -f 3)}"
+
+echo "current_version: \"$STACKGRES_VERSION\"" > "${CURRENT_DIR}/data/versions.yml"
--- stackgres-k8s/src/admin-ui/build.sh
+++ stackgres-k8s/src/admin-ui/build.sh
@@ -3,23 +3,35 @@
 set -e
 
 SHELL="$(readlink /proc/$$/exe)"
-if [ "$(basename "$SHELL")" = busybox ]
-then
+if [ "$(basename "$SHELL")" = "busybox" ]; then
   SHELL=sh
 fi
 SHELL_XTRACE="$(! echo $- | grep -q x || echo "-x")"
 
-cd "$(dirname "$0")"
+CURRENT_DIR="$(dirname "$(realpath "$0")")"
+cd "$CURRENT_DIR"
 
-mkdir -p target
-rm -rf target/public
-cp -a dist target/public
+mkdir -p $CURRENT_DIR/target
+rm -rf $CURRENT_DIR/target/public
+cp -av $CURRENT_DIR/dist $CURRENT_DIR/target/public
 
-mkdir -p target/public/info
-cp ../api-web/target/swagger-merged.json target/public/info/sg-tooltips.json
+if [ ! -f "${CURRENT_DIR}/../api-web/target/swagger-merged.json" ]
+then
+  echo "Please build Stackgres operator and swagger first:"
+  echo
+  echo "cd stackgres-k8s/src"
+  echo "./mvnw clean package -DskipTests"
+  echo "sh api-web/src/main/swagger/build.sh"
+  echo
+  exit 1
+fi
 
-mkdir -p target/public/info
+mkdir -p $CURRENT_DIR/target/public/info
+cp -v $CURRENT_DIR/../api-web/target/swagger-merged.json $CURRENT_DIR/target/public/info/sg-tooltips.json
+
 # Export SG version to show on the UI
-grep '<artifactId>stackgres-parent</artifactId>' "../pom.xml" -A 2 -B 2 \
- | grep -oP '(?<=<version>).*?(?=</version>)' \
+mkdir -p $CURRENT_DIR/target/public/info
+
+grep '<artifactId>stackgres-parent</artifactId>' "../pom.xml" -C 2 --color=never \
+ | grep --color=never -oP '(?<=<version>).*?(?=</version>)' \
  | xargs -I % echo '{"version":"%"}' > target/public/info/sg-info.json
