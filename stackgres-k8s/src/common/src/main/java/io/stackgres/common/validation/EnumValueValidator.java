3e00223d9868aeb703623ff7a8098a0dec0e2c24
Jorge Solórzano <jorsol@ongres.com>
2021-06-14 18:11:02 +0200

    fix: Validate storageClass for s3 SGBackupConfig
    Signed-off-by: Jorge Solórzano <jorsol@ongres.com>

---
+++ stackgres-k8s/src/common/src/main/java/io/stackgres/common/validation/EnumValueValidator.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2019 OnGres, Inc.
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+package io.stackgres.common.validation;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class EnumValueValidator implements ConstraintValidator<ValidEnum, String> {
+  private ValidEnum annotation;
+
+  @Override
+  public void initialize(ValidEnum annotation) {
+    this.annotation = annotation;
+  }
+
+  @Override
+  public boolean isValid(String valueForValidation,
+      ConstraintValidatorContext constraintValidatorContext) {
+    if (valueForValidation == null && this.annotation.allowNulls()) {
+      return true;
+    }
+
+    Object[] enumValues = this.annotation.enumClass().getEnumConstants();
+    if (enumValues != null && valueForValidation != null) {
+      for (Object enumValue : enumValues) {
+        if (valueForValidation.equals(enumValue.toString())
+            || (this.annotation.ignoreCase()
+                && valueForValidation.equalsIgnoreCase(enumValue.toString()))) {
+          return true;
+        }
+      }
+    }
+
+    return false;
+  }
+}
--- stackgres-k8s/src/operator/src/test/java/io/stackgres/operator/validation/backupconfig/BackupConfigConstraintValidationTest.java
+++ stackgres-k8s/src/operator/src/test/java/io/stackgres/operator/validation/backupconfig/BackupConfigConstraintValidationTest.java
@@ -5,6 +5,10 @@
 
 package io.stackgres.operator.validation.backupconfig;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import java.util.Random;
 
 import javax.validation.constraints.Positive;
@@ -13,12 +17,17 @@ import io.stackgres.common.crd.sgbackupconfig.StackGresBackupConfig;
 import io.stackgres.common.crd.sgbackupconfig.StackGresBackupConfigSpec;
 import io.stackgres.common.crd.sgbackupconfig.StackGresBaseBackupConfig;
 import io.stackgres.common.crd.storages.BackupStorage;
-import io.stackgres.testutil.JsonUtil;
+import io.stackgres.common.crd.storages.StorageClassS3;
 import io.stackgres.operator.common.BackupConfigReview;
 import io.stackgres.operator.validation.ConstraintValidationTest;
 import io.stackgres.operator.validation.ConstraintValidator;
+import io.stackgres.operatorframework.admissionwebhook.validating.ValidationFailed;
+import io.stackgres.testutil.JsonUtil;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
@@ -79,7 +88,38 @@ class BackupConfigConstraintValidationTest
     BackupConfigReview review = getValidReview();
     review.getRequest().getObject().getSpec().getBaseBackups().setRetention(negativeRetention);
 
-    checkErrorCause(StackGresBaseBackupConfig.class, "spec.baseBackups.retention", review, Positive.class);
+    checkErrorCause(StackGresBaseBackupConfig.class, "spec.baseBackups.retention", review,
+        Positive.class);
 
   }
+
+  @ParameterizedTest
+  @ValueSource(strings = {"DEMO", "STANDARDIA", "reduced_redundancy"})
+  void givenStorageClass_shouldFail(String storageClass) {
+    BackupConfigReview review = JsonUtil.readFromJson("backupconfig_allow_request/update.json",
+        BackupConfigReview.class);
+    review.getRequest().getObject().getSpec().getStorage().getS3Compatible()
+        .setStorageClass(storageClass);
+
+    ValidationFailed ex = assertThrows(ValidationFailed.class, () -> {
+      validator.validate(review);
+    });
+
+    String errorMessage = ex.getResult().getMessage();
+    assertEquals("SGBackupConfig has invalid properties. "
+        + "storageClass must be one of STANDARD, STANDARD_IA or REDUCED_REDUNDANCY.",
+        errorMessage);
+  }
+
+  @ParameterizedTest
+  @EnumSource(value = StorageClassS3.class)
+  void givenStorageClass_shouldPass(StorageClassS3 storageClass) {
+    BackupConfigReview review = JsonUtil.readFromJson("backupconfig_allow_request/update.json",
+        BackupConfigReview.class);
+    review.getRequest().getObject().getSpec().getStorage().getS3Compatible()
+        .setStorageClass(storageClass.toString());
+
+    assertDoesNotThrow(() -> validator.validate(review));
+  }
+
 }
