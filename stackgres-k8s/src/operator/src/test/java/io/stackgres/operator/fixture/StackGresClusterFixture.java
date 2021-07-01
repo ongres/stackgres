04b6fbf5837d2d47c32ead5d68baeec60a29023e
Guilherme Elias <elias@ongres.com>
2021-07-01 02:57:54 -0300

    feat: Mapped kubernetes node affinity on conciliation behavior.
    Added some unit testing in the PodTemplateSpecFactory class
    
    Signed-off-by: Guilherme Elias <elias@ongres.com>

---
+++ stackgres-k8s/src/operator/src/test/java/io/stackgres/operator/fixture/StackGresClusterFixture.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2019 OnGres, Inc.
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+package io.stackgres.operator.fixture;
+
+import io.stackgres.common.crd.NodeAffinity;
+import io.stackgres.common.crd.sgcluster.StackGresCluster;
+
+public class StackGresClusterFixture {
+
+	private NodeAffinity nodeAffinity;
+
+	public StackGresClusterFixture withNodeAffinity(NodeAffinity nodeAffinity) {
+		this.nodeAffinity = nodeAffinity;
+		return this;
+	}
+
+	public StackGresCluster build() {
+		StackGresCluster cluster = new StackGresCluster();
+		cluster.setSpec(new StackGresClusterSpecFixture()
+				.withPod(new StackGresClusterPodFixture()
+						.withScheduling(new StackGresClusterPodSchedulingFixture()
+								.withNodeAffinity(nodeAffinity)
+								.build())
+						.build())
+				.build());
+		return cluster;
+	}
+
+  public StackGresCluster empty() {
+    return new StackGresCluster();
+  }
+
+}
