42a5bae4246a75126bca07330242ce04316dfb24
Matteo Melli <matteom@ongres.com>
2021-07-19 13:53:33 +0200

    fix: avoid propagate debug setting in JAVA_OPTS env var

--- stackgres-k8s/src/api-web/src/main/docker/stackgres-restapi.jvm.sh
+++ stackgres-k8s/src/api-web/src/main/docker/stackgres-restapi.jvm.sh
@@ -5,7 +5,7 @@ APP_OPTS="${APP_OPTS:-"-Dquarkus.http.host=0.0.0.0 -Dquarkus.http.port=8080 -Dqu
 if [ "$DEBUG_RESTAPI" = true ]
 then
   set -x
-  JAVA_OPTS="$JAVA_OPTS -agentlib:jdwp=transport=dt_socket,server=y,address=8000,suspend=$([ "$DEBUG_RESTAPI_SUSPEND" = true ] && echo y || echo n)"
+  DEBUG_JAVA_OPTS="-agentlib:jdwp=transport=dt_socket,server=y,address=8000,suspend=$([ "$DEBUG_RESTAPI_SUSPEND" = true ] && echo y || echo n)"
 fi
 if [ -n "$RESTAPI_LOG_LEVEL" ]
 then
@@ -15,4 +15,4 @@ if [ "$RESTAPI_SHOW_STACK_TRACES" = true ]
 then
   APP_OPTS="$APP_OPTS -Dquarkus.log.console.format=%d{yyyy-MM-dd HH:mm:ss,SSS} %-5p [%c{4.}] (%t) %s%e%n"
 fi
-exec java $JAVA_OPTS -jar /app/stackgres-restapi.jar $APP_OPTS
+exec java $JAVA_OPTS $DEBUG_JAVA_OPTS -jar /app/stackgres-restapi.jar $APP_OPTS
