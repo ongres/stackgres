277eb339f0a2a82c7bc0c577fed29c66f5d2eb4e
Guilherme Elias <elias@ongres.com>
2021-08-25 17:58:59 -0300

    feat: Implemented better results to the APIRest returned events

--- stackgres-k8s/src/api-web/src/main/java/io/stackgres/apiweb/transformer/EventMapper.java
+++ stackgres-k8s/src/api-web/src/main/java/io/stackgres/apiweb/transformer/EventMapper.java
@@ -5,10 +5,11 @@
 
 package io.stackgres.apiweb.transformer;
 
+import static org.apache.commons.lang3.StringUtils.isBlank;
+
 import io.fabric8.kubernetes.api.model.Event;
 import io.stackgres.apiweb.dto.event.EventDto;
 import io.stackgres.apiweb.dto.event.ObjectReference;
-import io.stackgres.apiweb.dto.event.Series;
 
 public class EventMapper {
 
@@ -17,40 +18,83 @@ public class EventMapper {
     eventDto.setMetadata(MetadataMapper.map(event.getMetadata()));
     eventDto.setType(event.getType());
     eventDto.setAction(event.getAction());
-    eventDto.setCount(event.getCount());
-    if (event.getEventTime() != null) {
-      eventDto.setEventTime(event.getEventTime().getTime());
-    }
-    eventDto.setFirstTimestamp(event.getFirstTimestamp());
-    eventDto.setLastTimestamp(event.getLastTimestamp());
     eventDto.setReason(event.getReason());
     eventDto.setMessage(event.getMessage());
-    eventDto.setReportingComponent(event.getReportingComponent());
-    eventDto.setReportingInstance(event.getReportingInstance());
     eventDto.setAction(event.getAction());
-    if (event.getInvolvedObject() != null) {
-      eventDto.setInvolvedObject(new ObjectReference());
-      eventDto.getInvolvedObject().setKind(event.getInvolvedObject().getKind());
-      eventDto.getInvolvedObject().setNamespace(event.getInvolvedObject().getNamespace());
-      eventDto.getInvolvedObject().setName(event.getInvolvedObject().getName());
-      eventDto.getInvolvedObject().setUid(event.getInvolvedObject().getUid());
-    }
-    if (event.getRelated() != null) {
-      eventDto.setRelated(new ObjectReference());
-      eventDto.getRelated().setKind(event.getRelated().getKind());
-      eventDto.getRelated().setNamespace(event.getRelated().getNamespace());
-      eventDto.getRelated().setName(event.getRelated().getName());
-      eventDto.getRelated().setUid(event.getRelated().getUid());
-    }
-    if (event.getSeries() != null) {
-      eventDto.setSeries(new Series());
-      eventDto.getSeries().setCount(event.getSeries().getCount());
-      if (eventDto.getSeries().getLastObservedTime() != null) {
-        eventDto.getSeries().setLastObservedTime(
-            event.getSeries().getLastObservedTime().getTime());
-      }
-    }
+    eventDto.setCount(setupEventCount(event));
+    eventDto.setFirstTimestamp(setupFirstTimestamp(event));
+    eventDto.setLastTimestamp(setupLastTimestamp(event));
+    eventDto.setReportingComponent(setupReportingComponent(event));
+    eventDto.setReportingInstance(setupReportingInstance(event));
+    eventDto.setInvolvedObject(setupinvolvedObject(event));
+    eventDto.setRelated(setupRelated(event));
     return eventDto;
+  }
+
+  private static ObjectReference setupRelated(Event event) {
+    if (event.getRelated() == null) {
+      return null;
+    }
+    return mapObjectReference(event.getRelated());
+  }
+
+  private static ObjectReference setupinvolvedObject(Event event) {
+    if (event.getInvolvedObject() == null) {
+      return null;
+    }
+    return mapObjectReference(event.getInvolvedObject());
+  }
+
+  private static ObjectReference mapObjectReference(
+      io.fabric8.kubernetes.api.model.ObjectReference reference) {
+    ObjectReference objectReference = new ObjectReference();
+    objectReference.setKind(reference.getKind());
+    objectReference.setNamespace(reference.getNamespace());
+    objectReference.setName(reference.getName());
+    objectReference.setUid(reference.getUid());
+    return objectReference;
+  }
+
+  private static String setupReportingInstance(Event event) {
+    String reportingInstance = event.getReportingInstance();
+    if (isBlank(reportingInstance) && event.getSource() != null) {
+      reportingInstance = event.getSource().getHost();
+    }
+    return reportingInstance;
+  }
+
+  private static String setupReportingComponent(Event event) {
+    String reportingComponent = event.getReportingComponent();
+    if (isBlank(reportingComponent) && event.getSource() != null) {
+      reportingComponent = event.getSource().getComponent();
+    }
+    return reportingComponent;
+  }
+
+  private static Integer setupEventCount(Event event) {
+    Integer eventCount = event.getCount();
+    if (eventCount == null && event.getSeries() != null) {
+      eventCount = event.getSeries().getCount();
+    }
+    return eventCount;
+  }
+
+  private static String setupFirstTimestamp(Event event) {
+    String firstTimeStamp = event.getFirstTimestamp();
+    if (isBlank(firstTimeStamp) && event.getEventTime() != null) {
+      // TODO - Convert Microtime to timestamp
+      firstTimeStamp = event.getEventTime().getTime();
+    }
+    return firstTimeStamp;
+  }
+
+  private static String setupLastTimestamp(Event event) {
+    String lastTimestamp = event.getLastTimestamp();
+    if (isBlank(lastTimestamp)) {
+      // TODO - Convert Microtime to timestamp
+      lastTimestamp = event.getSeries().getLastObservedTime().getTime();
+    }
+    return lastTimestamp;
   }
 
 }
