a7b374b2373ee3397c726c11861726d08befa15a
ldgarcia <lgarcia@ongres.com>
2021-09-01 19:02:24 +0200

    fix(ui): order cluster events by firstTimestamp

--- stackgres-k8s/src/admin-ui/src/components/ClusterEvents.vue
+++ stackgres-k8s/src/admin-ui/src/components/ClusterEvents.vue
@@ -53,8 +53,13 @@
                 <template v-if="!$route.params.hasOwnProperty('uid')">
                     <table id="events" class="resizable" v-columns-resizable>
                         <thead>
+							<th class="firstTimestamp hasTooltip">
+                                <span title="First Timestamp">
+                                    First Timestamp
+                                </span>
+                            </th>
                             <th class="lastTimestamp hasTooltip">
-                                <span @click="sort('lastTimestamp')" title="Last Timestamp">
+                                <span title="Last Timestamp">
                                     Last Timestamp
                                 </span>
                             </th>
@@ -82,7 +87,23 @@
                                     <template v-if="(index >= pagination.start) && (index < pagination.end)">
                                         <tr class="base">
 											<td class="timestamp hasTooltip">
-												<span>
+												<span v-if="event.hasOwnProperty('firstTimestamp')">
+													<router-link :to="'/' + $route.params.namespace + '/sgcluster/' + $route.params.name + '/event/' + event.metadata.uid" class="noColor">
+                                                        <span class='date'>
+                                                            {{ event.firstTimestamp | formatTimestamp('date') }}
+                                                        </span>
+                                                        <span class='time'>
+                                                            {{ event.firstTimestamp | formatTimestamp('time') }}
+                                                        </span>
+                                                        <span class='ms'>
+                                                            {{ event.firstTimestamp | formatTimestamp('ms') }}
+                                                        </span>
+                                                        <span class='tzOffset'>{{ showTzOffset() }}</span>
+													</router-link>
+												</span>
+											</td>
+											<td class="timestamp hasTooltip">
+												<span v-if="event.hasOwnProperty('lastTimestamp')">
 													<router-link :to="'/' + $route.params.namespace + '/sgcluster/' + $route.params.name + '/event/' + event.metadata.uid" class="noColor">
                                                         <span class='date'>
                                                             {{ event.lastTimestamp | formatTimestamp('date') }}
@@ -97,10 +118,12 @@
 													</router-link>
 												</span>
 											</td>
-                                            <td class="involvedObject hasTooltip" v-if="showInvolvedObjectsColumn">
-                                                <span v-if="event.involvedObject.kind != 'SGCluster'">
-                                                    {{ event.involvedObject.kind }}/{{ event.involvedObject.name }}
-                                                </span>
+                                            <td class="involvedObject" v-if="showInvolvedObjectsColumn">
+												<span>
+													<router-link :to="'/' + $route.params.namespace + '/sgcluster/' + $route.params.name + '/event/' + event.metadata.uid" class="noColor">
+														{{ event.involvedObject.kind }}/{{ event.involvedObject.name }}
+													</router-link>
+												</span>
                                             </td>
                                             <td class="eventMessage hasTooltip">
                                                 <span>
@@ -134,11 +157,11 @@
                                     <td class="label">Name</td>
                                     <td>{{ event.metadata.name }}</td>
                                 </tr>
-								<tr v-if="event.hasOwnProperty('type')">
+								<tr v-if="event.hasOwnProperty('type') && event.type">
                                     <td class="label">Type</td>
                                     <td>{{ event.type }}</td>
                                 </tr>
-								<tr v-if="event.hasOwnProperty('firstTimestamp')">
+								<tr v-if="event.hasOwnProperty('firstTimestamp') && event.firstTimestamp">
                                     <td class="label">First Timestamp</td>
                                     <td class="timestamp">
 										<span class='date'>
@@ -153,7 +176,7 @@
 										<span class='tzOffset'>{{ showTzOffset() }}</span>
 									</td>
                                 </tr>
-								<tr v-if="event.hasOwnProperty('lastTimestamp')">
+								<tr v-if="event.hasOwnProperty('lastTimestamp') && event.lastTimestamp">
                                     <td class="label">Last Timestamp</td>
                                     <td class="timestamp">
 										<span class='date'>
@@ -168,19 +191,34 @@
 										<span class='tzOffset'>{{ showTzOffset() }}</span>
 									</td>
                                 </tr>
-								<tr v-if="event.hasOwnProperty('count')">
+								<tr v-if="event.hasOwnProperty('count') && event.count">
                                     <td class="label">Count</td>
                                     <td>{{ event.count }}</td>
                                 </tr>
-								<tr v-if="event.hasOwnProperty('reason')">
+								<tr v-if="event.hasOwnProperty('reason') && event.reason">
                                     <td class="label">Reason</td>
                                     <td>{{ event.reason }}</td>
+                                </tr><tr v-if="event.hasOwnProperty('action') && event.action">
+                                    <td class="label">Action</td>
+                                    <td>{{ event.action }}</td>
                                 </tr>
-								<tr v-if="event.hasOwnProperty('reportingComponent')">
+								<tr v-if="event.hasOwnProperty('involvedObject') && event.involvedObject">
+                                    <td class="label">Involved Object</td>
+                                    <td>{{ event.involvedObject.kind }}/{{ event.involvedObject.name }}</td>
+                                </tr>
+								<tr v-if="event.hasOwnProperty('reportingComponent') && event.reportingComponent">
                                     <td class="label">Reporting Component</td>
                                     <td class="vPad">{{ event.reportingComponent }}</td>
                                 </tr>
-								<tr v-if="event.hasOwnProperty('message')">
+								<tr v-if="event.hasOwnProperty('reportingInstance') && event.reportingInstance">
+                                    <td class="label">Reporting Instance</td>
+                                    <td class="vPad">{{ event.reportingInstance }}</td>
+                                </tr>
+								<tr v-if="event.hasOwnProperty('related') && event.related">
+                                    <td class="label">Related</td>
+                                    <td>{{ event.related }}</td>
+                                </tr>
+								<tr v-if="event.hasOwnProperty('message') && event.message">
                                     <td class="label">Message</td>
                                     <td>{{ event.message }}</td>
                                 </tr>
@@ -197,6 +235,7 @@
 	import store from '../store'
 	import { mixin } from './mixins/mixin'
 	import axios from 'axios'
+	import moment from 'moment'
 
     export default {
         name: 'ClusterEvents',
@@ -229,6 +268,20 @@
 				.get('/stackgres/namespaces/' + vc.$route.params.namespace + '/sgclusters/' + vc.$route.params.name + '/events')
 				.then( function(response) {
 					vc.events = [...response.data]
+					
+					vc.events.sort((a,b) => {
+						
+						if(moment(a).isValid && moment(b).isValid) {
+
+							if(moment(a).isBefore(moment(b)))
+								return -1;
+						
+							if(moment(a).isAfter(moment(b)))
+								return 1;  
+
+						}
+					});
+
 					vc.showInvolvedObjectsColumn =  (vc.events.filter(e => (e.involvedObject.kind != 'SGCluster') ).length > 0)
 				}).catch(function(err) {
 					console.log(err);
@@ -254,7 +307,3 @@
 		} 
 	}
 </script>
-
-<style scoped>
-	
-</style>
