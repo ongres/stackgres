4cfc099acb0f8390e92b2d58adaebf66a8ce9dfd
Jorge Solórzano <jorsol@ongres.com>
2021-09-01 19:01:08 -0600

    chore: Update ubi8 containers base image
    Signed-off-by: Jorge Solórzano <jorsol@ongres.com>

--- stackgres-k8s/src/api-web/src/main/docker/build-image-jvm.sh
+++ stackgres-k8s/src/api-web/src/main/docker/build-image-jvm.sh
@@ -3,7 +3,7 @@
 set -e
 
 RESTAPI_IMAGE_NAME="${RESTAPI_IMAGE_NAME:-"stackgres/restapi:main-jvm"}"
-BASE_IMAGE="registry.access.redhat.com/ubi8/openjdk-11:1.3-10"
+BASE_IMAGE="registry.access.redhat.com/ubi8/openjdk-11-runtime:1.10-1"
 TARGET_RESTAPI_IMAGE_NAME="${TARGET_RESTAPI_IMAGE_NAME:-$RESTAPI_IMAGE_NAME}"
 
 docker build -t "$TARGET_RESTAPI_IMAGE_NAME" --build-arg BASE_IMAGE="$BASE_IMAGE" -f api-web/src/main/docker/Dockerfile.jvm api-web
--- stackgres-k8s/src/distributedlogs-controller/src/main/docker/build-image-native.sh
+++ stackgres-k8s/src/distributedlogs-controller/src/main/docker/build-image-native.sh
@@ -3,7 +3,7 @@
 set -e
 
 DISTRIBUTEDLOGS_CONTROLLER_IMAGE_NAME="${DISTRIBUTEDLOGS_CONTROLLER_IMAGE_NAME:-"stackgres/distributedlogs-controller:main"}"
-BASE_IMAGE="registry.access.redhat.com/ubi8-minimal:8.3-298.1618432845"
+BASE_IMAGE="registry.access.redhat.com/ubi8-minimal:8.4-208"
 TARGET_DISTRIBUTEDLOGS_CONTROLLER_IMAGE_NAME="${TARGET_DISTRIBUTEDLOGS_CONTROLLER_IMAGE_NAME:-$DISTRIBUTEDLOGS_CONTROLLER_IMAGE_NAME}"
 
 docker build -t "$TARGET_DISTRIBUTEDLOGS_CONTROLLER_IMAGE_NAME" --build-arg BASE_IMAGE="$BASE_IMAGE" -f distributedlogs-controller/src/main/docker/Dockerfile.native distributedlogs-controller
