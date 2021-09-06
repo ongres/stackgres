a21d247689a0cfaaf33c88520e837b2ed9df5da3
ldgarcia <lgarcia@ongres.com>
2021-09-06 16:29:44 +0200

    doc(ui): show search form and version dropdown on mobile

--- doc/themes/sg-doc/static/js/learn.js
+++ doc/themes/sg-doc/static/js/learn.js
@@ -27,7 +27,10 @@ function getScrollBarWidth() {
 
 function setMenuHeight() {
     $('#sidebar .highlightable').height($('#sidebar').innerHeight() - $('#header-wrapper').height() - 40);
-    $('#sidebar .highlightable').perfectScrollbar('update');
+
+    if(window.innerWidth > 768) {
+        $('#sidebar .highlightable').perfectScrollbar('update');
+    }
 }
 
 function fallbackMessage(action) {
@@ -45,6 +48,14 @@ function fallbackMessage(action) {
     }
 
     return actionMsg;
+}
+
+function navToggle() {
+    jQuery('.mobileMenu, #header').toggleClass('menuOpen')
+}
+
+function toggleDiv(id) {
+    jQuery(id).toggleClass('show')
 }
 
 // for the window resize
@@ -90,7 +101,11 @@ jQuery(document).ready(function() {
     });
 
     var sidebarStatus = searchStatus = 'open';
-    $('#sidebar .highlightable').perfectScrollbar();
+
+    if(window.innerWidth > 768) {
+        $('#sidebar .highlightable').perfectScrollbar();
+    }
+    
     setMenuHeight();
 
     jQuery('#overlay').on('click', function() {
