d8bc73b079c721b8e619a2d861b7d3dbbedf2637
Matteo Melli <matteom@ongres.com>
2021-09-13 14:10:25 +0200

    fix: Native and jvm image APP_OPTS and JAVA_OPTS defaults are overwritten

--- stackgres-k8s/src/distributedlogs-controller/src/main/docker/stackgres-distributedlogs-controller.native.sh
+++ stackgres-k8s/src/distributedlogs-controller/src/main/docker/stackgres-distributedlogs-controller.native.sh
@@ -4,7 +4,6 @@ if [ "$DEBUG_DISTRIBUTEDLOGS_CONTROLLER" = true ]
 then
   set -x
 fi
-APP_OPTS="${APP_OPTS:-"-Dquarkus.http.host=0.0.0.0 -Dquarkus.http.port=8080 -Dquarkus.http.ssl-port=8443 -Djava.util.logging.manager=org.jboss.logmanager.LogManager"}"
 if [ -n "$DISTRIBUTEDLOGS_CONTROLLER_LOG_LEVEL" ]
 then
   APP_OPTS="$APP_OPTS -Dquarkus.log.level=$DISTRIBUTEDLOGS_CONTROLLER_LOG_LEVEL"
@@ -13,4 +12,9 @@ if [ "$DISTRIBUTEDLOGS_CONTROLLER_SHOW_STACK_TRACES" = true ]
 then
   APP_OPTS="$APP_OPTS -Dquarkus.log.console.format=%d{yyyy-MM-dd HH:mm:ss,SSS} %-5p [%c{4.}] (%t) %s%e%n"
 fi
-exec /app/stackgres-distributedlogs-controller $APP_OPTS
+exec /app/stackgres-distributedlogs-controller \
+  -Dquarkus.http.host=0.0.0.0 \
+  -Dquarkus.http.port=8080 \
+  -Dquarkus.http.ssl-port=8443 \
+  -Djava.util.logging.manager=org.jboss.logmanager.LogManager \
+  $APP_OPTS
