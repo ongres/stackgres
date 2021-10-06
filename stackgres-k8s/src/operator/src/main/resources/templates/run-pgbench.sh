791006cfa9db6385d63cb0a6ec15686f6f2d39ad
Matteo Melli <matteom@ongres.com>
2021-10-06 12:40:43 +0200

    fix: add events to pgbench sgdbops

--- stackgres-k8s/src/operator/src/main/resources/templates/run-pgbench.sh
+++ stackgres-k8s/src/operator/src/main/resources/templates/run-pgbench.sh
@@ -11,18 +11,7 @@ run_op() {
 
   EXIT_CODE="$?"
 
-  DROP_RETRY=3
-  while [ "$DROP_RETRY" -ge 0 ]
-  do
-    if psql --host="$PRIMARY_PGHOST" -c "SELECT pg_terminate_backend(pid)
-        FROM pg_stat_activity WHERE datname = 'pgbench'" \
-      -c "DROP DATABASE pgbench"
-    then
-      break
-    fi
-    DROP_RETRY="$((DROP_RETRY - 1))"
-    sleep 3
-  done
+  try_drop_pgbench_database
 
   return "$EXIT_CODE"
 }
@@ -31,35 +20,38 @@ run_pgbench() {
   (
   export PGHOST="$PRIMARY_PGHOST"
 
-  (
-  set +e
+  try_drop_pgbench_database
 
-  DROP_RETRY=3
-  while [ "$DROP_RETRY" -ge 0 ]
-  do
-    if [ "$(psql -t -A \
-      -c "SELECT EXISTS (SELECT * FROM pg_database WHERE datname = 'pgbench')")" != 'f' ]
-    then
-      if psql -c "SELECT pg_cancel_backend(pid) FROM pg_stat_activity WHERE datname = 'pgbench' AND pid != pg_backend_pid()" \
-        -c "DROP DATABASE pgbench"
-      then
-        break
-      fi
-    fi
-    DROP_RETRY="$((DROP_RETRY - 1))"
-    sleep 3
-  done
-  )
+  if MESSAGE="$(psql -c "CREATE DATABASE pgbench" 2>&1)"
+  then
+    echo "$MESSAGE"
+    create_event "DatabaseCreated" "Normal" "Database pgbench created"
+  else
+    create_event "CreateDatabaseFailed" "Warning" "Can not create database pgbench: $MESSAGE"
+  fi
 
-  psql -c "CREATE DATABASE pgbench"
-
-  pgbench -s "$SCALE" -i pgbench
+  create_event "BenchmarkInitializationStarted" "Normal" "Benchamrk initialization started"
+  if MESSAGE="$(pgbench -s "$SCALE" -i pgbench 2>&1)"
+  then
+    echo "$MESSAGE"
+    create_event "BenchmarkInitialized" "Normal" "Benchamrk initialized"
+  else
+    create_event "BenchmarkInitializationFailed" "Warning" "Can not initialize benchmark: $MESSAGE"
+  fi
   )
 
   if "$READ_WRITE"
   then
-    pgbench -M "$PROTOCOL" -s "$SCALE" -T "$DURATION" -c "$CLIENTS" -j "$JOBS" -r -P 1 pgbench
+    create_event "BenchmarkStarted" "Normal" "Benchamrk started"
+    if MESSAGE="$(pgbench -M "$PROTOCOL" -s "$SCALE" -T "$DURATION" -c "$CLIENTS" -j "$JOBS" -r -P 1 pgbench 2>&1)"
+    then
+      echo "$MESSAGE"
+      create_event "BenchmarkCompleted" "Normal" "Benchmark completed"
+    else
+      create_event "BenchmarkFailed" "Warning" "Can not complete benchmark: $MESSAGE"
+    fi
   else
+    create_event "BenchmarkPostInitializationStarted" "Normal" "Benchamrk post initialization started"
     PGBENCH_ACCOUNTS_COUNT="$(PGHOST="$PRIMARY_PGHOST" psql -t -A -d pgbench \
       -c "SELECT COUNT(*) FROM pgbench_accounts")"
 
@@ -68,7 +60,35 @@ run_pgbench() {
     do
       sleep 1
     done
+    create_event "BenchmarkPostInitializationCompleted" "Normal" "Benchamrk post initialization completed"
 
-    pgbench -b "select-only" -M "$PROTOCOL" -s "$SCALE" -T "$DURATION" -c "$CLIENTS" -j "$JOBS" -r -P 1 pgbench
+    create_event "BenchmarkStarted" "Normal" "Benchamrk started"
+    if MESSAGE="$(pgbench -b "select-only" -M "$PROTOCOL" -s "$SCALE" -T "$DURATION" -c "$CLIENTS" -j "$JOBS" -r -P 1 pgbench 2>&1)"
+    then
+      echo "$MESSAGE"
+      create_event "BenchmarkCompleted" "Normal" "Benchmark completed"
+    else
+      create_event "BenchmarkFailed" "Warning" "Can not complete benchmark: $MESSAGE"
+    fi
   fi
 }
+
+try_drop_pgbench_database() {
+  (
+  set +e
+  DROP_RETRY=3
+  while [ "$DROP_RETRY" -ge 0 ]
+  do
+      if MESSAGE="$(psql \
+        -c "SELECT pg_cancel_backend(pid) FROM pg_stat_activity WHERE datname = 'pgbench' AND pid != pg_backend_pid()" \
+        -c "DROP DATABASE pgbench" 2>&1)"
+    then
+      break
+    fi
+    create_event "DropDatabaseFailed" "Warning" "Can not drop pgbench database: $MESSAGE"
+    DROP_RETRY="$((DROP_RETRY - 1))"
+    sleep 3
+  done
+  )
+}
+
