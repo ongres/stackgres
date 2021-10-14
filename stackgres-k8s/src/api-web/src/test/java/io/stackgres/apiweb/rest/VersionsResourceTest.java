bb9ddce6673ca1a0b7ffe88ef7dc6a4f6fedef1a
Matteo Melli <matteom@ongres.com>
2021-10-14 16:29:05 +0200

    chore: remove unused imports

--- stackgres-k8s/src/api-web/src/test/java/io/stackgres/apiweb/rest/VersionsResourceTest.java
+++ stackgres-k8s/src/api-web/src/test/java/io/stackgres/apiweb/rest/VersionsResourceTest.java
@@ -11,7 +11,6 @@ import io.quarkus.test.junit.QuarkusTest;
 import io.stackgres.common.StackGresComponent;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
 
 @QuarkusTest
 class VersionsResourceTest {
