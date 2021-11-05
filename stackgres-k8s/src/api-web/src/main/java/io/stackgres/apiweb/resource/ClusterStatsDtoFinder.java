390a361bac5fcc5e511c73a267a06d5763953257
Jorge Solórzano <jorsol@ongres.com>
2021-11-05 10:22:01 +0100

    fix: Restapi error in stats endpoint (Message container not found patroni)
    Signed-off-by: Jorge Solórzano <jorsol@ongres.com>

--- stackgres-k8s/src/api-web/src/main/java/io/stackgres/apiweb/resource/ClusterStatsDtoFinder.java
+++ stackgres-k8s/src/api-web/src/main/java/io/stackgres/apiweb/resource/ClusterStatsDtoFinder.java
@@ -14,10 +14,15 @@ import javax.inject.Inject;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
+import io.fabric8.kubernetes.api.model.ContainerState;
+import io.fabric8.kubernetes.api.model.ContainerStateRunning;
+import io.fabric8.kubernetes.api.model.ContainerStateTerminated;
+import io.fabric8.kubernetes.api.model.ContainerStatus;
 import io.fabric8.kubernetes.api.model.PersistentVolumeClaim;
 import io.fabric8.kubernetes.api.model.PersistentVolumeClaimVolumeSource;
 import io.fabric8.kubernetes.api.model.Pod;
 import io.fabric8.kubernetes.api.model.PodSpec;
+import io.fabric8.kubernetes.api.model.PodStatus;
 import io.fabric8.kubernetes.api.model.Volume;
 import io.stackgres.apiweb.dto.cluster.ClusterStatsDto;
 import io.stackgres.apiweb.transformer.ClusterStatsTransformer;
@@ -76,6 +81,22 @@ public class ClusterStatsDtoFinder
 
   private ImmutableMap<PatroniStatsScripts, String> getPodStats(Pod pod) {
     try {
+      if (Optional.ofNullable(pod.getStatus())
+          .map(PodStatus::getContainerStatuses).stream()
+          .flatMap(List::stream)
+          .noneMatch(container -> container.getName().equals(StackgresClusterContainers.PATRONI)
+              && Optional.of(container).map(ContainerStatus::getState)
+                  .map(ContainerState::getRunning)
+                  .map(ContainerStateRunning::getStartedAt).isPresent()
+              && Optional.of(container).map(ContainerStatus::getState)
+                  .map(ContainerState::getTerminated)
+                  .map(ContainerStateTerminated::getFinishedAt).isEmpty())) {
+        LOGGER.debug("Patroni container is not running, could not retrieve stats for pod {}.{}",
+            pod.getMetadata().getNamespace(),
+            pod.getMetadata().getName());
+        return ImmutableMap.<PatroniStatsScripts, String>of();
+      }
+
       return Seq.seq(podExecutor.exec(pod, StackgresClusterContainers.PATRONI, "sh", "-c",
           Seq.seq(PatroniStatsScripts.getScripts())
               .map(tt -> "echo \"" + tt.v1.getName() + ":$( (" + tt.v2
@@ -95,7 +116,7 @@ public class ClusterStatsDtoFinder
               PatroniStatsScripts.fromName(tt.v1.substring(0, tt.v2)),
               tt.v1.substring(tt.v2 + 1)))
           .collect(ImmutableMap.toImmutableMap(Tuple2::v1, Tuple2::v2));
-    } catch (Exception ex) {
+    } catch (RuntimeException ex) {
       LOGGER.debug("An error accurred while retrieving stats for pod {}.{}: {}",
           pod.getMetadata().getNamespace(),
           pod.getMetadata().getName(),
