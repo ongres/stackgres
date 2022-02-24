ee0c967f9e3bbd41066565ac5c8f3b9b79a60f8b
Olesya Saletska <olesya@ongres.com>
2022-02-24 12:47:54 +0100

    test(ui): add notifications test

---
+++ stackgres-k8s/src/admin-ui/cypress/integration/notifications.spec.js
@@ -0,0 +1,33 @@
+describe('Notifications Area', () => {
+
+    const namespace = Cypress.env('k8s_namespace')
+    
+    before( () => {
+        cy.login()
+    });
+
+    it('Notification details and link should be visible if they exist', () => {
+        // Submit form without selecting an op
+        cy.visit(namespace + '/sgclusters/new')
+
+        cy.get('[data-field="metadata.name"]')
+            .type(' ')
+
+        cy.get('form#createCluster button[type="submit"]')
+            .click()
+
+        // Error notificacion should appear
+        cy.get('#notifications .message.show .kind')
+            .should(($notification) => {
+                expect($notification).contain('error')
+            })
+
+        // Notification should have details
+        cy.get('#notifications .message.show .details')
+            .should('be.visible')
+
+        // Notification should have Info link
+        cy.get('#notifications .message.show .doclink')
+            .should('be.visible')
+    });
+})
