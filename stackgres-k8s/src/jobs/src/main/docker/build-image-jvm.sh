3aa10037a4cac603ce68008f8f953ca66bcaf859
Jorge Solórzano <jorsol@ongres.com>
2022-04-29 14:35:09 +0200

    fix: Potential security issue in OpenJDK image
    Signed-off-by: Jorge Solórzano <jorsol@ongres.com>

--- stackgres-k8s/src/jobs/src/main/docker/build-image-jvm.sh
+++ stackgres-k8s/src/jobs/src/main/docker/build-image-jvm.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-BASE_IMAGE="registry.access.redhat.com/ubi8/openjdk-17-runtime:1.11"
+BASE_IMAGE="registry.access.redhat.com/ubi8/openjdk-17-runtime:1.12"
 
 JOBS_IMAGE_NAME="${JOBS_IMAGE_NAME:-"stackgres/jobs:main-jvm"}"
 TARGET_JOBS_IMAGE_NAME="${TARGET_JOBS_IMAGE_NAME:-$JOBS_IMAGE_NAME}"
