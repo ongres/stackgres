371f385cb2a008b489360c4f760d16d8d24e84f0
Matteo Melli <matteom@ongres.com>
2022-06-20 22:16:43 +0200

    feat: disallow creation using PostgreSQL 14.3- and create a warning if user is using it

--- stackgres-k8s/src/operator/src/test/java/io/stackgres/operator/validation/cluster/PostgresVersionValidatorTest.java
+++ stackgres-k8s/src/operator/src/test/java/io/stackgres/operator/validation/cluster/PostgresVersionValidatorTest.java
@@ -24,6 +24,7 @@ import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import com.google.common.base.Predicates;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import io.stackgres.common.StackGresComponent;
