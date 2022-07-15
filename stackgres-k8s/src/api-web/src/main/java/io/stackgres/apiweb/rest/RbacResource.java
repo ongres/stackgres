76d01a9260cb0234a960739b1834980aca267d00
Guilherme Elias <elias@ongres.com>
2022-07-15 16:12:31 -0300

    chore: added missing resources permission
    - stats
    - events
    - logs
    - pod/exec
    
    Signed-off-by: Guilherme Elias <elias@ongres.com>

--- stackgres-k8s/src/api-web/src/main/java/io/stackgres/apiweb/rest/RbacResource.java
+++ stackgres-k8s/src/api-web/src/main/java/io/stackgres/apiweb/rest/RbacResource.java
@@ -57,12 +57,12 @@ public class RbacResource {
   @Claim(standard = Claims.sub)
   String user;
 
+  @Inject
   NamespaceResource namespaces;
 
   @Inject
-  public RbacResource(NamespaceResource namespaces) {
+  public RbacResource() {
     super();
-    this.namespaces = namespaces;
   }
 
   @Operation(
@@ -78,7 +78,6 @@ public class RbacResource {
   public Response verb(@PathParam("verb") String verb, @PathParam("resource") String resource,
       @QueryParam("namespace") String namespace, @QueryParam("group") Optional<String> group) {
     LOGGER.debug("User to review access {}", user);
-    // Connect with the serviceaccount permissions
     try (KubernetesClient client = new DefaultKubernetesClient()) {
 
       SubjectAccessReview review = new SubjectAccessReviewBuilder()
@@ -118,52 +117,67 @@ public class RbacResource {
   @GET
   @Path("can-i")
   public Response caniList() {
-    // Connect with the serviceaccount permissions
+    PermissionsListDto permissionsList = new PermissionsListDto();
     try (KubernetesClient client = new DefaultKubernetesClient()) {
-
-      List<String> verbs = List.of("get", "list", "create", "patch", "delete");
-      List<String> resourcesNamespaced = List.of("pods", "secrets", "configmaps",
-          HasMetadata.getFullResourceName(StackGresObjectStorage.class),
-          HasMetadata.getFullResourceName(StackGresBackupConfig.class),
-          HasMetadata.getFullResourceName(StackGresBackup.class),
-          HasMetadata.getFullResourceName(StackGresCluster.class),
-          HasMetadata.getFullResourceName(StackGresDistributedLogs.class),
-          HasMetadata.getFullResourceName(StackGresProfile.class),
-          HasMetadata.getFullResourceName(StackGresDbOps.class),
-          HasMetadata.getFullResourceName(StackGresPostgresConfig.class),
-          HasMetadata.getFullResourceName(StackGresPoolingConfig.class));
-      List<String> resourcesUnnamespaced =
-          List.of("namespaces", "storageclasses.storage.k8s.io");
-
-      PermissionsListDto permissionsList = new PermissionsListDto();
-      List<PermissionsListDto.Namespaced> listNamespaced = new ArrayList<>();
-      for (String ns : namespaces.get()) {
-        PermissionsListDto.Namespaced permisionsNamespaced = new PermissionsListDto.Namespaced();
-        Map<String, List<String>> resourceNamespace = new HashMap<>();
-        for (String rsNamespaced : resourcesNamespaced) {
-          String[] resource = rsNamespaced.split("\\.", 2);
-          List<String> allowed = accessReview(client, user, ns, resource[0],
-              resource.length == 2 ? resource[1] : "", verbs);
-          resourceNamespace.put(resource[0], allowed);
-        }
-        permisionsNamespaced.setNamespace(ns);
-        permisionsNamespaced.setResources(resourceNamespace);
-        listNamespaced.add(permisionsNamespaced);
-      }
-
-      Map<String, List<String>> resourceUnamespace = new HashMap<>();
-      for (String rsUnnamespaced : resourcesUnnamespaced) {
-        String[] resource = rsUnnamespaced.split("\\.", 2);
-        List<String> allowed = accessReview(client, user, null, resource[0],
-            resource.length == 2 ? resource[1] : "", verbs);
-        resourceUnamespace.put(resource[0], allowed);
-      }
-
-      permissionsList.setNamespaced(listNamespaced);
-      permissionsList.setUnnamespaced(resourceUnamespace);
-
-      return Response.ok(permissionsList).build();
+      permissionsList.setNamespaced(buildNamespacedPermissionList(client));
+      permissionsList.setUnnamespaced(buildUnnamespacedPermissionList(client));
     }
+    return Response.ok(permissionsList).build();
+  }
+
+  private Map<String, List<String>> buildUnnamespacedPermissionList(KubernetesClient client) {
+    Map<String, List<String>> resourceUnamespace = new HashMap<>();
+    for (String rsUnnamespaced : getResourcesUnnamespaced()) {
+      String[] resource = rsUnnamespaced.split("\\.", 2);
+      List<String> allowed = accessReview(client, user, null, resource[0],
+          resource.length == 2 ? resource[1] : "", getVerbs());
+      resourceUnamespace.put(resource[0], allowed);
+    }
+    return resourceUnamespace;
+  }
+
+  private List<PermissionsListDto.Namespaced> buildNamespacedPermissionList(
+      KubernetesClient client) {
+    List<PermissionsListDto.Namespaced> listNamespaced = new ArrayList<>();
+    for (String ns : namespaces.get()) {
+      PermissionsListDto.Namespaced permisionsNamespaced = new PermissionsListDto.Namespaced();
+      Map<String, List<String>> resourceNamespace = new HashMap<>();
+      for (String rsNamespaced : getResourcesNamespaced()) {
+        String[] resource = rsNamespaced.split("\\.", 2);
+        List<String> allowed = accessReview(client, user, ns, resource[0],
+            resource.length == 2 ? resource[1] : "", getVerbs());
+        resourceNamespace.put(resource[0], allowed);
+      }
+      permisionsNamespaced.setNamespace(ns);
+      permisionsNamespaced.setResources(resourceNamespace);
+      listNamespaced.add(permisionsNamespaced);
+    }
+    return listNamespaced;
+  }
+
+  private List<String> getResourcesUnnamespaced() {
+    List<String> resourcesUnnamespaced =
+        List.of("namespaces", "storageclasses.storage.k8s.io");
+    return resourcesUnnamespaced;
+  }
+
+  private List<String> getResourcesNamespaced() {
+    List<String> resourcesNamespaced =
+        List.of("pods", "secrets", "configmaps", "stats", "events", "logs", "pod/exec",
+            HasMetadata.getFullResourceName(StackGresObjectStorage.class),
+            HasMetadata.getFullResourceName(StackGresBackupConfig.class),
+            HasMetadata.getFullResourceName(StackGresBackup.class),
+            HasMetadata.getFullResourceName(StackGresCluster.class),
+            HasMetadata.getFullResourceName(StackGresDistributedLogs.class),
+            HasMetadata.getFullResourceName(StackGresProfile.class),
+            HasMetadata.getFullResourceName(StackGresDbOps.class),
+            HasMetadata.getFullResourceName(StackGresPostgresConfig.class),
+            HasMetadata.getFullResourceName(StackGresPoolingConfig.class));
+    return resourcesNamespaced;
+  }
+
+  private List<String> getVerbs() {
+    return List.of("get", "list", "create", "patch", "delete");
   }
 
   private List<String> accessReview(KubernetesClient client, String user, String namespace,
