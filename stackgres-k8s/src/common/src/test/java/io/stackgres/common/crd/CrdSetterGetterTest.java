75b0ce7e76021131181ba80a24da6c40b78a534b
Matteo Melli <matteom@ongres.com>
2022-08-25 14:16:06 +0200

    test: check for setters and getters in all CRDs

---
+++ stackgres-k8s/src/common/src/test/java/io/stackgres/common/crd/CrdSetterGetterTest.java
@@ -0,0 +1,184 @@
+/*
+ * Copyright (C) 2019 OnGres, Inc.
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+package io.stackgres.common.crd;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.lang.reflect.ParameterizedType;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+
+import io.stackgres.common.crd.sgbackupconfig.StackGresBackupConfig;
+import io.stackgres.common.crd.sgcluster.StackGresCluster;
+import io.stackgres.common.crd.sgdbops.StackGresDbOps;
+import io.stackgres.common.crd.sgdistributedlogs.StackGresDistributedLogs;
+import io.stackgres.common.crd.sgobjectstorage.StackGresObjectStorage;
+import io.stackgres.common.crd.sgpgconfig.StackGresPostgresConfig;
+import io.stackgres.common.crd.sgpooling.StackGresPoolingConfig;
+import io.stackgres.common.crd.sgprofile.StackGresProfile;
+import io.stackgres.common.crd.sgscript.StackGresScript;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class CrdSetterGetterTest {
+
+  @ParameterizedTest
+  @ValueSource(classes = {
+      StackGresCluster.class,
+      StackGresProfile.class,
+      StackGresPostgresConfig.class,
+      StackGresPoolingConfig.class,
+      StackGresBackupConfig.class,
+      StackGresObjectStorage.class,
+      StackGresDbOps.class,
+      StackGresDistributedLogs.class,
+      StackGresScript.class
+  })
+  void assertSettersAndGetters(Class<?> sourceClazz) {
+    if (isValueType(sourceClazz)) {
+      return;
+    }
+
+    Field[] fields = getRepresentativeFields(sourceClazz);
+
+    Arrays.asList(fields).forEach(
+        field -> assertSettersAndGettersForField(field, sourceClazz)
+    );
+  }
+
+  public Field[] getRepresentativeFields(Class<?> clazz) {
+    if (clazz != null) {
+      Field[] declaredFields = clazz.getDeclaredFields();
+      Field[] parentFields = getRepresentativeFields(clazz.getSuperclass());
+      return Stream
+          .concat(
+              Arrays.stream(declaredFields),
+              Arrays.stream(parentFields)
+              .filter(field -> Arrays.stream(declaredFields)
+                  .map(Field::getName)
+                  .noneMatch(field.getName()::equals)))
+          .filter(field -> !Modifier.isStatic(field.getModifiers()))
+          .filter(field -> !Modifier.isFinal(field.getModifiers()))
+          .toArray(Field[]::new);
+    } else {
+      return new Field[0];
+    }
+  }
+
+  private <S> void assertSettersAndGettersForField(
+      Field field, Class<S> targetClazz) {
+    if (List.class.isAssignableFrom(field.getType())) {
+      var parameterType = getCollectionGenericType(field);
+      assertSettersAndGetters(
+          parameterType
+      );
+    } else if (Map.class.isAssignableFrom(field.getType())) {
+      var valueType = getMapValueType(field);
+
+      assertSettersAndGetters(
+          valueType
+      );
+    } else {
+      String fieldName = field.getName();
+
+      final String setMethodName = "set"
+          + fieldName.substring(0, 1).toUpperCase()
+          + fieldName.substring(1);
+      Method setMethod = findMethod(
+          targetClazz,
+          setMethodName,
+          field.getType());
+      final String getMethodName = "get"
+          + fieldName.substring(0, 1).toUpperCase()
+          + fieldName.substring(1);
+      final String isMethodName = "is"
+          + fieldName.substring(0, 1).toUpperCase()
+          + fieldName.substring(1);
+      Method getMethod = findMethod(
+          targetClazz,
+          getMethodName);
+      Method isMethod = findMethod(
+          targetClazz,
+          isMethodName);
+      assertNotNull(setMethod, "Set method " + setMethodName + " with parameter type "
+          + field.getType() + " was not found in class "
+          + targetClazz.getName() + " and field " + fieldName);
+      if (isBoolean(field.getType())) {
+        assertTrue(getMethod != null || isMethod != null, "Get method " + getMethodName + " or "
+            + isMethodName + " not found in class "
+            + targetClazz.getName() + " and field " + fieldName);
+        Class<?> getMethodReturnType =
+            getMethod != null ? getMethod.getReturnType() : isMethod.getReturnType();
+        assertSame(field.getType(), getMethodReturnType, "Get method " + getMethodName
+            + " return type " + getMethodReturnType.getName() + " should be of type "
+            + field.getType().getName() + " in class "
+            + targetClazz.getName() + " and field " + fieldName);
+      } else {
+        assertNotNull(getMethod, "Get method " + getMethodName + " not found in class "
+            + targetClazz.getName() + " and field " + fieldName);
+        assertSame(field.getType(), getMethod.getReturnType(), "Get method " + getMethodName
+            + " return type " + getMethod.getReturnType().getName() + " should be of type "
+            + field.getType().getName() + " in class "
+            + targetClazz.getName() + " and field " + fieldName);
+      }
+
+      assertSettersAndGetters(field.getType());
+    }
+  }
+
+  private boolean isBoolean(Class<?> valueClass) {
+    if (valueClass.isPrimitive()
+        && valueClass.getName().equals("boolean")) {
+      return true;
+    } else if (valueClass == Boolean.class) {
+      return true;
+    }
+    return false;
+  }
+
+  private boolean isValueType(Class<?> type) {
+    return String.class == type
+        || Number.class.isAssignableFrom(type)
+        || Boolean.class == type
+        || type.isPrimitive()
+        || Object.class == type;
+  }
+
+  private Class<?> getCollectionGenericType(Field collectionField) {
+    ParameterizedType listType = (ParameterizedType) collectionField.getGenericType();
+    return (Class<?>) listType.getActualTypeArguments()[0];
+  }
+
+  private Class<?> getMapValueType(Field mapField) {
+    ParameterizedType mapType = (ParameterizedType) mapField.getGenericType();
+    return getMapValueType(mapType);
+  }
+
+  private Class<?> getMapValueType(ParameterizedType mapType) {
+    if (mapType.getActualTypeArguments()[1] instanceof ParameterizedType parameterizedTypeValue) {
+      if (Map.class.isAssignableFrom((Class<?>) parameterizedTypeValue.getRawType())) {
+        return getMapValueType(parameterizedTypeValue);
+      }
+    }
+    return (Class<?>) mapType.getActualTypeArguments()[1];
+  }
+
+  private Method findMethod(Class<?> targetClazz, String name, Class<?>... parameterTypes)
+      throws SecurityException {
+    try {
+      return targetClazz.getMethod(name, parameterTypes);
+    } catch (NoSuchMethodException e) {
+      return null;
+    }
+  }
+}
