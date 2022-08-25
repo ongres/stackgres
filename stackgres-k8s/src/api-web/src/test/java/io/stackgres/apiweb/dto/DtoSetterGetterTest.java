5d9e3bd381f47137893637f210628443dfa571bc
Matteo Melli <matteom@ongres.com>
2022-08-25 15:26:18 +0200

    test: check for setters and getters in all DTOs

---
+++ stackgres-k8s/src/api-web/src/test/java/io/stackgres/apiweb/dto/DtoSetterGetterTest.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2019 OnGres, Inc.
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+package io.stackgres.apiweb.dto;
+
+import io.stackgres.apiweb.dto.backupconfig.BackupConfigDto;
+import io.stackgres.apiweb.dto.cluster.ClusterDto;
+import io.stackgres.apiweb.dto.configmap.ConfigMapDto;
+import io.stackgres.apiweb.dto.dbops.DbOpsDto;
+import io.stackgres.apiweb.dto.distributedlogs.DistributedLogsDto;
+import io.stackgres.apiweb.dto.event.EventDto;
+import io.stackgres.apiweb.dto.extension.ExtensionsDto;
+import io.stackgres.apiweb.dto.objectstorage.ObjectStorageDto;
+import io.stackgres.apiweb.dto.pgconfig.PostgresConfigDto;
+import io.stackgres.apiweb.dto.pooling.PoolingConfigDto;
+import io.stackgres.apiweb.dto.profile.ProfileDto;
+import io.stackgres.apiweb.dto.script.ScriptDto;
+import io.stackgres.apiweb.dto.secret.SecretDto;
+import io.stackgres.testutil.SetterGetterTestCase;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class DtoSetterGetterTest extends SetterGetterTestCase {
+
+  @ParameterizedTest
+  @ValueSource(classes = {
+      ClusterDto.class,
+      ProfileDto.class,
+      PostgresConfigDto.class,
+      PoolingConfigDto.class,
+      BackupConfigDto.class,
+      ObjectStorageDto.class,
+      DbOpsDto.class,
+      DistributedLogsDto.class,
+      ScriptDto.class,
+      ConfigMapDto.class,
+      EventDto.class,
+      ExtensionsDto.class,
+      SecretDto.class,
+  })
+  @Override
+  protected void assertSettersAndGetters(Class<?> sourceClazz) {
+    super.assertSettersAndGetters(sourceClazz);
+  }
+
+}
