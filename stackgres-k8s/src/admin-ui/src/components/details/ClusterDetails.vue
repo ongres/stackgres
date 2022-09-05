167725917f617fdc3585ddbc7f4cde2b9736a576
Olesya Saletska <olesya@ongres.com>
2022-09-05 12:07:02 +0200

    fix(ui): hide profile mismatch alerts on cluster status and config

--- stackgres-k8s/src/admin-ui/src/components/details/ClusterDetails.vue
+++ stackgres-k8s/src/admin-ui/src/components/details/ClusterDetails.vue
@@ -59,9 +59,11 @@
                                     {{ cluster.data.spec.sgInstanceProfile }} (Cores: {{ profile.data.spec.cpu }}, RAM: {{ profile.data.spec.memory }}) 
                                     <svg xmlns="http://www.w3.org/2000/svg" width="18.556" height="14.004" viewBox="0 0 18.556 14.004"><g transform="translate(0 -126.766)"><path d="M18.459,133.353c-.134-.269-3.359-6.587-9.18-6.587S.232,133.084.1,133.353a.93.93,0,0,0,0,.831c.135.269,3.36,6.586,9.18,6.586s9.046-6.317,9.18-6.586A.93.93,0,0,0,18.459,133.353Zm-9.18,5.558c-3.9,0-6.516-3.851-7.284-5.142.767-1.293,3.382-5.143,7.284-5.143s6.516,3.85,7.284,5.143C15.795,135.06,13.18,138.911,9.278,138.911Z" transform="translate(0 0)"/><path d="M9.751,130.857a3.206,3.206,0,1,0,3.207,3.207A3.21,3.21,0,0,0,9.751,130.857Z" transform="translate(-0.472 -0.295)"/></g></svg>
                                 </router-link>
-                                <template v-if="clusterProfileMismatch(cluster, profile)">
+                                <!--Temporarily disabled until REST API response data is set properly-->
+                                
+                                <!--<template v-if="clusterProfileMismatch(cluster, profile)">
                                     <span class="helpTooltip alert" data-tooltip="This profile has been modified recently. Cluster must be restarted in order to apply such changes."></span>
-                                </template>
+                                </template>-->
                             </td>
                         </template>
                         <template v-else>
