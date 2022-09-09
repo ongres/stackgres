4540f0908893bc398b784145605efc4fb7d6b898
Matteo Melli <matteom@ongres.com>
2022-09-09 10:24:56 +0200

    feat: improve JVM startup with Java CDS

--- stackgres-k8s/src/distributedlogs-controller/src/main/docker/stackgres-distributedlogs-controller.jvm.sh
+++ stackgres-k8s/src/distributedlogs-controller/src/main/docker/stackgres-distributedlogs-controller.jvm.sh
@@ -13,7 +13,29 @@ if [ "$DISTRIBUTEDLOGS_CONTROLLER_SHOW_STACK_TRACES" = true ]
 then
   APP_OPTS="$APP_OPTS -Dquarkus.log.console.format=%d{yyyy-MM-dd HH:mm:ss,SSS} %-5p [%c{4.}] (%t) %s%e%n"
 fi
+if [ "$JAVA_CDS_GENERATION" = true ]
+then
+  export KUBERNETES_MASTER=240.0.0.1
+  java \
+    -XX:ArchiveClassesAtExit=/app/quarkus-run.jsa \
+    -XX:MaxRAMPercentage=75.0 \
+    -Djava.net.preferIPv4Stack=true \
+    -Djava.awt.headless=true \
+    -Djava.util.logging.manager=org.jboss.logmanager.LogManager \
+    $JAVA_OPTS $DEBUG_JAVA_OPTS -jar /app/quarkus-run.jar \
+    -Dquarkus.http.host=0.0.0.0 \
+    $APP_OPTS &
+  PID=$!
+  until curl -s localhost:8080/q/health/ready
+  do
+    sleep 1
+  done
+  kill "$PID"
+  wait "$PID" || true
+  exit
+fi
 exec java \
+  -XX:SharedArchiveFile=/app/quarkus-run.jsa \
   -XX:MaxRAMPercentage=75.0 \
   -Djava.net.preferIPv4Stack=true \
   -Djava.awt.headless=true \
