3c7ada475623c030fdbc2ae3ccd7685be4033b81
Matteo Melli <matteom@ongres.com>
2022-10-07 13:54:28 +0200

    fix: avoid errors for old versions of a SGBackup, SGDbOps and SGScript

--- stackgres-k8s/src/operator/src/main/java/io/stackgres/operator/mutation/dbops/DbOpsAnnotationMutator.java
+++ stackgres-k8s/src/operator/src/main/java/io/stackgres/operator/mutation/dbops/DbOpsAnnotationMutator.java
@@ -5,8 +5,13 @@
 
 package io.stackgres.operator.mutation.dbops;
 
+import java.util.Map;
+import java.util.Optional;
+
 import javax.enterprise.context.ApplicationScoped;
 
+import io.stackgres.common.StackGresContext;
+import io.stackgres.common.StackGresVersion;
 import io.stackgres.common.crd.sgdbops.StackGresDbOps;
 import io.stackgres.operator.common.DbOpsReview;
 import io.stackgres.operator.mutation.AbstractAnnotationMutator;
@@ -15,4 +20,16 @@ import io.stackgres.operator.mutation.AbstractAnnotationMutator;
 public class DbOpsAnnotationMutator
     extends AbstractAnnotationMutator<StackGresDbOps, DbOpsReview>
     implements DbOpsMutator {
+
+  private static final long VERSION_1_2 = StackGresVersion.V_1_2.getVersionAsNumber();
+
+  @Override
+  public Optional<Map<String, String>> getAnnotationsToOverwrite(StackGresDbOps resource) {
+    final long version = StackGresVersion.getStackGresVersionAsNumber(resource);
+    if (VERSION_1_2 > version) {
+      return Optional.of(Map.of(StackGresContext.VERSION_KEY, StackGresVersion.V_1_2.getVersion()));
+    }
+    return Optional.empty();
+  }
+
 }
