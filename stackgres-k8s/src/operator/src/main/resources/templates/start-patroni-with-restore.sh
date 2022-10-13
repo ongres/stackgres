aa36f3f03e1aa5ce52238725aad9c2afb0ed8f2f
Matteo Melli <matteom@ongres.com>
2022-10-14 00:23:56 +0200

    fix: cluster crash-Loop due to permission change in data dir

--- stackgres-k8s/src/operator/src/main/resources/templates/start-patroni-with-restore.sh
+++ stackgres-k8s/src/operator/src/main/resources/templates/start-patroni-with-restore.sh
@@ -74,11 +74,11 @@ watchdog:
 tags: {}
 EOF
 
-cat << EOF > '${PATRONI_CONFIG_PATH}/setup-data-path.sh'
+cat << EOF > "${PATRONI_CONFIG_PATH}/setup-data-path.sh"
 mkdir -p "$PG_DATA_PATH"
 chmod -R 700 "$PG_DATA_PATH"
 EOF
-chmod 755 '${PATRONI_CONFIG_PATH}/setup-data-path.sh'
+chmod 755 "${PATRONI_CONFIG_PATH}/setup-data-path.sh"
 
 cat << EOF > "$PATRONI_CONFIG_PATH/bootstrap"
 #!/bin/sh
