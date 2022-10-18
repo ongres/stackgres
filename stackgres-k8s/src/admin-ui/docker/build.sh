c0bd70d52d02b4302863693ea8b8f87977e5d2a7
Jorge Solórzano <jorsol@ongres.com>
2022-10-18 21:36:24 +0200

    chore: Update ubi8 base images
    Signed-off-by: Jorge Solórzano <jorsol@ongres.com>

--- stackgres-k8s/src/admin-ui/docker/build.sh
+++ stackgres-k8s/src/admin-ui/docker/build.sh
@@ -2,7 +2,7 @@
 
 set -e
 ADMINUI_IMAGE_NAME="${ADMINUI_IMAGE_NAME:-"stackgres/admin-ui:${IMAGE_TAG%-jvm}"}"
-BASE_IMAGE="registry.access.redhat.com/ubi8/nginx-118:1-42"
+BASE_IMAGE="registry.access.redhat.com/ubi8/nginx-120:1-60.1665590917"
 TARGET_ADMINUI_IMAGE_NAME="${TARGET_ADMINUI_IMAGE_NAME:-$ADMINUI_IMAGE_NAME}"
 
 docker build -t "$TARGET_ADMINUI_IMAGE_NAME" --build-arg BASE_IMAGE="$BASE_IMAGE" -f admin-ui/docker/Dockerfile admin-ui
