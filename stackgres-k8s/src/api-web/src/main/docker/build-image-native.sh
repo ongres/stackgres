795038ba65800f1ca79ec18bc717834307b275b9
Guilherme Elias <elias@ongres.com>
2023-01-06 10:32:11 -0300

    chore: Updated the native images to use the latest ubi 8 minimal version
    - Changed the logic to use latest kubectl version
    
    Signed-off-by: Guilherme Elias <elias@ongres.com>

--- stackgres-k8s/src/api-web/src/main/docker/build-image-native.sh
+++ stackgres-k8s/src/api-web/src/main/docker/build-image-native.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-BASE_IMAGE="registry.access.redhat.com/ubi8-minimal:8.6-941"
+BASE_IMAGE="registry.access.redhat.com/ubi8-minimal:8.7-1031"
 
 RESTAPI_IMAGE_NAME="${RESTAPI_IMAGE_NAME:-"stackgres/restapi:main"}"
 TARGET_RESTAPI_IMAGE_NAME="${TARGET_RESTAPI_IMAGE_NAME:-$RESTAPI_IMAGE_NAME}"
