6f751c9ff747d2c7f00c8290efd227cd573d1c6f
Matteo Melli <matteom@ongres.com>
2023-02-21 15:40:30 +0100

    ci: fix restapi image build

--- stackgres-k8s/src/admin-ui/docker/build.sh
+++ stackgres-k8s/src/admin-ui/docker/build.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 
 set -e
-ADMINUI_IMAGE_NAME="${ADMINUI_IMAGE_NAME:-"stackgres/admin-ui:${IMAGE_TAG%-jvm}"}"
+
+PROJECT_PATH="$(dirname "$0")/../../../.."
+STACKGRES_VERSION="$(sh "$PROJECT_PATH"/stackgres-k8s/ci/build/version.sh)"
+
+cd "$PROJECT_PATH"/stackgres-k8s/src
+
+ADMINUI_IMAGE_NAME="${ADMINUI_IMAGE_NAME:-"stackgres/admin-ui:main"}"
 BASE_IMAGE="registry.access.redhat.com/ubi8/nginx-120:1-74.1669834626"
 TARGET_ADMINUI_IMAGE_NAME="${TARGET_ADMINUI_IMAGE_NAME:-$ADMINUI_IMAGE_NAME}"
 
-docker build -t "$TARGET_ADMINUI_IMAGE_NAME" --build-arg BASE_IMAGE="$BASE_IMAGE" -f admin-ui/docker/Dockerfile admin-ui
+docker build -t "$TARGET_ADMINUI_IMAGE_NAME" \
+  --build-arg BASE_IMAGE="$BASE_IMAGE" \
+  --build-arg STACKGRES_VERSION="$STACKGRES_VERSION" \
+  -f admin-ui/docker/Dockerfile admin-ui
--- stackgres-k8s/src/operator/src/main/docker/build-image-jvm.sh
+++ stackgres-k8s/src/operator/src/main/docker/build-image-jvm.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+PROJECT_PATH="$(dirname "$0")/../../../../../.."
+STACKGRES_VERSION="$(sh "$PROJECT_PATH"/stackgres-k8s/ci/build/version.sh)"
+
+cd "$PROJECT_PATH"/stackgres-k8s/src
+
 BASE_IMAGE="registry.access.redhat.com/ubi8/openjdk-17-runtime:1.14-8"
 
 OPERATOR_IMAGE_NAME="${OPERATOR_IMAGE_NAME:-"stackgres/operator:main-jvm"}"
@@ -9,4 +14,5 @@ TARGET_OPERATOR_IMAGE_NAME="${TARGET_OPERATOR_IMAGE_NAME:-$OPERATOR_IMAGE_NAME}"
 
 docker build -t "$TARGET_OPERATOR_IMAGE_NAME" \
   --build-arg BASE_IMAGE="$BASE_IMAGE" \
+  --build-arg STACKGRES_VERSION="$STACKGRES_VERSION" \
   -f operator/src/main/docker/Dockerfile.jvm operator
