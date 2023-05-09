dd56eb94afda606f3a67c4ba7121dd198bd9d0a5
Roberto Rodriguez <rcrodriguez@ongres.com>
2023-05-08 19:34:47 -0600

    test: Replace unit test clusterWithHugePages_shouldAddHugePagesVolumes with more generic

--- stackgres-k8s/src/operator/src/test/java/io/stackgres/operator/conciliation/factory/cluster/PodTemplateSpecFactoryTest.java
+++ stackgres-k8s/src/operator/src/test/java/io/stackgres/operator/conciliation/factory/cluster/PodTemplateSpecFactoryTest.java
@@ -5,18 +5,8 @@
 
 package io.stackgres.operator.conciliation.factory.cluster;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.when;
-
-import java.util.List;
-import java.util.Map;
-
-import io.fabric8.kubernetes.api.model.Container;
-import io.fabric8.kubernetes.api.model.PodSecurityContext;
-import io.fabric8.kubernetes.api.model.VolumeBuilder;
-import io.fabric8.kubernetes.api.model.VolumeMountBuilder;
+import io.fabric8.kubernetes.api.model.*;
 import io.stackgres.common.StackGresComponent;
-import io.stackgres.common.StackGresVolume;
 import io.stackgres.common.crd.sgcluster.StackGresCluster;
 import io.stackgres.common.fixture.Fixtures;
 import io.stackgres.common.labels.ClusterLabelMapper;
@@ -26,12 +16,20 @@ import io.stackgres.operator.conciliation.InitContainerFactoryDiscover;
 import io.stackgres.operator.conciliation.cluster.StackGresClusterContext;
 import io.stackgres.operator.conciliation.factory.ContainerFactory;
 import io.stackgres.operator.conciliation.factory.ResourceFactory;
+import io.stackgres.testutil.JsonUtil;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class PodTemplateSpecFactoryTest {
@@ -79,63 +77,39 @@ class PodTemplateSpecFactoryTest {
         initContainerFactoryDiscoverer);
     cluster = Fixtures.cluster().loadDefault().get();
     cluster.getSpec().getPostgres().setVersion(POSTGRES_VERSION);
+  }
+
+  @Test
+  void clusterWithVolumes_shouldAddOnlyUsedVolumes() {
     when(clusterContainerContext.getClusterContext()).thenReturn(clusterContext);
     when(clusterContext.getSource()).thenReturn(cluster);
     when(clusterContext.getCluster()).thenReturn(cluster);
     when(labelFactory.labelMapper()).thenReturn(labelMapper);
-  }
-
-  @Test
-  void clusterWithHugePages_shouldAddHugePagesVolumes() {
     when(containerFactoryDiscoverer.discoverContainers(clusterContainerContext))
         .thenReturn(List.of(patroniContainerFactory));
     when(patroniContainerFactory.getContainer(clusterContainerContext))
         .thenReturn(patroniContainer);
     when(patroniContainer.getVolumeMounts()).thenReturn(List.of(
         new VolumeMountBuilder()
-        .withName(StackGresVolume.HUGEPAGES_2M.getName())
-        .build(),
-        new VolumeMountBuilder()
-        .withName(StackGresVolume.HUGEPAGES_1G.getName())
-        .build()));
-    when(clusterContainerContext.availableVolumes()).thenReturn(Map.of(
-        StackGresVolume.HUGEPAGES_2M.getName(),
-        new VolumeBuilder()
-        .withName(StackGresVolume.HUGEPAGES_2M.getName())
-        .withNewEmptyDir()
-        .withMedium("HugePages-2Mi")
-        .endEmptyDir()
-        .build(),
-        StackGresVolume.HUGEPAGES_1G.getName(),
-        new VolumeBuilder()
-        .withName(StackGresVolume.HUGEPAGES_1G.getName())
-        .withNewEmptyDir()
-        .withMedium("HugePages-1Gi")
-        .endEmptyDir()
-        .build()));
-    var podTemplateSpec = podTemplateSpecFactory.getPodTemplateSpec(clusterContainerContext);
-    assertTrue(podTemplateSpec.getSpec().getSpec().getVolumes().stream()
-        .anyMatch(volume -> volume.getName()
-            .equals(StackGresVolume.HUGEPAGES_2M.getName())));
-    assertTrue(podTemplateSpec.getSpec().getSpec().getVolumes().stream()
-        .filter(volume -> volume.getName()
-            .equals(StackGresVolume.HUGEPAGES_2M.getName()))
-        .anyMatch(volume -> volume.getEmptyDir() != null));
-    assertTrue(podTemplateSpec.getSpec().getSpec().getVolumes().stream()
-        .filter(volume -> volume.getName()
-            .equals(StackGresVolume.HUGEPAGES_2M.getName()))
-        .anyMatch(volume -> volume.getEmptyDir().getMedium().equals("HugePages-2Mi")));
-    assertTrue(podTemplateSpec.getSpec().getSpec().getVolumes().stream()
-        .anyMatch(volume -> volume.getName()
-            .equals(StackGresVolume.HUGEPAGES_1G.getName())));
-    assertTrue(podTemplateSpec.getSpec().getSpec().getVolumes().stream()
-        .filter(volume -> volume.getName()
-            .equals(StackGresVolume.HUGEPAGES_1G.getName()))
-        .anyMatch(volume -> volume.getEmptyDir() != null));
-    assertTrue(podTemplateSpec.getSpec().getSpec().getVolumes().stream()
-        .filter(volume -> volume.getName()
-            .equals(StackGresVolume.HUGEPAGES_1G.getName()))
-        .anyMatch(volume -> volume.getEmptyDir().getMedium().equals("HugePages-1Gi")));
-  }
+            .withName("used-volume")
+            .build()));
 
+    Volume usedVolume = new VolumeBuilder()
+        .withName("used-volume")
+        .build();
+    var availableVolumes = Map.of(
+        "used-volume",
+        JsonUtil.copy(usedVolume),
+        "unused-volume",
+        new VolumeBuilder()
+            .withName("unused-volume")
+            .build());
+
+    when(clusterContainerContext.availableVolumes()).thenReturn(availableVolumes);
+    var podTemplateSpec = podTemplateSpecFactory.getPodTemplateSpec(clusterContainerContext);
+    assertTrue(podTemplateSpec.getSpec().getSpec().getVolumes().size() == 1);
+    assertTrue(
+        podTemplateSpec.getSpec().getSpec().getVolumes().get(0).getName().equals("used-volume"));
+    assertTrue(podTemplateSpec.getSpec().getSpec().getVolumes().get(0).equals(usedVolume));
+  }
 }
