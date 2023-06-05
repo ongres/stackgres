6eda17567779955eb4b5018d0b921e07f235416a
Matteo Melli <matteom@ongres.com>
2023-06-06 01:37:17 +0200

    fix: Major version upgrade must never change extension version

--- stackgres-k8s/src/common/src/test/java/io/stackgres/common/extension/ExtensionMetadataManagerMock.java
+++ stackgres-k8s/src/common/src/test/java/io/stackgres/common/extension/ExtensionMetadataManagerMock.java
@@ -6,12 +6,15 @@
 package io.stackgres.common.extension;
 
 import java.net.URI;
+import java.util.List;
 
 import io.quarkus.test.Mock;
 import io.stackgres.common.fixture.Fixtures;
 
 @Mock
 public class ExtensionMetadataManagerMock extends ExtensionMetadataManager {
+
+  List<StackGresExtension> extraExtensions = List.of();
 
   public ExtensionMetadataManagerMock() {
     super(null, null);
@@ -21,7 +24,14 @@ public class ExtensionMetadataManagerMock extends ExtensionMetadataManager {
   synchronized ExtensionMetadataCache getExtensionsMetadata() {
     return ExtensionMetadataCache.from(
         URI.create("https://extensions.strackgres.io/postgres/repository"),
-        Fixtures.extensionMetadata().loadDefault().get());
+        Fixtures.extensionMetadata().loadDefault()
+        .getBuilder()
+        .addAllToExtensions(extraExtensions)
+        .build());
+  }
+
+  public void setExtraExtensions(List<StackGresExtension> extraExtensions) {
+    this.extraExtensions = extraExtensions;
   }
 
 }
