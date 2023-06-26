438e4beeeb50f949191f2000e75fc00787a9fe41
Matteo Melli <matteom@ongres.com>
2023-06-26 16:00:54 +0200

    e2e: fixed tests for sharded cluster

--- stackgres-k8s/src/admin-ui/cypress/support/commands.js
+++ stackgres-k8s/src/admin-ui/cypress/support/commands.js
@@ -121,3 +121,59 @@ Cypress.Commands.add('deleteCluster', (namespace, clusterName) => {
         })
     })
 });
+
+Cypress.Commands.add('deleteShardedCluster', (namespace, clusterName) => {
+    cy.getResources('sgshardedclusters').then(resp => {
+        let cluster = resp.find(el => (el.metadata.namespace === namespace) && (el.metadata.name === clusterName))
+
+        cy.deleteCRD('sgshardedclusters', {
+            metadata: {
+                name: cluster.metadata.name,
+                namespace: cluster.metadata.namespace
+            }
+        })
+        .then( () => {
+            cy.deleteCRD('sginstanceprofiles', {
+                metadata: {
+                    name: cluster.spec.coordinator.sgInstanceProfile,
+                    namespace: cluster.metadata.namespace
+                }
+            });
+        
+            cy.deleteCRD('sgpgconfigs', {
+                metadata: {
+                    name: cluster.spec.coordinator.configurations.sgPostgresConfig,
+                    namespace: cluster.metadata.namespace
+                }
+            });
+        
+            cy.deleteCRD('sgpoolconfigs', {
+                metadata: {
+                    name: cluster.spec.coordinator.configurations.sgPoolingConfig,
+                    namespace: cluster.metadata.namespace
+                }
+            })
+
+          cy.deleteCRD('sginstanceprofiles', {
+              metadata: {
+                  name: cluster.spec.shards.sgInstanceProfile,
+                  namespace: cluster.metadata.namespace
+              }
+          });
+      
+          cy.deleteCRD('sgpgconfigs', {
+              metadata: {
+                  name: cluster.spec.shards.configurations.sgPostgresConfig,
+                  namespace: cluster.metadata.namespace
+              }
+          });
+      
+          cy.deleteCRD('sgpoolconfigs', {
+              metadata: {
+                  name: cluster.spec.shards.configurations.sgPoolingConfig,
+                  namespace: cluster.metadata.namespace
+              }
+          })
+        })
+    })
+});
