db764061447bddbb42e4ab80c0a40000bdb49389
Matteo Melli <matteom@ongres.com>
2023-06-27 23:20:44 +0200

    e2e(ui): fixed sharded cluster checks

--- stackgres-k8s/src/admin-ui/cypress/e2e/03-forms/10-create-sgshardedcluster.cy.js
+++ stackgres-k8s/src/admin-ui/cypress/e2e/03-forms/10-create-sgshardedcluster.cy.js
@@ -2749,7 +2749,7 @@ describe('Create SGShardedCluster', () => {
             .should('eq', false)
         cy.get('@putCluster')
             .its('request.body.spec.postgres.extensions')
-            .should('have.lengthOf', 5)
+            .should('have.lengthOf', 4)
             .then((list) => Cypress._.map(list, 'name'))
             .should('include', "pg_repack")
             .and('include', "plpgsql_check")
