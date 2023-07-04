97a3551ae0d08dd50d845cc04d4edd5d9441ce20
Matteo Melli <matteom@ongres.com>
2023-07-04 19:15:31 +0200

    chore: added script to semi-automate release of operator bundle

--- stackgres-k8s/install/operator-sdk/stackgres-operator/deploy-to-red-hat-community.sh
+++ stackgres-k8s/install/operator-sdk/stackgres-operator/deploy-to-red-hat-community.sh
@@ -1,72 +1,9 @@
 #!/bin/sh
 
-set -e
-
-if [ "$#" -lt 1 ]
-then
-  >&2 echo "Must specify git URL"
-  exit 1
-fi
-
-GIT_URL="$1"
-
-PROJECT_PATH=../../../../
-
-cd "$(dirname "$0")"
-
-STACKGRES_VERSION="${STACKGRES_VERSION:-$(sh "$PROJECT_PATH"/stackgres-k8s/ci/build/version.sh)}"
-
-mkdir -p target
-echo "Cloning Upstream Red Hat Certified"
-rm -rf target/upstream-red-hat-community
-git clone https://github.com/redhat-openshift-ecosystem/community-operators-prod target/upstream-red-hat-community
-echo "Cloning Red Hat Maretplace fork for StackGres from $GIT_URL"
-rm -rf target/red-hat-community
-git clone "$GIT_URL" target/red-hat-community
-if [ "$(git -C target/red-hat-community rev-list --max-parents=0 HEAD)" != "$(git -C target/upstream-red-hat-community rev-list --max-parents=0 HEAD)" ]
-then
-  >&2 echo "Git repository $GIT_URL seems not a fork of https://github.com/redhat-openshift-ecosystem/community-operators-prod"
-  exit 1
-fi
-
-echo "Copying new files to path operators/stackgres/$STACKGRES_VERSION from quay.io/stackgres/helm-operator-bundle:$STACKGRES_VERSION-openshift"
-(
-mkdir "target/red-hat-community/operators/stackgres/$STACKGRES_VERSION"
-cd "target/red-hat-community/operators/stackgres/$STACKGRES_VERSION"
-docker pull quay.io/stackgres/helm-operator-bundle:$STACKGRES_VERSION-openshift
-docker save quay.io/stackgres/helm-operator-bundle:$STACKGRES_VERSION-openshift | tar tv | tr -s ' ' | cut -d ' ' -f 6 | grep -F layer.tar \
-  | while read LAYER
-    do
-      docker save quay.io/stackgres/helm-operator-bundle:$STACKGRES_VERSION-openshift | tar xO "$LAYER" | tar xv
-    done
-)
-git -C target/red-hat-community add .
-git -C target/red-hat-community status
-
-echo "Pinning images:"
-echo
-(
-cd "target/red-hat-community/operators/stackgres/$STACKGRES_VERSION"
-IMAGES="$(grep 'image:' "manifests/stackgres.clusterserviceversion.yaml" | tr -d ' ' | cut -d : -f 2-)"
-echo "$IMAGES" \
-  | while read -r IMAGE
-    do
-      DIGEST="$(docker buildx imagetools inspect "$IMAGE" | grep '^Digest:' | tr -d ' ' | cut -d : -f 2-)"
-      IMAGE_NAME="${IMAGE%%:*}"
-      IMAGE_NAME="${IMAGE_NAME%%@sha256}"
-      echo "Pinning $IMAGE to $IMAGE_NAME@$DIGEST"
-      sed -i "s#image: $IMAGE\$#image: $IMAGE_NAME@$DIGEST#" "manifests/stackgres.clusterserviceversion.yaml"
-    done
-echo
-)
-git -C target/red-hat-community diff
-echo "Pinning done!"
-
-git -C target/red-hat-community add .
-git -C target/red-hat-community status
-echo "Creating commit"
-git -C target/red-hat-community commit -m "operator stackgres (${STACKGRES_VERSION})"
-echo "To push use the following command"
-echo git -C "$PROJECT_PATH"/stackgres-k8s/install/operator-sdk/stackgres-operator/target/red-hat-community push
-echo
-echo "To create the PR go to: https://github.com/redhat-openshift-ecosystem/community-operators-prod/compare"
+export UPSTREAM_NAME="Red Hat Community"
+export UPSTREAM_GIT_URL="https://github.com/redhat-openshift-ecosystem/community-operators-prod"
+export FORK_GIT_URL="${FORK_GIT_URL:-$1}"
+export PROJECT_NAME="stackgres-community"
+export DO_PIN_IMAGES=true
+export HELM_OPERATOR_BUNDLE_IMAGE_TAG_SUFFIX=-openshift
+exec sh $(printf %s "$-" | grep -q x && printf %s -x) "$(dirname "$0")/deploy.sh"
---
+++ stackgres-k8s/install/operator-sdk/stackgres-operator/deploy.sh
@@ -0,0 +1,123 @@
+#!/bin/sh
+
+set -e
+
+if [ -z "$UPSTREAM_NAME" ]
+then
+  >&2 echo "Must set UPSTREAM_NAME env var"
+  exit 1
+fi
+
+if [ -z "$UPSTREAM_GIT_URL" ]
+then
+  >&2 echo "Must set UPSTREAM_GIT_URL env var"
+  exit 1
+fi
+
+if [ -z "$FORK_GIT_URL" ]
+then
+  >&2 echo "Must set FORK_GIT_URL env var"
+  exit 1
+fi
+
+if [ -z "$PROJECT_NAME" ]
+then
+  >&2 echo "Must set PROJECT_NAME env var"
+  exit 1
+fi
+
+PROJECT_PATH=../../../../
+
+cd "$(dirname "$0")"
+
+STACKGRES_VERSION="${STACKGRES_VERSION:-$(sh "$PROJECT_PATH"/stackgres-k8s/ci/build/version.sh)}"
+
+mkdir -p target
+UPSTREAM_SUFFIX="$(printf %s "$UPSTREAM_NAME" | tr '[A-Z] ' '[a-z]-' | tr -dc '[a-z0-9]-')"
+UPSTREAM_GIT_PATH=target/"upstream-$UPSTREAM_SUFFIX"
+FORK_GIT_PATH=target/"fork-$UPSTREAM_SUFFIX"
+HELM_OPERATOR_BUNDLE_IMAGE_TAG="${STACKGRES_VERSION}$HELM_OPERATOR_BUNDLE_IMAGE_TAG_SUFFIX"
+
+if [ -d "$UPSTREAM_GIT_PATH" ] && git -C "$UPSTREAM_GIT_PATH" remote -v | grep -qF "$UPSTREAM_GIT_URL"
+then
+  echo "Resetting Upstream $UPSTREAM_NAME from $UPSTREAM_GIT_URL"
+  git -C "$UPSTREAM_GIT_PATH" fetch
+  git -C "$UPSTREAM_GIT_PATH" reset --hard HEAD
+  git -C "$UPSTREAM_GIT_PATH" checkout main
+  git -C "$UPSTREAM_GIT_PATH" reset --hard origin/main
+else
+  echo "Cloning Upstream $UPSTREAM_NAME from $UPSTREAM_GIT_URL"
+  rm -rf "$UPSTREAM_GIT_PATH"
+  git clone "$UPSTREAM_GIT_URL" "$UPSTREAM_GIT_PATH"
+fi
+
+if [ -d "$FORK_GIT_PATH" ] && git -C "$FORK_GIT_PATH" remote -v | grep -qF "$FORK_GIT_URL"
+then
+  echo "Resetting OperatorHub fork for StackGres from $FORK_GIT_URL"
+  git -C "$FORK_GIT_PATH" fetch
+  git -C "$FORK_GIT_PATH" reset --hard HEAD
+  git -C "$FORK_GIT_PATH" checkout main
+  git -C "$FORK_GIT_PATH" reset --hard origin/main
+else
+  echo "Cloning OperatorHub fork for StackGres from $FORK_GIT_URL"
+  rm -rf "$FORK_GIT_PATH"
+  git clone "$FORK_GIT_URL" "$FORK_GIT_PATH"
+fi
+
+if [ "$(git -C "$FORK_GIT_PATH" rev-list --max-parents=0 HEAD)" != "$(git -C "$UPSTREAM_GIT_PATH" rev-list --max-parents=0 HEAD)" ]
+then
+  >&2 echo "Git repository $FORK_GIT_URL seems not a fork of $UPSTREAM_GIT_URL"
+  exit 1
+fi
+
+echo "Copying new files to path operators/$PROJECT_NAME/$STACKGRES_VERSION from quay.io/stackgres/helm-operator-bundle:$HELM_OPERATOR_BUNDLE_IMAGE_TAG"
+(
+rm -rf "$FORK_GIT_PATH/operators/$PROJECT_NAME/$STACKGRES_VERSION"
+mkdir -p "$FORK_GIT_PATH/operators/$PROJECT_NAME/$STACKGRES_VERSION"
+cd "$FORK_GIT_PATH/operators/$PROJECT_NAME/$STACKGRES_VERSION"
+docker pull quay.io/stackgres/helm-operator-bundle:$HELM_OPERATOR_BUNDLE_IMAGE_TAG
+docker save quay.io/stackgres/helm-operator-bundle:$HELM_OPERATOR_BUNDLE_IMAGE_TAG | tar tv | tr -s ' ' | cut -d ' ' -f 6 | grep -F layer.tar \
+  | while read LAYER
+    do
+      docker save quay.io/stackgres/helm-operator-bundle:$HELM_OPERATOR_BUNDLE_IMAGE_TAG | tar xO "$LAYER" | tar xv
+    done
+)
+find "$FORK_GIT_PATH" -name '.wh*' \
+  | while read FILE
+    do
+      rm "$FILE"
+    done
+cp ci-"$UPSTREAM_SUFFIX".yaml "$FORK_GIT_PATH/operators/$PROJECT_NAME/ci.yaml"
+git -C "$FORK_GIT_PATH" add .
+git -C "$FORK_GIT_PATH" status
+
+if [ "$DO_PIN_IMAGES" = true ]
+then
+  echo "Pinning images:"
+  echo
+  (
+  cd "$FORK_GIT_PATH/operators/$PROJECT_NAME/$STACKGRES_VERSION"
+  IMAGES="$(grep 'image:' "manifests/stackgres.clusterserviceversion.yaml" | tr -d ' ' | cut -d : -f 2-)"
+  echo "$IMAGES" \
+    | while read -r IMAGE
+      do
+        DIGEST="$(docker buildx imagetools inspect "$IMAGE" | grep '^Digest:' | tr -d ' ' | cut -d : -f 2-)"
+        IMAGE_NAME="${IMAGE%%:*}"
+        IMAGE_NAME="${IMAGE_NAME%%@sha256}"
+        echo "Pinning $IMAGE to $IMAGE_NAME@$DIGEST"
+        sed -i "s#image: $IMAGE\$#image: $IMAGE_NAME@$DIGEST#" "manifests/stackgres.clusterserviceversion.yaml"
+      done
+  echo
+  )
+  git -C "$FORK_GIT_PATH" diff
+  echo "Pinning done!"
+fi
+
+git -C "$FORK_GIT_PATH" add .
+git -C "$FORK_GIT_PATH" status
+echo "Creating commit"
+git -C "$FORK_GIT_PATH" commit -m "operator $PROJECT_NAME (${STACKGRES_VERSION})"
+echo "To push use the following command"
+echo git -C "$PROJECT_PATH"/stackgres-k8s/install/operator-sdk/stackgres-operator/"$FORK_GIT_PATH" push
+echo
+echo "To create the PR go to: $UPSTREAM_GIT_URL/compare"
