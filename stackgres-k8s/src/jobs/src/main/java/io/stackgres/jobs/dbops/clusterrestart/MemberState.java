880e5cfa766eb01e8f4710d2f595548f3e8ce52b
Jorge Solórzano <jorsol@ongres.com>
2023-09-15 00:38:07 +0200

    fix: Add "streaming", "in archive recovery" as MemberState.RUNNING used by patroni since 3.0.4
    Signed-off-by: Jorge Solórzano <jorsol@ongres.com>

--- stackgres-k8s/src/jobs/src/main/java/io/stackgres/jobs/dbops/clusterrestart/MemberState.java
+++ stackgres-k8s/src/jobs/src/main/java/io/stackgres/jobs/dbops/clusterrestart/MemberState.java
@@ -9,4 +9,14 @@ public enum MemberState {
 
   RUNNING, STOPPED, STARTING, RESTARTING;
 
+  static MemberState fromString(String state) {
+    return switch (state) {
+      case "running", "streaming", "in archive recovery" -> MemberState.RUNNING;
+      case "stopped" -> MemberState.STOPPED;
+      case "starting" -> MemberState.STARTING;
+      case "restarting" -> MemberState.RESTARTING;
+      default -> null;
+    };
+  }
+
 }
