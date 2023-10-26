baf9d992fe5c994b194f6d1f95903697976628af
Luis Garcia <lgarcia@ongres.com>
2023-10-26 17:23:27 +0200

    chore(ui): refactor local proxy for grafana endpoint

--- stackgres-k8s/src/admin-ui/vue.config.js
+++ stackgres-k8s/src/admin-ui/vue.config.js
@@ -1,31 +1,29 @@
 module.exports = {
-    runtimeCompiler: true,
-    publicPath: '/admin/',
-    lintOnSave: false,
-    devServer:{
-      proxy: {
-        '^/stackgres': {
-          target: process.env.VUE_APP_API_PROXY_URL,
-          pathRewrite: { '^/stackgres' : '' },
-          changeOrigin: true,
-          secure: false,
-          logLevel: 'debug'
-        },
-        '^/grafana': {
-          target: process.env.VUE_APP_GRAFANA_PROXY_URL,
-          pathRewrite: { '^/grafana' : '' },
-          changeOrigin: true,
-          secure: false,
-          logLevel: 'debug'
-        },
-        '^/d/^': {
-          target: process.env.VUE_APP_GRAFANA_DASHBOARD_PROXY_URL,
-          pathRewrite: { '^/d/^' : '' },
-          changeOrigin: true,
-          secure: false,
-          logLevel: 'debug'
-        }
+  runtimeCompiler: true,
+  publicPath: '/admin/',
+  lintOnSave: false,
+  devServer:{
+    proxy: {
+      '^/stackgres': {
+        target: process.env.VUE_APP_API_PROXY_URL,
+        pathRewrite: { '^/stackgres' : '' },
+        changeOrigin: true,
+        secure: false,
+        logLevel: 'debug'
+      },
+      '^/grafana': {
+        target: process.env.VUE_APP_GRAFANA_PROXY_URL,
+        changeOrigin: true,
+        secure: false,
+        logLevel: 'debug'
+      },
+      '^/d/^': {
+        target: process.env.VUE_APP_GRAFANA_DASHBOARD_PROXY_URL,
+        pathRewrite: { '^/d/^' : '' },
+        changeOrigin: true,
+        secure: false,
+        logLevel: 'debug'
       }
     }
   }
-  
+}
