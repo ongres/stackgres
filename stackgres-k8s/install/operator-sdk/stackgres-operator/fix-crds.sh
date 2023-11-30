787a4a09ae40d65ea9c89146124dfa8d1dbbee40
Matteo Melli <matteom@ongres.com>
2023-11-30 23:19:54 +0100

    e2e: add fix to test buggy 1.6.0 operator bundle

--- stackgres-k8s/install/operator-sdk/stackgres-operator/fix-crds.sh
+++ stackgres-k8s/install/operator-sdk/stackgres-operator/fix-crds.sh
@@ -9,15 +9,19 @@ do
   CRD_SINGULAR="$(yq -r .spec.names.singular "$CRD")"
   if [ "$CRD_SINGULAR" = SGConfig ]
   then
-    continue;
+    yq -y '.spec.webhookdefinitions = (.spec.webhookdefinitions 
+      | map(select((.type == "ConversionWebhook" and .conversionCRDs[0] == "'"$CRD_NAME"'") | not))
+      )' bundle/manifests/stackgres.clusterserviceversion.yaml \
+      > bundle/manifests/stackgres.clusterserviceversion.yaml.tmp
+  else
+    yq -y '.spec.webhookdefinitions = (.spec.webhookdefinitions 
+      | map(
+        if .type == "ConversionWebhook" and .conversionCRDs[0] == "'"$CRD_NAME"'"
+          then .webhookPath = "/stackgres/conversion/'"$CRD_SINGULAR"'" else . end
+        )
+      )' bundle/manifests/stackgres.clusterserviceversion.yaml \
+      > bundle/manifests/stackgres.clusterserviceversion.yaml.tmp
   fi
-  yq -y '.spec.webhookdefinitions = (.spec.webhookdefinitions 
-    | map(
-      if .type == "ConversionWebhook" and .conversionCRDs[0] == "'"$CRD_NAME"'"
-        then .webhookPath = "/stackgres/conversion/'"$CRD_SINGULAR"'" else . end
-      )
-    )' bundle/manifests/stackgres.clusterserviceversion.yaml \
-    > bundle/manifests/stackgres.clusterserviceversion.yaml.tmp
   mv bundle/manifests/stackgres.clusterserviceversion.yaml.tmp \
     bundle/manifests/stackgres.clusterserviceversion.yaml
 done
