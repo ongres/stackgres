ec4ec30f108396bf543a2026c97d7e1c6398354e
Matteo Melli <matteom@ongres.com>
2023-12-12 11:38:53 +0100

    fix: sharded backups do not start if cronSchedule is not set

--- stackgres-k8s/src/operator/src/main/java/io/stackgres/operator/conciliation/factory/shardedcluster/backup/ShardedBackupTemplatesVolumeFactory.java
+++ stackgres-k8s/src/operator/src/main/java/io/stackgres/operator/conciliation/factory/shardedcluster/backup/ShardedBackupTemplatesVolumeFactory.java
@@ -5,48 +5,28 @@
 
 package io.stackgres.operator.conciliation.factory.shardedcluster.backup;
 
-import java.util.Map;
 import java.util.stream.Stream;
 
-import io.fabric8.kubernetes.api.model.ConfigMapBuilder;
 import io.fabric8.kubernetes.api.model.ConfigMapVolumeSourceBuilder;
-import io.fabric8.kubernetes.api.model.HasMetadata;
 import io.fabric8.kubernetes.api.model.Volume;
 import io.fabric8.kubernetes.api.model.VolumeBuilder;
 import io.stackgres.common.StackGresVolume;
-import io.stackgres.common.crd.sgshardedcluster.StackGresShardedCluster;
-import io.stackgres.common.labels.LabelFactoryForShardedCluster;
-import io.stackgres.operator.conciliation.factory.AbstractTemplatesConfigMap;
 import io.stackgres.operator.conciliation.factory.ImmutableVolumePair;
+import io.stackgres.operator.conciliation.factory.VolumeFactory;
 import io.stackgres.operator.conciliation.factory.VolumePair;
 import io.stackgres.operator.conciliation.shardedcluster.StackGresShardedClusterContext;
 import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.inject.Inject;
 import org.jetbrains.annotations.NotNull;
 
 @ApplicationScoped
 public class ShardedBackupTemplatesVolumeFactory
-    extends AbstractTemplatesConfigMap<StackGresShardedClusterContext> {
-
-  private LabelFactoryForShardedCluster labelFactory;
-
-  public static String name(StackGresShardedClusterContext context) {
-    final String clusterName = context.getSource().getMetadata().getName();
-    return StackGresVolume.SCRIPT_TEMPLATES.getResourceName(clusterName);
-  }
-
-  @Inject
-  public ShardedBackupTemplatesVolumeFactory(LabelFactoryForShardedCluster labelFactory) {
-    super();
-    this.labelFactory = labelFactory;
-  }
+    implements VolumeFactory<StackGresShardedClusterContext> {
 
   @Override
   public @NotNull Stream<VolumePair> buildVolumes(StackGresShardedClusterContext context) {
     return Stream.of(
         ImmutableVolumePair.builder()
             .volume(buildVolume(context))
-            .source(buildSource(context))
             .build());
   }
 
@@ -54,23 +34,9 @@ public class ShardedBackupTemplatesVolumeFactory
     return new VolumeBuilder()
         .withName(StackGresVolume.SCRIPT_TEMPLATES.getName())
         .withConfigMap(new ConfigMapVolumeSourceBuilder()
-            .withName(name(context))
+            .withName(ShardedBackupTemplatesConfigMap.name(context))
             .withDefaultMode(0444)
             .build())
-        .build();
-  }
-
-  public @NotNull HasMetadata buildSource(StackGresShardedClusterContext context) {
-    Map<String, String> data = getShardedClusterTemplates();
-
-    final StackGresShardedCluster cluster = context.getSource();
-    return new ConfigMapBuilder()
-        .withNewMetadata()
-        .withNamespace(cluster.getMetadata().getNamespace())
-        .withName(name(context))
-        .withLabels(labelFactory.genericLabels(cluster))
-        .endMetadata()
-        .withData(data)
         .build();
   }
 
