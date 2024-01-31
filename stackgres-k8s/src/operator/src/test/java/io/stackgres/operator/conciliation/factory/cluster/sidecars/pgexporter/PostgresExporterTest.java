e6b54b5ad913347a5c12bd65de260ef0a9e7dd3c
Gianluca Cannata <gcannata@ongres.com>
2024-01-31 09:57:56 +0100

    feat: PostgresExporter: remove PgBouncer queries if Cluster connection pooling is disabled

--- stackgres-k8s/src/operator/src/test/java/io/stackgres/operator/conciliation/factory/cluster/sidecars/pgexporter/PostgresExporterTest.java
+++ stackgres-k8s/src/operator/src/test/java/io/stackgres/operator/conciliation/factory/cluster/sidecars/pgexporter/PostgresExporterTest.java
@@ -1,13 +1,19 @@
 /*
- * Copyright (C) 2019 OnGres, Inc.
+ * Copyright (C) 2024 OnGres, Inc.
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
 package io.stackgres.operator.conciliation.factory.cluster.sidecars.pgexporter;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.List;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import io.fabric8.kubernetes.api.model.ConfigMap;
+import io.fabric8.kubernetes.api.model.ConfigMapVolumeSourceBuilder;
+import io.stackgres.common.StackGresVolume;
+import io.stackgres.common.YamlMapperProvider;
+import io.stackgres.common.crd.Volume;
+import io.stackgres.common.crd.VolumeBuilder;
 import io.stackgres.common.crd.sgcluster.StackGresCluster;
 import io.stackgres.common.crd.sgconfig.StackGresConfig;
 import io.stackgres.common.crd.sgpgconfig.StackGresPostgresConfig;
@@ -18,54 +24,126 @@ import io.stackgres.operator.conciliation.cluster.ImmutableStackGresClusterConte
 import io.stackgres.operator.conciliation.factory.ContainerUserOverrideMounts;
 import io.stackgres.operator.conciliation.factory.PostgresSocketMount;
 import io.stackgres.operator.conciliation.factory.ScriptTemplatesVolumeMounts;
+import io.stackgres.operator.conciliation.factory.VolumePair;
 import io.stackgres.operator.conciliation.factory.cluster.ClusterContainerContext;
 import io.stackgres.operator.conciliation.factory.cluster.ImmutableClusterContainerContext;
-import jakarta.inject.Inject;
+import io.stackgres.testutil.JsonUtil;
+import org.jetbrains.annotations.NotNull;
+import org.jooq.lambda.Seq;
+import org.jooq.lambda.Unchecked;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 class PostgresExporterTest {
 
-  @Inject
+  @Mock
   private LabelFactoryForCluster<StackGresCluster> labelFactory;
 
-  @Inject
+  @Mock
   private ContainerUserOverrideMounts containerUserOverrideMounts;
 
-  @Inject
+  @Mock
   private PostgresSocketMount postgresSocket;
 
-  @Inject
+  @Mock
   private ScriptTemplatesVolumeMounts scriptTemplatesVolumeMounts;
 
-  @InjectMocks
+  private final YamlMapperProvider yamlMapperProvider = new YamlMapperProvider();
+
   private PostgresExporter postgresExporter;
 
   @BeforeEach
   public void setupClass() {
     this.postgresExporter = new PostgresExporter(labelFactory, containerUserOverrideMounts,
-        postgresSocket, scriptTemplatesVolumeMounts);
+        postgresSocket, scriptTemplatesVolumeMounts, yamlMapperProvider);
   }
 
   @Test
   void ifDisableMetricsExporterIsNotSpecified_shouldBeActivated() {
     ClusterContainerContext context = getClusterContainerContext();
-    assertTrue(postgresExporter.isActivated(context));
+    Assertions.assertTrue(postgresExporter.isActivated(context));
   }
 
   @Test
   void ifDisableMetricsExporterIsFalse_shouldBeActivated() {
     ClusterContainerContext context = getClusterContainerContext();
     context.getClusterContext().getSource().getSpec().getPods().setDisableMetricsExporter(false);
-    assertTrue(postgresExporter.isActivated(context));
+    Assertions.assertTrue(postgresExporter.isActivated(context));
   }
 
   @Test
   void ifDisableMetricsExporterIsTrue_shouldNotBeActivated() {
     ClusterContainerContext context = getClusterContainerContext();
     context.getClusterContext().getSource().getSpec().getPods().setDisableMetricsExporter(true);
-    assertFalse(postgresExporter.isActivated(context));
+    Assertions.assertFalse(postgresExporter.isActivated(context));
+  }
+
+  @Test
+  void ifDisableConnectionPooling_shouldNotBeActivated() {
+    ClusterContainerContext context = getClusterContainerContext();
+    context.getClusterContext().getCluster().getSpec().getPods().setDisableConnectionPooling(true);
+
+    List<VolumePair> volumes = postgresExporter.buildVolumes(context.getClusterContext()).toList();
+
+    Volume expected = new VolumeBuilder()
+        .withName(StackGresVolume.EXPORTER_QUERIES.getName())
+        .withConfigMap(new ConfigMapVolumeSourceBuilder()
+          .withName(PostgresExporter.configName(context.getClusterContext()))
+          .build())
+        .build();
+
+    Assertions.assertEquals(1L, volumes.size());
+    Assertions.assertEquals(expected, volumes.getFirst().getVolume());
+    Assertions.assertTrue(volumes.getFirst().getSource().isPresent());
+    Assertions.assertInstanceOf(ConfigMap.class, volumes.getFirst().getSource().get());
+    Assertions.assertEquals(
+        expected.getConfigMap().getName(),
+        volumes.getFirst().getSource().get().getMetadata().getName()
+    );
+
+    var sourceVolumeDataQueriesKeys = getSourceVolumeDataQueriesKeys(volumes);
+
+    Assertions.assertFalse(sourceVolumeDataQueriesKeys.isEmpty());
+    Assertions.assertFalse(
+        sourceVolumeDataQueriesKeys.stream()
+          .anyMatch(key -> key.startsWith(PostgresExporter.POSTGRES_EXPORTER_PGBOUNCER_QUERIES_PREFIX))
+    );
+  }
+
+  @Test
+  void ifEnabledConnectionPooling_shouldBeActivated() {
+    ClusterContainerContext context = getClusterContainerContext();
+    context.getClusterContext().getCluster().getSpec().getPods().setDisableConnectionPooling(false);
+
+    List<VolumePair> volumes = postgresExporter.buildVolumes(context.getClusterContext()).toList();
+
+    Volume expected = new VolumeBuilder()
+        .withName(StackGresVolume.EXPORTER_QUERIES.getName())
+        .withConfigMap(new ConfigMapVolumeSourceBuilder()
+          .withName(PostgresExporter.configName(context.getClusterContext()))
+          .build())
+        .build();
+
+    Assertions.assertEquals(1L, volumes.size());
+    Assertions.assertEquals(expected, volumes.getFirst().getVolume());
+    Assertions.assertTrue(volumes.getFirst().getSource().isPresent());
+    Assertions.assertInstanceOf(ConfigMap.class, volumes.getFirst().getSource().get());
+    Assertions.assertEquals(
+        expected.getConfigMap().getName(), volumes.getFirst().getSource().get().getMetadata().getName()
+    );
+
+    var sourceVolumeDataQueriesKeys = getSourceVolumeDataQueriesKeys(volumes);
+
+    Assertions.assertFalse(sourceVolumeDataQueriesKeys.isEmpty());
+    Assertions.assertTrue(
+        sourceVolumeDataQueriesKeys.stream()
+          .anyMatch(key -> key.startsWith(PostgresExporter.POSTGRES_EXPORTER_PGBOUNCER_QUERIES_PREFIX))
+    );
   }
 
   private ClusterContainerContext getClusterContainerContext() {
@@ -86,5 +164,20 @@ class PostgresExporterTest {
 
   private StackGresCluster getDefaultCluster() {
     return Fixtures.cluster().loadDefault().get();
+  }
+
+  @NotNull
+  private static List<String> getSourceVolumeDataQueriesKeys(List<VolumePair> volumes) {
+    return volumes
+      .getFirst()
+      .getSource()
+      .map(ConfigMap.class::cast)
+      .map(ConfigMap::getData)
+      .map(data -> data.get(PostgresExporter.QUERIES_YAML))
+      .map(Unchecked.function(queries -> JsonUtil.yamlMapper().readTree(queries)))
+      .map(JsonNode::fieldNames)
+      .stream()
+      .flatMap(Seq::seq)
+      .toList();
   }
 }
