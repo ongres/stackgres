fd4e9dfdbe75b755833de83d2b11a2e7fee7ca2e
Luis Garcia <lgarcia@ongres.com>
2024-04-16 18:36:32 +0200

    chore(ui): include main loading overlay

--- stackgres-k8s/src/admin-ui/src/App.vue
+++ stackgres-k8s/src/admin-ui/src/App.vue
@@ -2,7 +2,13 @@
   <div id="app">
 		<NavBar></NavBar>
 		<SideBar v-if="$route.params.hasOwnProperty('namespace')"></SideBar>
-    <div id="main" :class="!$route.params.hasOwnProperty('namespace') && 'noSidebar'">
+    <div
+      id="main"
+      :class="[
+        !$route.params.hasOwnProperty('namespace') && 'noSidebar',
+        isLoading && 'loading'
+      ]"
+    >
       <HeaderSection></HeaderSection>
       <router-view :key="$route.path"></router-view>
     </div>
@@ -14,12 +20,19 @@
   import NavBar from '@/components/navbar/NavBar.vue'
   import SideBar from '@/components/SideBar.vue'
   import HeaderSection from '@/components/navbar/HeaderSection.vue'
+  import store from './store'
 
   export default {
     components: {
       NavBar,
       SideBar, 
       HeaderSection
+    },
+
+    computed: {
+      isLoading() {
+        return store.state.isLoading
+      }
     }
   }
 
@@ -30,5 +43,17 @@
     margin: 0 auto;
     float: none;
     width: 100%;
+  }
+
+  #main.loading:after {
+    display: block;
+    content: " ";
+    position: absolute;
+    top: 50px;
+    left: 0;
+    width: 100%;
+    height: calc(100% - 50px);
+    background: url('/assets/img/loader.gif') center no-repeat rgba(0, 0, 0, .35);
+    background-size: 30px;
   }
 </style>
