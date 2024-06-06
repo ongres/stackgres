287f5fc20869c6a3d92fc6efc0ed6a2055e030e1
Matteo Melli <matteom@ongres.com>
2024-06-06 13:06:36 +0200

    fix: native image script overwrites key and cert files paths globally

--- stackgres-k8s/src/operator/src/main/docker/stackgres-operator.native.sh
+++ stackgres-k8s/src/operator/src/main/docker/stackgres-operator.native.sh
@@ -4,14 +4,6 @@ if [ "$DEBUG_OPERATOR" = true ]
 then
   set -x
 fi
-if [ -n "$OPERATOR_CERT_FILE" ]
-then
-  APP_OPTS="$APP_OPTS -Dquarkus.http.ssl.certificate.files=$OPERATOR_CERT_FILE"
-fi
-if [ -n "$OPERATOR_KEY_FILE" ]
-then
-  APP_OPTS="$APP_OPTS -Dquarkus.http.ssl.certificate.key-files=$OPERATOR_KEY_FILE"
-fi
 if [ -n "$OPERATOR_LOG_LEVEL" ]
 then
   APP_OPTS="$APP_OPTS -Dquarkus.log.level=$OPERATOR_LOG_LEVEL"
@@ -23,4 +15,8 @@ fi
 exec /app/stackgres-operator \
   -Dquarkus.http.host=0.0.0.0 \
   -Djava.util.logging.manager=org.jboss.logmanager.LogManager \
+  $(
+    [ -z "$OPERATOR_CERT_FILE" ] || printf ' %s' "-Dquarkus.http.ssl.certificate.files=$OPERATOR_CERT_FILE"
+    [ -z "$OPERATOR_KEY_FILE" ] || printf ' %s' "-Dquarkus.http.ssl.certificate.key-files=$OPERATOR_KEY_FILE"
+  ) \
   $APP_OPTS
