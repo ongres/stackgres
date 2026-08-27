620fb161539d2ca34187c493f1532b79d613b503
Sebastian Daschner <sebastian.daschner@ongres.com>
2026-08-27 13:09:19 +0200

    chore: updated CI build script for Matriarch

--- stackgres-k8s/ci/build/redact-version.sh
+++ stackgres-k8s/ci/build/redact-version.sh
@@ -6,6 +6,8 @@ POM_FILE=../../src/pom.xml
 if [ -n "$1" ]
 then
   POM_FILE="../../src/$1/pom.xml"
+  # repo-root Matriarch modules (proto, matriarch) live outside stackgres-k8s/src
+  [ -f "$POM_FILE" ] || POM_FILE="../../../$1/pom.xml"
 fi
 
 sed -f redact-version.sed "$POM_FILE"
